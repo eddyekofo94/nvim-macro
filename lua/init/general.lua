a4520857ccb74f1b6802fc34978849cad31ac170
bekaboo <kankefengjing@gmail.com>
2022-01-20 15:18:04 +0800

    t6: Fix: general.lua: typos; Add: general.lua: spelloptions, no swapfile; Mod: hl trailing spaces -> underline trailing spaces.

--- lua/init/general.lua
+++ lua/init/general.lua
@@ -18,7 +18,7 @@ o.termguicolors = false     -- Default GUI colors are too vivid
 execute                     -- Underline bad spellings
 [[
 hi clear SpellBad
-hi SpellBad cterm=underline
+hi SpellBad cterm=undercurl
 ]]
 execute
 [[
@@ -28,10 +28,11 @@ match OverLength /\%81v.*/
 execute [[ highlight Pmenu ctermbg=gray guibg=gray ]]
 execute                     -- Underline trailing white spaces
 [[
-highlight TrailingWhitespace cterm=underline guibg=#ddc7a1
-match TrailingWhitespace /\s\+$/
+highlight TrailingWhiteSpace cterm=underline guibg=#ddc7a1
+match TrailingWhiteSpace /\s\+$/
 ]]
 o.updatetime = 100  -- (ms)
+o.swapfile = false
 
 -- Indentation settings
 o.ts = 4
@@ -51,9 +52,16 @@ o.spell = true
 g.spelllang = {en, cjk}
 o.spellsuggest = 'best, 9'
 o.spellcapcheck = ''
+o.spelloptions = 'camel'
+-- Excludes characters that are in the parenthesis
+-- and are preceded by a capitalized letter
+execute [[ syn match myExCapitalWords +\<\w*[A-Z]\S*\>+ contains=@NoSpell ]]
+-- Exclude capitalized words and capitalized words + 's'
+execute [[ syn match myExCapitalWords +\<[A-Z]\w*\>+ contains=@NoSpell ]]
+execute [[ syn match myExCapitalWords +\<\w*[A-Z]\K*\>\|'s+ contains=@NoSpell ]]
 
 
--- Lsp options
+-- LSP options
 lsp.handlers["textDocument/publishDiagnostics"] =
   lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
     -- Disable underline, it's very annoying
