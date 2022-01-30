d46e6f27da4a5613ab386640746dc7bb5b2e0397
bekaboo <kankefengjing@gmail.com>
2022-01-30 18:26:40 +0800

    59: Mod: treesitter installs parser without confirmation.

--- lua/plugin-configs/config-nvim-treesitter.lua
+++ lua/plugin-configs/config-nvim-treesitter.lua
@@ -40,19 +40,30 @@ require 'nvim-treesitter.configs'.setup {
 -- Automatically install parser for new filetype (with confirmation)
 -- From nvim-treesitter issue #2108:
 -- https://github.com/nvim-treesitter/nvim-treesitter/issues/2108
-local ask_install = {}
+-- local ask_install = {}
+-- function _G.ensure_treesitter_language_installed()
+--   local parsers = require "nvim-treesitter.parsers"
+--   local lang = parsers.get_buf_lang()
+--   if parsers.get_parser_configs()[lang] and not parsers.has_parser(lang) and ask_install[lang] ~= false then
+--     vim.schedule_wrap(function()
+--       vim.ui.select({"yes", "no"}, { prompt = "Install tree-sitter parsers for " .. lang .. "?" }, function(item)
+--         if item == "yes" then
+--           vim.cmd("TSInstall " .. lang)
+--         elseif item == "no" then
+--           ask_install[lang] = false
+--         end
+--       end)
+--     end)()
+--   end
+-- end
+
+-- Automatically install treesitter parsers (no confirmation)
+local parsers = require'nvim-treesitter.parsers'
 function _G.ensure_treesitter_language_installed()
-  local parsers = require "nvim-treesitter.parsers"
   local lang = parsers.get_buf_lang()
-  if parsers.get_parser_configs()[lang] and not parsers.has_parser(lang) and ask_install[lang] ~= false then
+  if parsers.get_parser_configs()[lang] and not parsers.has_parser(lang) then
     vim.schedule_wrap(function()
-      vim.ui.select({"yes", "no"}, { prompt = "Install tree-sitter parsers for " .. lang .. "?" }, function(item)
-        if item == "yes" then
-          vim.cmd("TSInstall " .. lang)
-        elseif item == "no" then
-          ask_install[lang] = false
-        end
-      end)
+    vim.cmd("TSInstall "..lang)
     end)()
   end
 end
