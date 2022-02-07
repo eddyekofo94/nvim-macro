5500939df602c313311070a264627920649f9181
bekaboo <kankefengjing@gmail.com>
2022-02-07 17:04:37 +0800

    81: Fix: nvim-tree bug in recent commit, specify commit.

--- lua/plugin-specs/spec-nvim-tree.lua
+++ lua/plugin-specs/spec-nvim-tree.lua
@@ -2,6 +2,7 @@ local get = require('utils/get')
 
 return {
   'kyazdani42/nvim-tree.lua',
+  commit = '7b64075bdf66fc31660f35fa67ba71611121ac1d',
   cmd = {
     'NvimTreeClipboard', 'NvimTreeClose', 'NvimTreeFindFile',
     'NvimTreeFindFileToggle', 'NvimTreeFocus', 'NvimTreeOpen',
