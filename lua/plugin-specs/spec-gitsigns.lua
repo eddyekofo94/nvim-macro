8279063fd131230fe86145a47ab849bbb12fccb1
bekaboo <kankefengjing@gmail.com>
2022-02-08 01:50:53 +0800

    88: Opt: indent-blankline & vim-sleuth & gitsigns.
    89: Opt: gitsigns

--- lua/plugin-specs/spec-gitsigns.lua
+++ lua/plugin-specs/spec-gitsigns.lua
@@ -2,6 +2,7 @@ local get = require('utils/get')
 
 return {
   'lewis6991/gitsigns.nvim',
+  evnet = 'BufAdd',
   requires = {
     get.spec('plenary'),
     get.spec('vim-repeat')
--- lua/plugin-specs/spec-indent-blankline.lua
+++ lua/plugin-specs/spec-indent-blankline.lua
@@ -2,6 +2,7 @@ local get = require('utils/get')
 
 return {
   'lukas-reineke/indent-blankline.nvim',
+  event = 'BufAdd',
   requires = {
     get.spec('nvim-treesitter'),    -- To identify functions, methods, etc
     get.spec('vim-sleuth')          -- To automatically detect indentation
