92e3140e8e248323df84e138af554949412835ca
bekaboo <kankefengjing@gmail.com>
2022-03-01 02:47:12 +0800

    inst_autp_2: Add: autopairs: fast_wrap.

--- lua/plugin-configs/config-nvim-autopairs.lua
+++ lua/plugin-configs/config-nvim-autopairs.lua
@@ -3,10 +3,16 @@ local Rule = require('nvim-autopairs.rule')
 
 npairs.setup({
   check_ts = true,
-  ts_config = {
-    lua = {'string'},   -- it will not add a pair on that treesitter node
-    javascript = {'template_string'},
-    java = false,       -- don't check treesitter on java
+  fast_wrap = {
+    map = '<M-e>',
+    chars = { '{', '[', '(', '"', "'", "`" },
+    pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], '%s+', ''),
+    offset = 0, -- Offset from pattern match
+    end_key = '$',
+    keys = 'qwertyuiopzxcvbnmasdfghjkl',
+    check_comma = true,
+    highlight = 'Search',
+    highlight_grey='Comment'
   }
 })
 
@@ -18,21 +24,21 @@ npairs.add_rules({
       return vim.tbl_contains({ '()', '[]', '{}' }, pair)
     end),
   Rule('( ', ' )')
-      :with_pair(function() return false end)
-      :with_move(function(opts)
-          return opts.prev_char:match('.%)') ~= nil
-      end)
-      :use_key(')'),
+    :with_pair(function() return false end)
+    :with_move(function(opts)
+      return opts.prev_char:match('.%)') ~= nil
+    end)
+    :use_key(')'),
   Rule('{ ', ' }')
-      :with_pair(function() return false end)
-      :with_move(function(opts)
-          return opts.prev_char:match('.%}') ~= nil
-      end)
-      :use_key('}'),
+    :with_pair(function() return false end)
+    :with_move(function(opts)
+      return opts.prev_char:match('.%}') ~= nil
+    end)
+    :use_key('}'),
   Rule('[ ', ' ]')
-      :with_pair(function() return false end)
-      :with_move(function(opts)
-          return opts.prev_char:match('.%]') ~= nil
-      end)
-      :use_key(']')
+    :with_pair(function() return false end)
+    :with_move(function(opts)
+      return opts.prev_char:match('.%]') ~= nil
+    end)
+    :use_key(']')
 })
