f46e8a1d83ec8f58e6d36f0aace9d26d1bb7d239
bekaboo <kankefengjing@gmail.com>
2022-03-07 16:39:15 +0800

    151: Remove/Mod: nvim-treesitter-textobj move settings.

--- lua/plugin-configs/config-nvim-treesitter.lua
+++ lua/plugin-configs/config-nvim-treesitter.lua
@@ -57,26 +57,6 @@ require 'nvim-treesitter.configs'.setup {
           ['ap'] = '@parameter.outer',
           ['a/'] = '@comment.outer',
           ['a*'] = '@comment.outer'
-        },
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            [']m'] = '@function.outer',
-            [']]'] = '@class.outer',
-          },
-          goto_next_end = {
-            [']M'] = '@function.outer',
-            [']['] = '@class.outer',
-          },
-          goto_previous_start = {
-            ['[m'] = '@function.outer',
-            ['[['] = '@class.outer',
-          },
-          goto_previous_end = {
-            ['[M'] = '@function.outer',
-            ['[]'] = '@class.outer',
-          }
         }
       }
     }
