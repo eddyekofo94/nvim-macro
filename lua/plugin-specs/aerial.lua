d85ed83072b9e4519f938a9ca82d7e5148724096
bekaboo <kankefengjing@gmail.com>
2022-03-12 19:54:24 +0800

    193: OptMod: in aerial spec: aerial loaded after nvim-lsp-installer.

--- lua/plugin-specs/aerial.lua
+++ lua/plugin-specs/aerial.lua
@@ -2,13 +2,11 @@ local get = require('utils.get')
 
 return {
   'stevearc/aerial.nvim',
-  ft = get.ft_list(require('utils.shared').langs),
-  keys = { '<Leader>aa', '<Leader>ao' },
-  cmd = { 'AerialOpen', 'AerialToggle' },
   requires = {
     get.spec('nvim-lspconfig'),
     get.spec('nvim-lsp-installer'),
     get.spec('nvim-treesitter')
   },
+  after = 'nvim-lsp-installer',
   config = get.config('aerial')
 }
