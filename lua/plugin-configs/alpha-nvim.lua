523d4f88dbdf09cb2697dbc22cfd73c15e987e60
bekaboo <kankefengjing@gmail.com>
2022-03-19 23:22:52 +0800

    206: Mod: alpha-nvim config: layout padding calculation; button icon.

--- lua/plugin-configs/alpha-nvim.lua
+++ lua/plugin-configs/alpha-nvim.lua
@@ -41,7 +41,7 @@ dashboard.section.header.val = headers[math.random(1, #headers)]
 
 dashboard.section.buttons.val= {
   button('e', 'ﱐ  New file', leader, '<cmd>ene<CR>'),
-  button('s', '  Sync plugins' , leader, '<cmd>PackerSync<CR>'),
+  button('s', '  Sync plugins' , leader, '<cmd>PackerSync<CR>'),
   button('c', '  Configurations', leader, '<cmd>e ~/.config/nvim/<CR>'),
   button(leader .. ' f f', '  Find files', leader, '<cmd>Telescope find_files<CR>'),
   button(leader .. ' fof', '  Find old files', leader, '<cmd>Telescope oldfiles<CR>'),
@@ -84,13 +84,10 @@ dashboard.section.footer.opts.hl = 'Comment'
 
 local head_butt_padding = 4
 local occu_height = #dashboard.section.header.val + 2 * #dashboard.section.buttons.val + head_butt_padding
-local header_padding = math.ceil((vim.fn.winheight('$') - occu_height) * 0.25)
+local header_padding = math.max(0, math.ceil((vim.fn.winheight('$') - occu_height) * 0.25))
+local foot_butt_padding_ub = vim.o.lines - header_padding - occu_height - #dashboard.section.footer.val - 3
 local foot_butt_padding = math.floor((vim.fn.winheight('$') - 2 * header_padding - occu_height))
-if 0 > header_padding then header_padding = 0 end
-if vim.o.lines - header_padding - occu_height - foot_butt_padding - #dashboard.section.footer.val < 3 then
-  foot_butt_padding = vim.o.lines - header_padding - occu_height - #dashboard.section.footer.val - 3
-  if foot_butt_padding < 0 then foot_butt_padding = 0 end
-end
+foot_butt_padding = math.max(0, math.max(math.min(0, foot_butt_padding), math.min(math.max(0, foot_butt_padding), foot_butt_padding_ub)))
 
 dashboard.config.layout = {
   { type = 'padding', val = header_padding },
