3a2d1488592fbee33422539520ed76ba415fa1fa
bekaboo <kankefengjing@gmail.com>
2022-05-20 01:45:40 +0800

    226: Fix: nvim-lsp-installer spec: remove run... ; fix typo.

--- lua/plugin-specs/nvim-lsp-installer.lua
+++ lua/plugin-specs/nvim-lsp-installer.lua
@@ -3,16 +3,14 @@ local get = require('utils.get')
 return {
   'williamboman/nvim-lsp-installer',
 
-  -- Use double border instead of rounded border (unluckily, lsp-installer has not provided a config option for this...)
-  run = "sed -i -e 's/border = \"rounded\",*/border = \"double\"/' ~/.local/share/nvim/site/pack/packer/opt/nvim-lsp-installer/lua/nvim-lsp-installer/ui/display.lua",
   cmd = {
-      'LspInstallInfo', 'LspInstall', 'LspUninstall', 'LspUninstallAll',
-      'LspInstallLog', 'LspPrintInstalled'
+    'LspInstallInfo', 'LspInstall', 'LspUninstall', 'LspUninstallAll',
+    'LspInstallLog', 'LspPrintInstalled'
   },
   requires = get.spec('nvim-lspconfig'),
 
   -- If cmp-nvim-lsp is installed then this
-  -- plugin should be loaded after cmp-nvimlsp
+  -- plugin should be loaded after cmp-nvim-lsp
   after = { 'nvim-lspconfig', 'cmp-nvim-lsp' },
   config = get.config('nvim-lsp-installer')
 }
