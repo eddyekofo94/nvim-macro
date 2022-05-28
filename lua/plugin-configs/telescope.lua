1e7278e074a8e46e2479420217ef515b8e0e305c
bekaboo <kankefengjing@gmail.com>
2022-05-29 00:30:49 +0800

    230: Mod: telescope config: map keybindings to lua functions; add new keymap for git_files().

--- lua/plugin-configs/telescope.lua
+++ lua/plugin-configs/telescope.lua
@@ -1,31 +1,32 @@
-local map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 map('n', '<Leader>F', [[<cmd>Telescope<CR>]], opts)
-map('n', '<Leader>ff', [[<cmd>lua require('telescope.builtin').find_files()<CR>]], opts)
-map('n', '<Leader>fof', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], opts)
-map('n', '<Leader>f;', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], opts)
-map('n', '<Leader>f*', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], opts)
-map('n', '<Leader>fh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], opts)
-map('n', '<Leader>fm', [[<cmd>lua require('telescope.builtin').marks()<CR>]], opts)
-map('n', '<Leader>fq', [[<cmd>lua require('telescope.builtin').quickfix()<CR>]], opts)
-map('n', '<Leader>fl', [[<cmd>lua require('telescope.builtin').loclist()<CR>]], opts)
-map('n', '<Leader>fbf', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], opts)
-map('n', '<Leader>fR', [[<cmd>lua require('telescope.builtin').lsp_references()<CR>]], opts)
-map('n', '<Leader>fa', [[<cmd>lua require('telescope.builtin').lsp_code_actions()<CR>]], opts)
-map('n', '<Leader>fe', [[<cmd>lua require('telescope.builtin').diagnostics()<CR>]], opts)
-map('n', '<Leader>fd', [[<cmd>lua require('telescope.builtin').lsp_definitions()<CR>]], opts)
-map('n', '<Leader>ftd', [[<cmd>lua require('telescope.builtin').lsp_type_definitions()<CR>]], opts)
-map('n', '<Leader>fi', [[<cmd>lua require('telescope.builtin').lsp_implementations()<CR>]], opts)
-map('n', '<Leader>fp', [[<cmd>lua require('telescope.builtin').treesitter()<CR>]], opts)
-map('n', '<Leader>fs', [[<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>]], opts)
-map('n', '<Leader>fS', [[<cmd>lua require('telescope.builtin').lsp_workspace_symbols()<CR>]], opts)
-map('n', '<Leader>fg', [[<cmd>lua require('telescope.builtin').git_status()<CR>]], opts)
-map('n', '<Leader>fj', [[<cmd>lua require('telescope').extensions.project.project({display_type = 'full'})<CR>]], opts)
+map('n', '<Leader>ff', function() require('telescope.builtin').find_files() end, opts)
+map('n', '<Leader>fof', function() require('telescope.builtin').oldfiles() end, opts)
+map('n', '<Leader>f;', function() require('telescope.builtin').live_grep() end, opts)
+map('n', '<Leader>f*', function() require('telescope.builtin').grep_string() end, opts)
+map('n', '<Leader>fh', function() require('telescope.builtin').help_tags() end, opts)
+map('n', '<Leader>fm', function() require('telescope.builtin').marks() end, opts)
+map('n', '<Leader>fq', function() require('telescope.builtin').quickfix() end, opts)
+map('n', '<Leader>fl', function() require('telescope.builtin').loclist() end, opts)
+map('n', '<Leader>fbf', function() require('telescope.builtin').current_buffer_fuzzy_find() end, opts)
+map('n', '<Leader>fR', function() require('telescope.builtin').lsp_references() end, opts)
+map('n', '<Leader>fa', function() require('telescope.builtin').lsp_code_actions() end, opts)
+map('n', '<Leader>fe', function() require('telescope.builtin').diagnostics() end, opts)
+map('n', '<Leader>fd', function() require('telescope.builtin').lsp_definitions() end, opts)
+map('n', '<Leader>ftd', function() require('telescope.builtin').lsp_type_definitions() end, opts)
+map('n', '<Leader>fi', function() require('telescope.builtin').lsp_implementations() end, opts)
+map('n', '<Leader>fp', function() require('telescope.builtin').treesitter() end, opts)
+map('n', '<Leader>fs', function() require('telescope.builtin').lsp_document_symbols() end, opts)
+map('n', '<Leader>fS', function() require('telescope.builtin').lsp_workspace_symbols() end, opts)
+map('n', '<Leader>fg', function() require('telescope.builtin').git_status() end, opts)
+map('n', '<Leader>fgf', function() require('telescope.builtin').git_files() end, opts)
+map('n', '<Leader>fj', function() require('telescope').extensions.project.project({ display_type = 'full' }) end, opts)
 
 local telescope = require('telescope')
 local actions = require('telescope.actions')
 
-telescope.setup{
+telescope.setup {
   defaults = {
     prompt_prefix = '/ ',
     selection_caret = '→ ',
