7ff98c72f0158416323888ce45ceb94d8b347139
bekaboo <kankefengjing@gmail.com>
2022-05-29 12:31:27 +0800

    232: Fix: nvim-tree config: fixes due to breaking changes.

--- lua/plugin-configs/nvim-tree.lua
+++ lua/plugin-configs/nvim-tree.lua
@@ -1,42 +1,27 @@
-local map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
 
-map('n', '<Leader>tt', [[<cmd>lua require('nvim-tree').toggle(false, false)<CR>]], {noremap = true})
-map('n', '<Leader>tc', [[<cmd>lua require('nvim-tree.actions.copy-paste').print_clipboard()<CR>]], {noremap = true})
-map('n', '<Leader>tq', [[<cmd>lua require('nvim-tree.view').close()<CR>]], {noremap = true})
-map('n', '<Leader>tff', [[<cmd>lua require('nvim-tree').find_file(true)<CR>]], {noremap = true})
-map('n', '<Leader>tft', [[<cmd>lua require('nvim-tree').toggle(true, false)<CR>]], {noremap = true})
-map('n', '<Leader>to', [[<cmd>lua require('nvim-tree').open()<CR> ]], {noremap = true})
-map('n', '<Leader>tr', [[<cmd>lua require('nvim-tree.actions.reloaders').reload_explorer()<CR>]], {noremap = true})
-
-local g = vim.g
-g.nvim_tree_highlight_opened_files = 2
-g.nvim_tree_indent_markers = 1
-g.nvim_tree_icons = {
-  git = {
-    unstaged = '',
-    staged = '',
-    unmerged = '',
-    renamed = '隷',
-    deleted = '',
-    untracked = '',
-    ignored = ''
-  }
-}
+map('n', '<Leader>tt', function() require('nvim-tree').toggle(false, false) end, { noremap = true })
+map('n', '<Leader>tc', function() require('nvim-tree.actions.copy-paste').print_clipboard() end, { noremap = true })
+map('n', '<Leader>tq', function() require('nvim-tree.view').close() end, { noremap = true })
+map('n', '<Leader>tff', function() require('nvim-tree').find_file(true) end, { noremap = true })
+map('n', '<Leader>tft', function() require('nvim-tree').toggle(true, false) end, { noremap = true })
+map('n', '<Leader>to', function() require('nvim-tree').open() end, { noremap = true })
+map('n', '<Leader>tr', function() require('nvim-tree.actions.reloaders').reload_explorer() end, { noremap = true })
 
 require 'nvim-tree'.setup {
   view = {
     auto_resize = true,
     mappings = {
       list = {
-        { key = '<C-[>',                action = 'dir_up' },
-        { key = '<M-v>',                action = 'vsplit' },
-        { key = '<M-s>',                action = 'split' },
-        { key = '<M-t>',                action = 'tabnew' },
-        { key = '{',                    action = 'first_sibling' },
-        { key = '}',                    action = 'last_sibling' },
-        { key = 'X',                    action = 'remove' },
-        { key = 'W',                    action = '' },
-        { key = 'zM',                   action = 'collapse_all' },
+        { key = '<C-[>', action = 'dir_up' },
+        { key = '<M-v>', action = 'vsplit' },
+        { key = '<M-s>', action = 'split' },
+        { key = '<M-t>', action = 'tabnew' },
+        { key = '{', action = 'first_sibling' },
+        { key = '}', action = 'last_sibling' },
+        { key = 'X', action = 'remove' },
+        { key = 'W', action = '' },
+        { key = 'zM', action = 'collapse_all' },
       }
     }
   },
@@ -49,5 +34,22 @@ require 'nvim-tree'.setup {
       info = ' ',    -- xf7fc
       hint = ' ',    -- xf835
     }
-  }
+  },
+  renderer = {
+    highlight_opened_files = 'name',
+    indent_markers = { enable = true },
+    icons = {
+      glyphs = {
+        git = {
+          unstaged = '',
+          staged = '',
+          unmerged = '',
+          renamed = '隷',
+          deleted = '',
+          untracked = '',
+          ignored = ''
+        }
+      }
+    }
+  },
 }
