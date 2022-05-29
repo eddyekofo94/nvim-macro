54f7e499aa021c64b0dbf804c72dcd1844715a07
bekaboo <kankefengjing@gmail.com>
2022-05-29 12:30:00 +0800

    231: Mod: gitsigns config: modification on changedelete symbol and highlightings.

--- lua/plugin-configs/gitsigns.lua
+++ lua/plugin-configs/gitsigns.lua
@@ -4,7 +4,7 @@ require('gitsigns').setup {
     change       = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
     delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
     topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '^', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    changedelete = {hl = 'GitSignsDelete', text = '~', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
   },
   signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
   numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
