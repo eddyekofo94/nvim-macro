a4939c8461ba28f2127f5e2901c25ac4ee805b90
bekaboo <kankefengjing@gmail.com>
2022-05-31 03:14:17 +0800

    244: OptMod: automatically load alpha-nvim only when argc() == 0.

--- lua/load/extra.lua
+++ lua/load/extra.lua
@@ -1,6 +1,13 @@
-if (vim.fn.argc() >= 2)
+local argc = vim.fn.argc()
+if (argc == 0)
+    and packer_plugins['alpha-nvim']
+    and not packer_plugins['alpha-nvim'].loaded
+then
+  vim.cmd [[packadd alpha-nvim]]
+  require('plugin-configs.alpha-nvim')
+elseif (argc >= 2)
     and packer_plugins['barbar.nvim']
-    and packer_plugins['barbar.nvim'].loaded == false
+    and not packer_plugins['barbar.nvim'].loaded
 then
   vim.cmd [[packadd barbar.nvim]]
   require('plugin-configs.barbar')
--- lua/plugin-specs/alpha-nvim.lua
+++ lua/plugin-specs/alpha-nvim.lua
@@ -2,6 +2,7 @@ local get = require('utils.get')
 
 return {
   'goolord/alpha-nvim',
+  cmd = 'Alpha',
   requires = get.spec('nvim-web-devicons'),
   config = get.config('alpha-nvim')
 }
