86f965cc2572ae8971978a6de0f4324ea0e2ed52
bekaboo <18127878294@qq.com>
2022-07-06 20:37:12 +0800

    248: Add: add comment to telescope-fzf spec.

--- lua/plugin-specs/telescope-fzf-native.lua
+++ lua/plugin-specs/telescope-fzf-native.lua
@@ -1,5 +1,7 @@
 return {
   'nvim-telescope/telescope-fzf-native.nvim',
+  -- If it complains 'fzf doesn't exists, run 'make' inside
+  -- the root folder of this plugin
   run = 'make',
   config = function() require('plugin-configs.telescope-fzf-native') end
 }
