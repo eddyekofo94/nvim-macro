e9daa37569a0f39ba381155dfb5199ac54d9b685
bekaboo <18127878294@qq.com>
2022-09-23 00:04:19 -0500

    278: Mod: Change telescope key mappings.

--- lua/plugin-specs/telescope.lua
+++ lua/plugin-specs/telescope.lua
@@ -3,12 +3,12 @@ return {
   opt = true,
   cmd = 'Telescope',
   keys = {
-    '<Leader>F', '<Leader>ff', '<Leader>fof', '<Leader>f;',
+    '<Leader>F', '<Leader>ff', '<Leader>fo', '<Leader>f;',
     '<Leader>f*', '<Leader>fh', '<Leader>fm', '<Leader>fq',
-    '<Leader>fl', '<Leader>fbf', '<Leader>fR', '<Leader>fa',
-    '<Leader>fe', '<Leader>fd', '<Leader>ftd', '<Leader>fi',
+    '<Leader>fl', '<Leader>fb', '<Leader>fR', '<Leader>fa',
+    '<Leader>fe', '<Leader>fd', '<Leader>ft', '<Leader>fi',
     '<Leader>fp', '<Leader>fs', '<Leader>fS', '<Leader>fg',
-    '<Leader>fgf', '<Leader>fj'
+    '<Leader>fj'
   },
   requires = {
     require('plugin-specs.plenary'),
