3c85cc6cc1dbdf3ce3949bac10a41b850ddc2680
bekaboo <18127878294@qq.com>
2022-09-23 00:04:53 -0500

    279: Mod: aerial config: close behavior.

--- lua/plugin-configs/aerial.lua
+++ lua/plugin-configs/aerial.lua
@@ -3,13 +3,6 @@ local M = {}
 M.aerial = require('aerial')
 
 M.opts = {
-  -- Enum: persist, close, auto, global
-  --   persist - aerial window will stay open until closed
-  --   close   - aerial window will close when original file is no longer visible
-  --   auto    - aerial window will stay open as long as there is a visible
-  --             buffer to attach to
-  --   global  - same as 'persist', and will always show symbols for the current buffer
-  close_behavior = 'close',
   -- Set to false to remove the default keybindings for the aerial buffer
   default_bindings = false,
   -- Disable aerial on files with this many lines
