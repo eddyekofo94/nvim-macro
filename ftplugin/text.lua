88ed38c008efd318deb50a71688e3ffa2de5bf8d
bekaboo <18127878294@qq.com>
2022-09-23 00:00:59 -0500

    274: Add: plain text ftplugin.

---
+++ ftplugin/text.lua
@@ -0,0 +1,7 @@
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.keymap.set({ 'n', 'v' }, 'j', 'gj', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, 'k', 'gk', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '$', 'g$', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '0', 'g0', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '^', 'g^', { noremap = true })
