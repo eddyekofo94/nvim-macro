5e96d344e187fb0672d7f70fb43b64967df9ccc6
bekaboo <18127878294@qq.com>
2022-10-17 00:12:07 -0500

    307: Fix: ftplugin for text.

--- ftplugin/text.lua
+++ ftplugin/text.lua
@@ -1,7 +1,7 @@
-vim.opt.wrap = true
-vim.opt.linebreak = true
-vim.keymap.set({ 'n', 'v' }, 'j', 'gj', { noremap = true })
-vim.keymap.set({ 'n', 'v' }, 'k', 'gk', { noremap = true })
-vim.keymap.set({ 'n', 'v' }, '$', 'g$', { noremap = true })
-vim.keymap.set({ 'n', 'v' }, '0', 'g0', { noremap = true })
-vim.keymap.set({ 'n', 'v' }, '^', 'g^', { noremap = true })
+vim.wo.wrap = true
+vim.wo.linebreak = true
+vim.keymap.set({ 'n', 'v' }, 'j', 'gj', { noremap = true, buffer = true })
+vim.keymap.set({ 'n', 'v' }, 'k', 'gk', { noremap = true, buffer = true })
+vim.keymap.set({ 'n', 'v' }, '$', 'g$', { noremap = true, buffer = true })
+vim.keymap.set({ 'n', 'v' }, '0', 'g0', { noremap = true, buffer = true })
+vim.keymap.set({ 'n', 'v' }, '^', 'g^', { noremap = true, buffer = true })
