005faff0416c0a152a30e5204c5be559a7fa5e95
bekaboo <18127878294@qq.com>
2022-11-27 21:00:54 -0600

    352: OptMod: optimize lazyloading barabr.nvim, lualine.nvim, and alpha-nvim.

--- lua/modules/ui/init.lua
+++ lua/modules/ui/init.lua
@@ -9,19 +9,36 @@ M['nvim-web-devicons'] = {
 
 M['barbar.nvim'] = {
   'romgrk/barbar.nvim',
-  event = 'BufAdd',
+  cond = function()
+    vim.api.nvim_create_autocmd('BufAdd', {
+      pattern = '*',
+      callback = function()
+        if not vim.g.loaded_barbar then
+          vim.g.loaded_barbar = true
+          vim.cmd('packadd barbar.nvim')
+          require('modules.ui.configs')['barbar.nvim']()
+        end
+      end
+    })
+    return vim.fn.argc() > 0
+  end,
   requries = 'nvim-web-devicons',
   config = configs['barbar.nvim'],
 }
 
 M['lualine.nvim'] = {
   'nvim-lualine/lualine.nvim',
+  event = 'BufReadPre',
   requires = 'nvim-web-devicons',
   config = configs['lualine.nvim'],
 }
 
 M['alpha-nvim'] = {
   'goolord/alpha-nvim',
+  cond = function()
+    return vim.fn.argc() == 0 and
+        vim.o.lines >= 36 and vim.o.columns >= 112
+  end,
   requires = 'nvim-web-devicons',
   config = configs['alpha-nvim'],
 }
