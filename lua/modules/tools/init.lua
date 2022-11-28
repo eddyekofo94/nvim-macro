11ea4ec1e9ce2cb5556d262d0040f4644c91de2d
bekaboo <18127878294@qq.com>
2022-11-27 22:26:52 -0600

    356: Reframe: cancel module misc and merge into tools.

--- lua/modules/tools/init.lua
+++ lua/modules/tools/init.lua
@@ -67,4 +67,33 @@ M['gitsigns.nvim'] = {
   config = configs['gitsigns.nvim'],
 }
 
+M['plenary.nvim'] = {
+ 'nvim-lua/plenary.nvim',
+ module = 'plenary',
+}
+
+M['nvim-surround'] = {
+  'kylechui/nvim-surround',
+  event = 'ModeChanged',
+  keys = { 'ys', 'ds', 'cs' },
+  config = configs['nvim-surround'],
+}
+
+M['Comment.nvim'] = {
+  'numToStr/Comment.nvim',
+  keys = { 'gc', 'gb' },
+  config = configs['Comment.nvim'],
+}
+
+M['vim-sleuth'] = {
+  'tpope/vim-sleuth',
+  event = 'BufReadPre',
+}
+
+M['nvim-autopairs'] = {
+  'windwp/nvim-autopairs',
+  event = 'InsertEnter',
+  config = configs['nvim-autopairs']
+}
+
 return M
