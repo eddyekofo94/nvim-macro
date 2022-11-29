ef39921058da566d288120f89999b968851b991e
bekaboo <18127878294@qq.com>
2022-11-29 03:16:12 -0600

    381: Mod: code style cleanup for custom plugin auto_hlsearch.lua.

--- plugin/auto_hlsearch.lua
+++ plugin/auto_hlsearch.lua
@@ -8,10 +8,8 @@ vim.keymap.set('n', 'g#', 'g#<cmd>set hlsearch<CR>', { noremap = true })
 vim.keymap.set('n', 'n', 'n<cmd>set hlsearch<CR>', { noremap = true })
 vim.keymap.set('n', 'N', 'N<cmd>set hlsearch<CR>', { noremap = true })
 
-vim.api.nvim_create_autocmd(
-  { 'InsertEnter' },
-  {
-    pattern = '*',
-    command = 'set nohlsearch'
-  }
-)
+vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
+  pattern = '*',
+  command = 'set nohlsearch'
+})
+
