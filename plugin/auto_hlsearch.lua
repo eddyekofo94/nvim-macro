100ee5c8d3ab5ede8dde2bd36ccd3f6f084fea99
bekaboo <18127878294@qq.com>
2022-11-28 20:30:41 -0600

    373: Add: plugin direcotry & auto_hlsearch.

---
+++ plugin/auto_hlsearch.lua
@@ -0,0 +1,17 @@
+vim.keymap.set('n', '\\', '<cmd>set hlsearch!<CR>', { noremap = true })
+vim.keymap.set('n', '/', '/<cmd>set hlsearch<CR>', { noremap = true })
+vim.keymap.set('n', '?', '?<cmd>set hlsearch<CR>', { noremap = true })
+vim.keymap.set('n', '*', '*<cmd>set hlsearch<CR>', { noremap = true })
+vim.keymap.set('n', '#', '#<cmd>set hlsearch<CR>', { noremap = true })
+vim.keymap.set('n', 'g*', 'g*<cmd>set hlsearch<CR>', { noremap = true })
+vim.keymap.set('n', 'g#', 'g#<cmd>set hlsearch<CR>', { noremap = true })
+vim.keymap.set('n', 'n', 'n<cmd>set hlsearch<CR>', { noremap = true })
+vim.keymap.set('n', 'N', 'N<cmd>set hlsearch<CR>', { noremap = true })
+
+vim.api.nvim_create_autocmd(
+  { 'InsertEnter' },
+  {
+    pattern = '*',
+    command = 'set nohlsearch'
+  }
+)
