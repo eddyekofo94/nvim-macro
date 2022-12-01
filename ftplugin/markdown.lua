d71869c7ab601fc024451c0cf7501493716d6226
bekaboo <18127878294@qq.com>
2022-12-01 13:24:58 -0600

    414: Mod: ftplugin for markdown: run pandoc asynchronously.

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,11 +1,24 @@
-vim.api.nvim_buf_set_keymap(0, 'n', '<C-p>',
-  '<cmd>MarkdownPreview<CR>', { noremap = true, silent = true })
+local handler_md2pdf = nil
 
 vim.api.nvim_buf_create_user_command(
   0, 'MarkdownToPDF', function ()
+    local fname_md = vim.fn.expand('%')
+    local fname_pdf = string.format('%s.pdf', vim.fn.expand('%:r'))
     vim.cmd('write')
-    vim.cmd('silent !md2pdf %')
-    vim.cmd('silent !okular %:r.pdf &')
+    handler_md2pdf = vim.loop.spawn(
+      'md2pdf',   -- under ~/.scripts, should be append to $PATH
+      { args = { fname_md } },
+      function(code, _)
+        if handler_md2pdf then
+          handler_md2pdf:close()
+        end
+        if code ~= 0 then
+          vim.notify(string.format('md2pdf failed with code %d', code))
+        else
+          os.execute(string.format('okular %s', fname_pdf))
+        end
+      end
+    )
   end,
   { desc = 'Convert markdown to pdf' }
 )
