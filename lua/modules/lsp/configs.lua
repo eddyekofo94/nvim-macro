fdb9f17873d7c65bf41abc1282b8543644224ce2
bekaboo <18127878294@qq.com>
2022-12-02 17:25:15 -0600

    435: Mod/Add: add vim.lsp.buf.format keymap in visual mode.

--- lua/modules/lsp/configs.lua
+++ lua/modules/lsp/configs.lua
@@ -47,27 +47,28 @@ M['nvim-lspconfig'] = function()
     -- Use an on_attach function to only map the following keys
     -- after the language server attaches to the current buffer
     local keymaps = {
-      { 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>' },
-      { 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>' },
-      { 'K', '<cmd>lua vim.lsp.buf.hover()<CR>' },
-      { '<leader>ls', '<cmd>lua vim.lsp.buf.signature_help()<CR>' },
-      { '<Leader>li', '<cmd>lua vim.lsp.buf.implementation()<CR>' },
-      { '<Leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>' },
-      { '<Leader>wd', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>' },
-      { '<Leader>wl', '<cmd>lua vim.pretty_print(vim.lsp.buf.list_workspace_folders())<CR>' },
-      { '<leader>gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>' },
-      { '<Leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>' },
-      { '<Leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>' },
-      { '<Leader>R', '<cmd>lua vim.lsp.buf.references()<CR>' },
-      { '<Leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>' },
-      { '[e', '<cmd>lua vim.diagnostic.goto_prev()<CR>' },
-      { ']e', '<cmd>lua vim.diagnostic.goto_next()<CR>' },
-      { '<leader>ll', '<cmd>lua vim.diagnostic.setloclist()<CR>' },
-      { '<leader>=', '<cmd>lua vim.lsp.buf.format()<CR>' },
+      { 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>' },
+      { 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>' },
+      { 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>' },
+      { 'n', '<leader>ls', '<cmd>lua vim.lsp.buf.signature_help()<CR>' },
+      { 'n', '<Leader>li', '<cmd>lua vim.lsp.buf.implementation()<CR>' },
+      { 'n', '<Leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>' },
+      { 'n', '<Leader>wd', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>' },
+      { 'n', '<Leader>wl', '<cmd>lua vim.pretty_print(vim.lsp.buf.list_workspace_folders())<CR>' },
+      { 'n', '<leader>gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>' },
+      { 'n', '<Leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>' },
+      { 'n', '<Leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>' },
+      { 'n', '<Leader>R', '<cmd>lua vim.lsp.buf.references()<CR>' },
+      { 'n', '<Leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>' },
+      { 'n', '[e', '<cmd>lua vim.diagnostic.goto_prev()<CR>' },
+      { 'n', ']e', '<cmd>lua vim.diagnostic.goto_next()<CR>' },
+      { 'n', '<leader>ll', '<cmd>lua vim.diagnostic.setloclist()<CR>' },
+      { 'n', '<leader>=', '<cmd>lua vim.lsp.buf.format()<CR>' },
+      { 'v', '<leader>=', '<cmd>lua vim.lsp.buf.format()<CR>' },
     }
     for _, map in ipairs(keymaps) do
       -- use <unique> to avoid overriding lspsaga keymaps
-      vim.cmd(string.format('silent! nnoremap <buffer> <silent> <unique> %s %s',
+      vim.cmd(string.format('silent! %snoremap <buffer> <silent> <unique> %s %s',
             unpack(map)))
     end
   end
