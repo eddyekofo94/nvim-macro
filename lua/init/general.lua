1703ac1c48cd8b9d289d214f3eae9299fc7e93cc
bekaboo <18127878294@qq.com>
2022-12-04 22:07:07 -0600

    395: Mod: Simpler way to achieve automatic hlsearch.

--- lua/init/general.lua
+++ lua/init/general.lua
@@ -73,3 +73,11 @@ g.loaded_netrwSettings = 1
 
 -- colorscheme
 vim.cmd('colorscheme nvim-falcon')
+
+-- automatically set and unset hlsearch
+vim.on_key(function(char)
+  if vim.fn.mode() == 'n' then
+    vim.opt.hlsearch = vim.tbl_contains({ 'n', 'N', '*', '#', '?', '/' },
+      vim.fn.keytrans(char))
+  end
+end, vim.api.nvim_create_namespace('auto_hlsearch'))
