05487801fe3666b61a84a986f2b518519315610c
bekaboo <18127878294@qq.com>
2022-12-09 02:10:08 -0600

    402: Fix: typo in vimtex setup.

--- lua/modules/markup/init.lua
+++ lua/modules/markup/init.lua
@@ -5,7 +5,7 @@ M['vimtex'] = {
   'lervag/vimtex',
   ft = { 'tex', 'markdown' },
   setup = function()
-    vim.g.vimtex_mappings_ebabled = 0
+    vim.g.vimtex_mappings_enabled = 0
     vim.g.vimtex_motion_enabled = 0
     vim.g.vimtex_syntax_conceal = { math_bounds = 0 }
   end,
