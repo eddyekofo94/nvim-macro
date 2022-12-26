daf3702e4b0c9b91fed60ef4c94c994f54368a18
bekaboo <18127878294@qq.com>
2022-12-25 21:03:18 -0600

    468: Fix: markdown ftplugin: 'MarkdownToPDF' does not run okular asynchronously.

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,24 +1,25 @@
 local handler_md2pdf = nil
+local handler_viewer = nil
 
-vim.api.nvim_buf_create_user_command(
-  0, 'MarkdownToPDF', function ()
-    local fname_md = vim.fn.expand('%')
-    local fname_pdf = string.format('%s.pdf', vim.fn.expand('%:r'))
-    vim.cmd('write')
-    handler_md2pdf = vim.loop.spawn(
-      'md2pdf',   -- under ~/.scripts, should be append to $PATH
-      { args = { fname_md } },
-      function(code, _)
-        if handler_md2pdf then
-          handler_md2pdf:close()
+vim.api.nvim_buf_create_user_command(0, 'MarkdownToPDF', function()
+  local fname_md = vim.fn.expand('%')
+  local fname_pdf = string.format('%s.pdf', vim.fn.expand('%:r'))
+  vim.cmd('write')
+  handler_md2pdf = vim.loop.spawn('md2pdf', { args = { fname_md } }, function(code_md2pdf, _)
+    if handler_md2pdf then
+      handler_md2pdf:close()
+    end
+    if code_md2pdf ~= 0 then
+      vim.notify(string.format('md2pdf failed with code %d', code_md2pdf), vim.log.levels.ERROR)
+    else
+      handler_viewer = vim.loop.spawn('okular', { args = { fname_pdf } }, function(code_viewer, _)
+        if handler_viewer then
+          handler_viewer:close()
         end
-        if code ~= 0 then
-          vim.notify(string.format('md2pdf failed with code %d', code))
-        else
-          os.execute(string.format('okular %s', fname_pdf))
+        if code_viewer ~= 0 then
+          vim.notify(string.format('okular failed with code %d', code_viewer), vim.log.levels.ERROR)
         end
-      end
-    )
-  end,
-  { desc = 'Convert markdown to pdf' }
-)
+      end)
+    end
+  end)
+end, { desc = 'Convert markdown to pdf' })
