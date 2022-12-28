7307cf68ad9c2c0b61850a3b3c8409a4ae0ca664
bekaboo <18127878294@qq.com>
2022-12-28 00:45:28 -0600

    474: Mod: add key to toggle hlsearch.

--- plugin/auto_hlsearch.lua
+++ plugin/auto_hlsearch.lua
@@ -7,8 +7,9 @@ vim.on_key(function(char)
   end
 end, vim.api.nvim_create_namespace('auto_hlsearch'))
 
+vim.keymap.set('n', '\\', '<Cmd>set hlsearch!<CR>', { noremap = true })
+
 vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
   pattern = '*',
   command = 'set nohlsearch'
 })
-
