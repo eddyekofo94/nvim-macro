62845e5eb2a8faddef5fc946b6765b690478c65e
bekaboo <18127878294@qq.com>
2023-02-03 03:24:37 -0600

    686: Mod: decrease telescope's width and height.

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -27,8 +27,18 @@ telescope.setup({
     selection_caret = '→ ',
     borderchars = require('utils.static').borders.empty,
     layout_config = {
-      horizontal = { prompt_position = 'top', preview_width = 0.5 },
-      vertical = { prompt_position = 'top', preview_width = 0.5 }
+      horizontal = {
+        prompt_position = 'top',
+        width = 0.8,
+        height = 0.8,
+        preview_width = 0.5,
+      },
+      vertical = {
+        prompt_position = 'top',
+        width = 0.8,
+        height = 0.8,
+        preview_width = 0.5,
+      },
     },
     sorting_strategy = 'ascending',
     file_ignore_patterns = { '.git/', '%.pdf', '%.o', '%.zip' },
