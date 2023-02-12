5531f85fd6efe08df09410baafec975b61da91ba
bekaboo <18127878294@qq.com>
2023-02-12 11:41:48 -0600

    721: Mod: auto_hlsearch: disable hlsearch on any keystock other than 'n', 'N', '*', '#', '?', and '/'.

--- plugin/auto_hlsearch.lua
+++ plugin/auto_hlsearch.lua
@@ -1,9 +1,9 @@
 -- automatically set and unset hlsearch
 vim.on_key(function(char)
-  if vim.fn.mode() == 'n' and
-    vim.tbl_contains({ 'n', 'N', '*', '#', '?', '/' },
-    vim.fn.keytrans(char)) then
-      vim.opt.hlsearch = true
+  if vim.fn.mode() == 'n' then
+    vim.opt.hlsearch = vim.tbl_contains({
+      'n', 'N', '*', '#', '?', '/'
+    }, vim.fn.keytrans(char))
   end
 end, vim.api.nvim_create_namespace('auto_hlsearch'))
 
