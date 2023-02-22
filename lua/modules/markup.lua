19ad5e0ba46d19ffdcb4ec708b8a4a68e7fca3eb
bekaboo <18127878294@qq.com>
2023-02-21 20:02:21 -0600

    744: Mod: disable vim-markdown's keymap: 'ge'.

--- lua/modules/markup.lua
+++ lua/modules/markup.lua
@@ -16,6 +16,7 @@ return {
       vim.g.vim_markdown_conceal_code_blocks = 0
       vim.g.vim_markdown_auto_insert_bullets = 0
       vim.g.vim_markdown_new_list_item_indent = 0
+      vim.keymap.set('n', '<Plug>', '<Plug>Markdown_EditUrlUnderCursor')
     end,
   },
 
