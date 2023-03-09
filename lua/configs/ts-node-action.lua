0b4a3096d745af225a634f3c99dc2cadcfc99a95
bekaboo <18127878294@qq.com>
2023-03-09 14:58:27 -0600

    773: Fix: use pcall for ts-node-actions.

--- lua/configs/ts-node-action.lua
+++ lua/configs/ts-node-action.lua
@@ -1,4 +1,4 @@
 local action = require('ts-node-action')
 vim.keymap.set('n', '<leader><leader>',
-  action.node_action, { noremap = true, silent = true })
+  function() pcall(action.node_action) end, { noremap = true, silent = true })
 action.setup({})
