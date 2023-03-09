2040e7d1bbc72282810eb734939707759090130a
bekaboo <18127878294@qq.com>
2023-03-09 14:58:42 -0600

    774: Mod: ignore latex fontspec warnings.

--- lua/configs/vimtex.lua
+++ lua/configs/vimtex.lua
@@ -3,6 +3,7 @@ vim.g.vimtex_imaps_enabled = 0
 vim.g.vimtex_mappings_prefix = '<LocalLeader>t'
 vim.g.vimtex_quickfix_ignore_filters = {
   [[Font shape `.*' undefined]],
+  [[Package fontspec Warning]],
 }
 
 vim.api.nvim_create_autocmd('FileType', {
