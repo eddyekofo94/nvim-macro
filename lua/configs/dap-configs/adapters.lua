45e050ae68e55a08dbb85875946b5c6ae5cc675d
bekaboo <18127878294@qq.com>
2023-03-19 01:38:45 -0500

    796: Feat: add adapter configurations for bashdb.

--- lua/configs/dap-configs/adapters.lua
+++ lua/configs/dap-configs/adapters.lua
@@ -4,4 +4,9 @@ return {
     command = 'python',
     args = { '-m', 'debugpy.adapter' },
   },
+  bashdb = {
+    type = 'executable',
+    command = vim.fn.stdpath('data') .. '/mason/packages/bash-debug-adapter/bash-debug-adapter',
+    name = 'bashdb',
+  },
 }
--- lua/configs/dap-configs/configurations.lua
+++ lua/configs/dap-configs/configurations.lua
@@ -15,4 +15,25 @@ return {
       end
     end,
   } },
+  sh = {
+    {
+      type = 'bashdb',
+      request = 'launch',
+      name = 'Launch file',
+      showDebugOutput = true,
+      pathBashdb = vim.fn.stdpath('data') .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb',
+      pathBashdbLib = vim.fn.stdpath('data') .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir',
+      trace = true,
+      file = '${file}',
+      program = '${file}',
+      cwd = '${workspaceFolder}',
+      pathCat = 'cat',
+      pathBash = '/bin/bash',
+      pathMkfifo = 'mkfifo',
+      pathPkill = 'pkill',
+      args = {},
+      env = {},
+      terminalKind = 'integrated',
+    },
+  },
 }
