b93fee03ca6fddef87677508c60db26b807c0d10
bekaboo <18127878294@qq.com>
2023-03-19 02:27:43 -0500

    799: Mod: do not cancel hlsearch automatically.

--- plugin/auto_hlsearch.lua
+++ plugin/auto_hlsearch.lua
@@ -1,9 +1,19 @@
 -- automatically set and unset hlsearch
+
+local searchkeys = {
+  ['n'] = true,
+  ['N'] = true,
+  ['*'] = true,
+  ['#'] = true,
+  ['?'] = true,
+  ['/'] = true,
+}
+
 vim.on_key(function(char)
   if vim.fn.mode() == 'n' then
-    vim.opt.hlsearch = vim.tbl_contains({
-      'n', 'N', '*', '#', '?', '/'
-    }, vim.fn.keytrans(char))
+    if searchkeys[vim.fn.keytrans(char)] then
+      vim.opt.hlsearch = true
+    end
   end
 end, vim.api.nvim_create_namespace('auto_hlsearch'))
 
