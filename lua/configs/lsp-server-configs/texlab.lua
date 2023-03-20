07f5d8f5704dd56a4405f42ed415d15926abd2aa
bekaboo <18127878294@qq.com>
2023-03-19 23:46:03 -0500

    807: Mod: update texlab's config.

--- lua/configs/lsp-server-configs/texlab.lua
+++ lua/configs/lsp-server-configs/texlab.lua
@@ -2,38 +2,26 @@ local default_config = require('configs.lsp-server-configs.shared.default')
 
 local function on_attach(client, bufnr)
   -- Enable completion triggered by <c-x><c-o>
-  local buf_set_option = function(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Use an on_attach function to only map the following keys
-  -- after the language server attaches to the current buffer
-  local keymaps = {
-    { 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>' },
-    { 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>' },
-    { 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>' },
-    { 'n', '<Leader>li', '<cmd>lua vim.lsp.buf.implementation()<CR>' },
-    { 'n', '<Leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>' },
-    { 'n', '<Leader>wd', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>' },
-    { 'n', '<Leader>wl', '<cmd>lua vim.pretty_print(vim.lsp.buf.list_workspace_folders())<CR>' },
-    { 'n', '<leader>td', '<cmd>lua vim.lsp.buf.type_definition()<CR>' },
-    { 'n', '<Leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>' },
-    { 'n', '<Leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>' },
-    { 'n', '<Leader>R', '<cmd>lua vim.lsp.buf.references()<CR>' },
-    { 'n', '<Leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>' },
-    { 'n', '[E', '<cmd>lua vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })<CR>' },
-    { 'n', ']E', '<cmd>lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })<CR>' },
-    { 'n', '[e', '<cmd>lua vim.diagnostic.goto_prev()<CR>' },
-    { 'n', ']e', '<cmd>lua vim.diagnostic.goto_next()<CR>' },
-    { 'n', '<leader>ll', '<cmd>lua vim.diagnostic.setloclist()<CR>' },
-    -- User VimTeX's formatting instead
-    -- { 'n', '==', '<cmd>lua vim.lsp.buf.format()<CR>' },
-    -- { 'x', '=', '<cmd>lua vim.lsp.buf.format()<CR>' },
-  }
-  for _, map in ipairs(keymaps) do
-    -- use <unique> to avoid overriding telescope keymaps
-    vim.cmd(string.format('silent! %snoremap <buffer> <silent> <unique> %s %s',
-          unpack(map)))
-  end
+  vim.keymap.set('n', 'gd',         vim.lsp.buf.definition,                                                                { buffer = true })
+  vim.keymap.set('n', 'gD',         vim.lsp.buf.type_definition,                                                           { buffer = true })
+  vim.keymap.set('n', 'K',          vim.lsp.buf.hover,                                                                     { buffer = true })
+  vim.keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder,                                                      { buffer = true })
+  vim.keymap.set('n', '<Leader>wd', vim.lsp.buf.remove_workspace_folder,                                                   { buffer = true })
+  vim.keymap.set('n', '<Leader>wl', function() vim.print(vim.lsp.buf.list_workspace_folders()) end,                        { buffer = true })
+  vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action,                                                               { buffer = true })
+  vim.keymap.set('n', '<Leader>r',  vim.lsp.buf.rename,                                                                    { buffer = true })
+  vim.keymap.set('n', '<Leader>R',  vim.lsp.buf.references,                                                                { buffer = true })
+  vim.keymap.set('n', '<Leader>e',  vim.diagnostic.open_float,                                                             { buffer = true })
+  vim.keymap.set('n', '<leader>E',  vim.diagnostic.setloclist,                                                             { buffer = true })
+  vim.keymap.set('n', '[e',         vim.diagnostic.goto_prev,                                                              { buffer = true })
+  vim.keymap.set('n', ']e',         vim.diagnostic.goto_next,                                                              { buffer = true })
+  vim.keymap.set('n', '[E',         function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR }) end, { buffer = true })
+  vim.keymap.set('n', ']E',         function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR }) end, { buffer = true })
+  vim.keymap.set('n', '[W',         function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN }) end,  { buffer = true })
+  vim.keymap.set('n', ']W',         function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN }) end,  { buffer = true })
 
   -- integration with nvim-navic
   if client and client.server_capabilities
