d21b0ee88bfc114089d85fdee2f6ce0cd4a975e5
bekaboo <18127878294@qq.com>
2023-03-20 01:16:27 -0500

    812: Fix/Cleanup: not installing lsp servers automatically.

--- lua/configs/mason-lspconfig.lua
+++ lua/configs/mason-lspconfig.lua
@@ -1,5 +1,4 @@
 local lsp_servers = require('utils.static').langs:list('lsp_server')
-lsp_servers = table.insert(lsp_servers, 'zk')
 
 require('mason-lspconfig').setup({
   ensure_installed = lsp_servers,
--- lua/configs/zk-nvim.lua
+++ lua/configs/zk-nvim.lua
@@ -1,12 +1,38 @@
-vim.env.ZK_NOTEBOOK_DIR= os.getenv('HOME') .. '/School/Notes'
+vim.env.ZK_NOTEBOOK_DIR = os.getenv('HOME') .. '/School/Notes'
+local zk = require('zk')
+local lsp_server_config = require('configs.lsp-server-configs.shared.default')
 
-require('zk').setup({
+local zk_config = {
   picker = 'telescope',
   lsp = {
     config = {
       cmd = { vim.fn.stdpath('data') .. '/mason/bin/zk', 'lsp' },
       name = 'zk',
-      on_attach = require('configs.lsp-server-configs.shared.default').on_attach,
-    }
-  }
-})
+      on_attach = lsp_server_config.on_attach,
+    },
+  },
+}
+
+-- Setup zk if installed locally
+if vim.loop.fs_stat(zk_config.lsp.config.cmd[1]) then
+  zk.setup(zk_config)
+  return true
+end
+
+-- zk is missing, check if zk is installed globally
+if vim.fn.executable('zk') == 1 then
+  zk_config.lsp.config.cmd = { 'zk', 'lsp' }
+  zk.setup(zk_config)
+  return true
+end
+
+-- Try to install zk using mason.nvim
+local mason_api_ok, mason_api = pcall(require, 'mason.api.command')
+if mason_api_ok then
+  mason_api.MasonInstall({ 'zk' })
+  zk.setup(zk_config)
+  return true
+end
+
+vim.notify('[zk] zk executable not found', vim.log.levels.WARN)
+return false
