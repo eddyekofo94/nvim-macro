f7c33fb476688d7cfaf49a3731b4e744aa037585
bekaboo <18127878294@qq.com>
2023-03-21 03:05:32 -0500

    827: Feat: markdown ft options: set textwidth dynamically.

--- ftplugin/markdown/options.lua
+++ ftplugin/markdown/options.lua
@@ -1,1 +1,21 @@
-vim.opt.tw = 79
+vim.api.nvim_create_augroup('MarkdownOpts', { clear = true })
+vim.api.nvim_create_autocmd({
+  'InsertEnter',
+  'CursorMovedI',
+  'TextChangedI',
+}, {
+  pattern = '*.md',
+  group = 'MarkdownOpts',
+  callback = function()
+    local line = vim.api.nvim_get_current_line()
+    if
+      line:match('^%s*|') -- table
+      or line:match('^#') -- heading
+      or vim.api.nvim_eval('vimtex#syntax#in_mathzone()') == 1
+    then
+      vim.bo.textwidth = 0
+      return
+    end
+    vim.bo.textwidth = 79
+  end,
+})
