8e443604c82d9b2d99df2fd15f28e33746178b7c
bekaboo <18127878294@qq.com>
2023-03-21 03:04:44 -0500

    826: Feat: vim-table-mode config: enter table mode automatically.

--- lua/configs/vim-table-mode.lua
+++ lua/configs/vim-table-mode.lua
@@ -1,6 +1,30 @@
-vim.api.nvim_create_augroup('TableModSetMarkdownCorner', { clear = true })
+vim.api.nvim_create_augroup('TableModeSetTableCorner', { clear = true })
 vim.api.nvim_create_autocmd('Filetype', {
   pattern = 'markdown',
-  group = 'TableModSetMarkdownCorner',
+  group = 'TableModeSetTableCorner',
   command = 'let b:table_mode_corner = "|"',
+})
+
+vim.api.nvim_create_augroup('TableModeAutoEnable', { clear = true })
+vim.api.nvim_create_autocmd({
+  'CursorMoved',
+  'CursorMovedI',
+  'TextChanged',
+  'TextChangedI',
+  'BufWinEnter',
+}, {
+  pattern = { '*.txt', '*.md' },
+  group = 'TableModeAutoEnable',
+  callback = function()
+    local line = vim.api.nvim_get_current_line()
+    if line:match('^%s*|') then
+      if not vim.g.tablemode_enabled then
+        vim.cmd('TableModeEnable')
+        vim.g.tablemode_enabled = true
+      end
+    elseif vim.g.tablemode_enabled then
+      vim.cmd('TableModeDisable')
+      vim.g.tablemode_enabled = false
+    end
+  end,
 })
