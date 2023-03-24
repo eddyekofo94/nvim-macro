a9f47dd3eba834719aeed9ccd7a5703f3757b441
bekaboo <18127878294@qq.com>
2023-03-24 15:11:28 -0500

    869: Add: insert mode mappings for prompt buffers.

--- lua/init/autocmds.lua
+++ lua/init/autocmds.lua
@@ -99,6 +99,60 @@ local autocmds = {
       once = true,
     },
   },
+
+  -- Set window-navigation keymaps for prompt buffers
+  {
+    { 'BufEnter' },
+    {
+      group = 'PromptKeymaps',
+      callback = function(tbl)
+        if vim.bo[tbl.buf].buftype == 'prompt' then
+          vim.keymap.set('i', '<M-W>',      '<C-w>W',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-H>',      '<C-w>H',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-J>',      '<C-w>J',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-K>',      '<C-w>K',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-L>',      '<C-w>L',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-=>',      '<C-w>=',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-->',      '<C-w>-',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-+>',      '<C-w>+',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-_>',      '<C-w>_',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-|>',      '<C-w>|',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-<>',      '<C-w><',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M->>',      '<C-w>>',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-p>',      '<C-w>p',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-r>',      '<C-w>r',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-v>',      '<C-w>v',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-s>',      '<C-w>s',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-x>',      '<C-w>x',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-z>',      '<C-w>z',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-c>',      '<C-w>c',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-n>',      '<C-w>n',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-o>',      '<C-w>o',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-t>',      '<C-w>t',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-T>',      '<C-w>T',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-]>',      '<C-w>]',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-^>',      '<C-w>^',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-b>',      '<C-w>b',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-d>',      '<C-w>d',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-f>',      '<C-w>f',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-}>',      '<C-w>}',      { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-g>]',     '<C-w>g]',     { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-g>}',     '<C-w>g}',     { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-g>f',     '<C-w>gf',     { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-g>F',     '<C-w>gF',     { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-g>t',     '<C-w>gt',     { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-g>T',     '<C-w>gT',     { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-w>',      '<C-w><C-w>',  { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-h>',      '<C-w><C-h>',  { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-j>',      '<C-w><C-j>',  { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-k>',      '<C-w><C-k>',  { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-l>',      '<C-w><C-l>',  { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-g><M-]>', '<C-w>g<C-]>', { buffer = tbl.buf })
+          vim.keymap.set('i', '<M-g><Tab>', '<C-w>g<Tab>', { buffer = tbl.buf })
+        end
+      end
+    }
+  }
 }
 
 set_autocmds(autocmds)
