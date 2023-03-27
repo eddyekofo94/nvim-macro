bcbc286331b6da5c435a852803c38cf8b87c02c2
bekaboo <18127878294@qq.com>
2023-03-27 12:02:45 -0500

    879: Fix: auto_colorcolumn: handle settings from modelines.

--- plugin/auto_colorcolumn.lua
+++ plugin/auto_colorcolumn.lua
@@ -208,6 +208,19 @@ local function show_colorcolumn()
   end
 end
 
+---Hide the colorcolumn
+local function init()
+  local wins = vim.api.nvim_list_wins()
+  for _, win in ipairs(wins) do
+    vim.w[win].cc = vim.wo[win].cc
+  end
+  vim.g.cc = vim.go.cc
+  for _, win in ipairs(wins) do
+    vim.wo[win].cc = ''
+  end
+  vim.go.cc = ''
+  store.colorcol_bg = get_hl('ColorColumn', 'background')
+end
 
 -- colorcolumn is a window-local option, with some special rules:
 -- 1. When a window is created, it inherits the value of the previous window or
@@ -218,129 +231,137 @@ end
 -- 3. Once the window-local cc is set, it's not changed by the global option
 --    or inheritance, it will only change when a different buffer is displayed
 --    or the option is set explicitly (via set or setlocal)
-
--- Save original cc settings
-vim.api.nvim_create_augroup('AutoColorColumn', { clear = true })
-vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-  group = 'AutoColorColumn',
-  callback = function()
-    vim.g.cc = vim.go.cc
-    vim.w.cc = vim.wo.cc
-    vim.opt.cc = ''
-  end,
-  once = true,
-})
-
--- Save previous window cc settings
-vim.api.nvim_create_autocmd({ 'WinLeave' }, {
-  group = 'AutoColorColumn',
-  callback = function()
-    store.previous_cc = vim.w.cc
-    vim.wo.cc = ''
-  end,
-})
-
--- Broadcast previous window or global cc settings to new windows
-vim.api.nvim_create_autocmd({ 'WinNew' }, {
-  group = 'AutoColorColumn',
-  callback = function()
-    vim.w.cc = str_fallback(store.previous_cc, vim.g.cc)
-  end,
-})
-
--- Handle cc settings from ftplugins
--- Detect cc changes in a quite hacky way, because OptionSet autocmd is not
--- triggered when cc is set in a ftplugin
--- Quirk: these two commands are not the same in a ftplugin:
---     setlocal cc=80 " vimscript
---     vim.wo.cc = 80 -- lua
--- The former (vimscript) will set the 'buffer-local' cc, i.e. it will set cc
--- for current window BUT will be reset for a different buffer displayed in
--- the same window.
--- The latter (lua) will set the 'window-local' cc, i.e. it will set cc for
--- current window and will NOT be reset for a different buffer displayed in
--- the same window.
--- Currently there is no way to tell which one is used in a ftplugin, since I
--- prefer the vimscript way, I simulate its behavior here when a change is
--- detected for cc in current window.
-vim.api.nvim_create_autocmd({ 'BufReadPre' }, {
-  group = 'AutoColorColumn',
-  callback = function()
-    vim.b._cc = vim.wo.cc
-    vim.g._cc = vim.go.cc
-  end,
-})
-vim.api.nvim_create_autocmd({ 'FileType' }, {
-  group = 'AutoColorColumn',
-  callback = function()
-    -- If cc changes between BufReadPre and FileType, it is an ftplugin
-    -- that sets cc, so we accept it as a 'buffer-local' (phony) cc setting
-    -- Notice that we will do nothing if vim.b._cc is nil, which means the
-    -- buffer is not the same buffer that triggers BufReadPre
-    if vim.b._cc and vim.wo.cc ~= vim.b._cc then
-      vim.b.cc = vim.wo.cc
-      if vim.go.cc ~= vim.g._cc then
-        vim.g.cc = vim.go.cc
-      end
-    end
-  end,
-})
-
--- Broadcast buffer or global cc settings
--- when a different buffer is displayed in current window
-vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-  group = 'AutoColorColumn',
-  callback = function()
-    vim.b.cc = str_fallback(vim.b.cc, vim.g.cc)
-    vim.w.cc = str_fallback(vim.b.cc, vim.g.cc)
-    local mode = vim.fn.mode()
-    if not vim.startswith(mode, 'i') or not vim.startswith(mode, 'R') then
+local function make_autocmds()
+  -- Save original cc settings
+  vim.api.nvim_create_augroup('AutoColorColumn', { clear = true })
+  -- Save previous window cc settings
+  vim.api.nvim_create_autocmd({ 'WinLeave' }, {
+    group = 'AutoColorColumn',
+    callback = function()
+      store.previous_cc = vim.w.cc
       vim.wo.cc = ''
-    end
-  end,
-})
+    end,
+  })
 
--- Save cc settings for each window
-vim.api.nvim_create_autocmd({ 'WinEnter' }, {
-  group = 'AutoColorColumn',
-  callback = function()
-    vim.w.cc = str_fallback(vim.w.cc, vim.wo.cc)
-  end,
-})
+  -- Broadcast previous window or global cc settings to new windows
+  vim.api.nvim_create_autocmd({ 'WinNew' }, {
+    group = 'AutoColorColumn',
+    callback = function()
+      vim.w.cc = str_fallback(store.previous_cc, vim.g.cc)
+    end,
+  })
 
--- Update cc settings on option change
-vim.api.nvim_create_autocmd({ 'OptionSet' }, {
-  pattern = 'colorcolumn',
-  group = 'AutoColorColumn',
-  callback = function()
-    if vim.v.option_type == 'global' then
-      vim.g.cc = vim.go.cc
-      vim.w.cc = vim.go.cc
-      vim.b.cc = vim.go.cc
-    elseif vim.v.option_type == 'local' then
-      vim.w.cc = vim.wo.cc
-      vim.b.cc = vim.wo.cc
-    end
-    vim.go.cc = ''
-    vim.wo.cc = ''
-  end,
-})
+  -- Handle cc settings from ftplugins
+  -- Detect cc changes in a quite hacky way, because OptionSet autocmd is not
+  -- triggered when cc is set in a ftplugin
+  -- Quirk: these two commands are not the same in a ftplugin:
+  --     setlocal cc=80 " vimscript
+  --     vim.wo.cc = 80 -- lua
+  -- The former (vimscript) will set the 'buffer-local' cc, i.e. it will set cc
+  -- for current window BUT will be reset for a different buffer displayed in
+  -- the same window.
+  -- The latter (lua) will set the 'window-local' cc, i.e. it will set cc for
+  -- current window and will NOT be reset for a different buffer displayed in
+  -- the same window.
+  -- Currently there is no way to tell which one is used in a ftplugin, since I
+  -- prefer the vimscript way, I simulate its behavior here when a change is
+  -- detected for cc in current window.
+  vim.api.nvim_create_autocmd({ 'BufReadPre' }, {
+    group = 'AutoColorColumn',
+    callback = function()
+      vim.b._cc = vim.wo.cc
+      vim.g._cc = vim.go.cc
+    end,
+  })
+  vim.api.nvim_create_autocmd({ 'FileType' }, {
+    group = 'AutoColorColumn',
+    callback = function()
+      -- If cc changes between BufReadPre and FileType, it is an ftplugin
+      -- that sets cc, so we accept it as a 'buffer-local' (phony) cc setting
+      -- Notice that we will do nothing if vim.b._cc is nil, which means the
+      -- buffer is not the same buffer that triggers BufReadPre
+      if vim.b._cc and vim.wo.cc ~= vim.b._cc then
+        vim.b.cc = vim.wo.cc
+        if vim.go.cc ~= vim.g._cc then
+          vim.g.cc = vim.go.cc
+        end
+      end
+    end,
+  })
 
--- Save Colorcolum background color
-vim.api.nvim_create_autocmd({ 'UIEnter', 'ColorScheme' }, {
-  group = 'AutoColorColumn',
-  callback = function()
-    store.colorcol_bg = get_hl('ColorColumn', 'background')
-  end,
-})
+  -- On entering a buffer, check and set vim.b.cc and vim.w.cc in the
+  -- following order:
+  -- 1. If vim.wo.cc is non empty, then it is set from a modeline, use it.
+  --    Notice that this is after the 'FileType' event, which applies ftplugin
+  --    settings
+  -- 2. If vim.b.cc if non empty, it is set previously by broadcasting or an
+  --    ftplugin, use it
+  -- 3. Else use vim.g.cc
+  -- We want to unset vim.wo.cc on leaving a buffer, so that vim.wo.cc reflects
+  -- changes from modelines
+  vim.api.nvim_create_autocmd({ 'BufLeave' }, {
+    group = 'AutoColorColumn',
+    callback = function()
+      vim.wo.cc = ''
+    end,
+  })
+  vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+    group = 'AutoColorColumn',
+    callback = function()
+      vim.b.cc = str_fallback(vim.wo.cc, vim.b.cc, vim.g.cc)
+      vim.w.cc = str_fallback(vim.wo.cc, vim.b.cc, vim.g.cc)
+      local mode = vim.fn.mode()
+      if not vim.startswith(mode, 'i') or not vim.startswith(mode, 'R') then
+        vim.wo.cc = ''
+      end
+    end,
+  })
 
--- Show colored column
-vim.api.nvim_create_autocmd(
-  { 'ModeChanged', 'TextChangedI', 'CursorMovedI', 'ColorScheme' },
-  { group = 'AutoColorColumn', callback = show_colorcolumn }
-)
-vim.api.nvim_create_autocmd({ 'OptionSet' }, {
-  pattern = { 'colorcolumn', 'textwidth' },
-  group = 'AutoColorColumn',
-  callback = show_colorcolumn,
-})
+  -- Save cc settings for each window
+  vim.api.nvim_create_autocmd({ 'WinEnter' }, {
+    group = 'AutoColorColumn',
+    callback = function()
+      vim.w.cc = str_fallback(vim.w.cc, vim.wo.cc)
+    end,
+  })
+
+  -- Update cc settings on option change
+  vim.api.nvim_create_autocmd({ 'OptionSet' }, {
+    pattern = 'colorcolumn',
+    group = 'AutoColorColumn',
+    callback = function()
+      if vim.v.option_type == 'global' then
+        vim.g.cc = vim.go.cc
+        vim.w.cc = vim.go.cc
+        vim.b.cc = vim.go.cc
+      elseif vim.v.option_type == 'local' then
+        vim.w.cc = vim.wo.cc
+        vim.b.cc = vim.wo.cc
+      end
+      vim.go.cc = ''
+      vim.wo.cc = ''
+    end,
+  })
+
+  -- Save Colorcolum background color
+  vim.api.nvim_create_autocmd({ 'UIEnter', 'ColorScheme' }, {
+    group = 'AutoColorColumn',
+    callback = function()
+      store.colorcol_bg = get_hl('ColorColumn', 'background')
+    end,
+  })
+
+  -- Show colored column
+  vim.api.nvim_create_autocmd(
+    { 'ModeChanged', 'TextChangedI', 'CursorMovedI', 'ColorScheme' },
+    { group = 'AutoColorColumn', callback = show_colorcolumn }
+  )
+  vim.api.nvim_create_autocmd({ 'OptionSet' }, {
+    pattern = { 'colorcolumn', 'textwidth' },
+    group = 'AutoColorColumn',
+    callback = show_colorcolumn,
+  })
+end
+
+init()
+make_autocmds()
