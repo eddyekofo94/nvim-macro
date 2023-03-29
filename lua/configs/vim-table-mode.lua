def742376499d1a6441b9d5a8f72ea09f784e064
bekaboo <18127878294@qq.com>
2023-03-29 10:42:07 -0500

    891: Mod: change table-mode-auto-enable to table-mode-format-on-save (laggy)

--- lua/configs/vim-table-mode.lua
+++ lua/configs/vim-table-mode.lua
@@ -5,29 +5,13 @@ vim.api.nvim_create_autocmd('Filetype', {
   command = 'let b:table_mode_corner = "|"',
 })
 
-vim.api.nvim_create_augroup('TableModeAutoEnable', { clear = true })
-vim.api.nvim_create_autocmd({
-  'CursorMoved',
-  'CursorMovedI',
-  'TextChanged',
-  'TextChangedI',
-  'BufWinEnter',
-}, {
+vim.api.nvim_create_augroup('TableModeFormatOnSave', { clear = true })
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
   pattern = { '*.txt', '*.md' },
-  group = 'TableModeAutoEnable',
-  callback = function(tbl)
-    if vim.bo[tbl.buf].ft == 'help' then
-      return
-    end
-    local line = vim.api.nvim_get_current_line()
-    if line:match('^%s*|') then
-      if not vim.g.tablemode_enabled then
-        vim.cmd('TableModeEnable')
-        vim.g.tablemode_enabled = true
-      end
-    elseif vim.g.tablemode_enabled then
-      vim.cmd('TableModeDisable')
-      vim.g.tablemode_enabled = false
+  group = 'TableModeFormatOnSave',
+  callback = function()
+    if vim.api.nvim_get_current_line():match('^%s*|') then
+      vim.cmd('silent! TableModeRealign')
     end
   end,
 })
