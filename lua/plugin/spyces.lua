60bfdbaa4f967a146daf531ecb66cb9f711d22c4
bekaboo <18127878294@qq.com>
2023-04-01 01:03:01 -0500

    918: Fix: spyces.

--- lua/plugin/spyces.lua
+++ lua/plugin/spyces.lua
@@ -1,4 +1,5 @@
 local ids = {}
+local lcs = {}
 
 ---Make the highlight group for concealing
 local function makehl()
@@ -9,30 +10,39 @@ end
 ---Check if the listchars field is set
 ---if not, conceal it with a matchadd;
 ---if true, remove the concealment with matchdelete
----@param lcs table listchars
 ---@param field string listchars field
 ---@param pattern string vim regex pattern to match
----@param force_conceal boolean|nil force concealment
-local function _matchconceal(lcs, field, pattern, force_conceal)
-  if not lcs[field] or force_conceal then
-    ids[field] = vim.fn.matchadd('SpyceConceal', pattern, 0, -1)
-  elseif ids[field] then
-    vim.fn.matchdelete(ids[field])
-    ids[field] = nil
+---@param force_add boolean|nil
+local function _matchconceal(field, pattern, force_add)
+  local win = vim.api.nvim_get_current_win()
+  if force_add and not (ids[field] and ids[field][win]) or not lcs[field] then
+    ids[field] = ids[field] or {}
+    ids[field][win] = {
+      vim.fn.matchadd('SpyceConceal', pattern, 0, -1),
+      vim.api.nvim_get_current_win(),
+    }
+  elseif not force_add and lcs[field] and ids[field] and ids[field][win] then
+    vim.fn.matchdelete(ids[field][win])
+    ids[field][win] = nil
   end
 end
 
 ---Show spaces mixed with tabs, respect the original listchars settigns
 local function matchconceal()
-  local lcs = vim.opt.lcs:get()
-  if not vim.opt.list:get() or lcs.space then
+  lcs = vim.opt.lcs:get()
+  if not vim.opt.list:get() then
     return
   end
   vim.opt.lcs:append({ space = '·' })
-  _matchconceal(lcs, 'space', [[\S\@1<=\ \S\@=]], true)
-  _matchconceal(lcs, 'trail', [[\ \+$]])
-  _matchconceal(lcs, 'precedes', [[^\ \+]])
-  _matchconceal(lcs, 'multispace', [[\(^\ *\|\t\)\@<!\ \{2,}\(\ *$\|\t\)\@!]])
+  _matchconceal('space', [[\S\@1<=\ \S\@=]], true)
+  _matchconceal('trail', [[\ \+$]])
+  _matchconceal('lead', [[^\ \+]])
+  _matchconceal('multispace', [[\(^\ *\|\t\)\@<!\ \{2,}\(\ *$\|\t\)\@!]])
+end
+
+---Match concealment for all windows
+local function matchconceal_windo()
+  vim.cmd('windo lua require("plugin.spyces").matchconceal()')
 end
 
 ---Some autocmds to update highlight groups and change concealment
@@ -43,33 +53,31 @@ local function makehooks()
     group = 'CatchSpyces',
     callback = makehl,
   })
-  vim.api.nvim_create_autocmd('ModeChanged', {
+  vim.api.nvim_create_autocmd({ 'BufWinEnter', 'WinEnter' }, {
     group = 'CatchSpyces',
-    pattern = { '[vV\x16]*:*', '*:[vV\x16]*' },
-    callback = function()
-      if vim.fn.match(vim.v.event.new_mode, '[vV\x16]') ~= -1 then
-        if ids.space then
-          vim.fn.matchdelete(ids.space)
-        end
-      else
-        matchconceal()
-      end
-    end,
+    callback = matchconceal,
   })
   vim.api.nvim_create_autocmd('OptionSet', {
     group = 'CatchSpyces',
     pattern = { 'list', 'listchars' },
-    callback = matchconceal,
+    callback = function()
+      if vim.v.option_type == 'global' then
+        matchconceal_windo()
+      else
+        matchconceal()
+      end
+    end,
   })
 end
 
 ---Initialize the plugin
 local function init()
   makehl()
-  matchconceal()
+  matchconceal_windo()
   makehooks()
 end
 
 return {
   init = init,
+  matchconceal = matchconceal,
 }
