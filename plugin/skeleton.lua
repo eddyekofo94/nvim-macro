322911025378350235460caeb98cb66ca6397a25
bekaboo <18127878294@qq.com>
2023-03-31 23:22:49 -0500

    913: Remove: custom plugin 'skeleton'.

--- plugin/skeleton.lua
+++
@@ -1,9 +0,0 @@
-require('plugin.skeleton').setup({
-  rules = {
-    [''] = {
-      ['.*%.scripts/'] = function(fallback)
-        return fallback({ 'sh.skel', '.sh.skel' })
-      end
-    },
-  },
-})
