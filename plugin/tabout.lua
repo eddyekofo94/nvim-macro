f5f310cd804df566289a9b7ab9eabbfd6bf20de6
bekaboo <18127878294@qq.com>
2023-03-31 23:29:00 -0500

    916: Format: plugin/tabout.lua.

--- plugin/tabout.lua
+++ plugin/tabout.lua
@@ -1,2 +1,6 @@
-vim.keymap.set('i', '<Tab>', function() require('plugin.tabout').do_key('<Tab>') end, { noremap = true })
-vim.keymap.set('i', '<S-Tab>', function() require('plugin.tabout').do_key('<S-Tab>') end, { noremap = true })
+vim.keymap.set('i', '<Tab>', function()
+  require('plugin.tabout').do_key('<Tab>')
+end)
+vim.keymap.set('i', '<S-Tab>', function()
+  require('plugin.tabout').do_key('<S-Tab>')
+end)
