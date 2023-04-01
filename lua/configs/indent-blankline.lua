63d03cca0553279b355c13f090e7ee50f1e5788d
bekaboo <18127878294@qq.com>
2023-04-01 02:14:00 -0500

    919: Mod: indent-blankline config.

--- lua/configs/indent-blankline.lua
+++ lua/configs/indent-blankline.lua
@@ -1,10 +1,5 @@
-vim.opt.listchars = {
-  tab        = '→ ',
-  extends    = '…',
-  precedes   = '…',
-  nbsp       = '⌴',
-  trail      = '·',
-}
+local indent_blankline = require('indent_blankline')
+local indent_blankline_utils = require('indent_blankline.utils')
 
 -- Fix indent-blankline bug #489
 -- https://github.com/lukas-reineke/indent-blankline.nvim/issues/489
@@ -13,10 +8,54 @@ vim.api.nvim_create_autocmd('WinScrolled', {
   group = 'IndentBlankLineFix',
   callback = function()
     if vim.v.event.all.leftcol ~= 0 then
-      vim.cmd('silent! IndentBlanklineRefresh')
+      indent_blankline.refresh(false)
     end
   end,
 })
+
+---Check if indent-blankline is enabled for current buffer
+---@return boolean
+local function indent_line_is_enabled()
+  return indent_blankline_utils.is_indent_blankline_enabled(
+    vim.b.indent_blankline_enabled,
+    vim.g.indent_blankline_enabled,
+    indent_blankline_utils.get_variable('indent_blankline_disable_with_nolist'),
+    vim.opt.list:get(),
+    vim.bo.filetype,
+    indent_blankline_utils.get_variable('indent_blankline_filetype') or {},
+    indent_blankline_utils.get_variable('indent_blankline_filetype_exclude'),
+    vim.bo.buftype,
+    indent_blankline_utils.get_variable('indent_blankline_buftype_exclude')
+      or {},
+    indent_blankline_utils.get_variable('indent_blankline_bufname_exclude')
+      or {},
+    vim.fn['bufname']('')
+  )
+end
+
+---Disable indent-blankline temporarily
+local function indent_blankline_temp_disable()
+  if indent_line_is_enabled() then
+    vim.b.indent_blankline_enabled = false
+    vim.b.__indent_blankline_active = false
+    vim.b.__indent_blankline_temp_disabled = true
+    vim.api.nvim_buf_clear_namespace(
+      0,
+      vim.g.indent_blankline_namespace,
+      1,
+      -1
+    )
+  end
+end
+
+---Recover indent-blankline from temporary disabled state
+local function indent_blankline_recover()
+  if vim.b.__indent_blankline_temp_disabled then
+    vim.b.__indent_blankline_temp_disabled = false
+    vim.b.indent_blankline_enabled = true
+    indent_blankline.refresh(false)
+  end
+end
 
 -- Disable indent-blankline when in visual mode
 vim.api.nvim_create_autocmd('ModeChanged', {
@@ -24,9 +63,9 @@ vim.api.nvim_create_autocmd('ModeChanged', {
   pattern = { '[vV\x16]*:*', '*:[vV\x16]*' },
   callback = function()
     if vim.fn.match(vim.v.event.new_mode, '[vV\x16]') ~= -1 then
-      vim.cmd('silent! IndentBlanklineDisable')
+      indent_blankline_temp_disable()
     else
-      vim.cmd('silent! IndentBlanklineEnable')
+      indent_blankline_recover()
     end
   end,
 })
@@ -37,14 +76,14 @@ vim.api.nvim_create_autocmd('OptionSet', {
   group = 'IndentBlankLineFix',
   callback = function()
     if vim.wo.diff then
-      vim.cmd('silent! IndentBlanklineDisable')
+      indent_blankline_temp_disable()
     else
-      vim.cmd('silent! IndentBlanklineEnable')
+      indent_blankline_recover()
     end
   end,
 })
 
-require('indent_blankline').setup({
+indent_blankline.setup({
   char = '',
   char_highlight_list = {
     'IndentBlanklineIndent1',
@@ -55,5 +94,13 @@ require('indent_blankline').setup({
     'IndentBlanklineIndent2',
   },
   show_trailing_blankline_indent = true,
-  indent_blankline_use_treesitter = false,
+  use_treesitter = false,
+  max_indent_increase = 1,
+  filetype_exclude = {
+    '',
+    'man',
+    'help',
+    'markdown',
+    'checkhealth',
+  },
 })
