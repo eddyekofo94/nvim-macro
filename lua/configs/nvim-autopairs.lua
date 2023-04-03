29e6f69b1a301c7677bb3f7cedf9cf448e9a7bc8
bekaboo <18127878294@qq.com>
2023-04-03 17:00:41 -0500

    951: Fix: override nvim-autopairs' func 'is_in_quotes()'.
    Treat primes differently from single quotes in text files (&ft = '',
    'tex', 'markdown', 'text', 'gitcommit').

--- lua/configs/nvim-autopairs.lua
+++ lua/configs/nvim-autopairs.lua
@@ -1,6 +1,55 @@
 local npairs = require('nvim-autopairs')
 local Rule = require('nvim-autopairs.rule')
 local cond = require('nvim-autopairs.conds')
+local utils = require('nvim-autopairs.utils')
+
+-- Override the default is_in_quotes function to
+-- treat primes differently from quotes
+-- Oringal function
+local _is_in_quotes = utils.is_in_quotes
+
+---check cursor is inside a quote
+---@param line string
+---@param pos number  position in line
+---@param quote_type nil|string specify a quote
+---@return boolean
+---@diagnostic disable-next-line: duplicate-set-field
+function utils.is_in_quotes(line, pos, quote_type)
+  local text_fts = {
+    '',
+    'tex',
+    'help',
+    'text',
+    'markdown',
+    'gitcommit',
+  }
+  if not vim.tbl_contains(text_fts, vim.bo.ft) then
+    return _is_in_quotes(line, pos, quote_type)
+  end
+
+  local cur_idx = 0
+  local result = false
+  local last_char = quote_type or ''
+
+  while cur_idx < string.len(line) and cur_idx < pos do
+    cur_idx = cur_idx + 1
+    local char = line:sub(cur_idx, cur_idx)
+    local char_before = line:sub(cur_idx - 1, cur_idx - 1)
+    if result == true and char == last_char and char_before ~= '\\' then
+      result = false
+      last_char = quote_type or ''
+    elseif
+      result == false
+      and utils.is_quote(char)
+      and (not quote_type or char == quote_type)
+      and not char_before:match('%w')
+    then
+      last_char = quote_type or char
+      result = true
+    end
+  end
+  return result
+end
 
 npairs.setup({
   check_ts = true,
