8b80d76050e004b4018522ecf5029a7583cac7aa
bekaboo <18127878294@qq.com>
2023-04-04 00:06:42 -0500

    964: Format: dap-configs.configurations.

--- lua/configs/dap-configs/configurations.lua
+++ lua/configs/dap-configs/configurations.lua
@@ -1,28 +1,32 @@
 return {
-  python = { {
-    type = 'python',
-    request = 'launch',
-    name = 'Launch file',
-    program = '${file}',
-    pythonPath = function()
-      local cwd = vim.fn.getcwd()
-      if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
-        return cwd .. '/venv/bin/python'
-      elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
-        return cwd .. '/.venv/bin/python'
-      else
-        return '/usr/bin/python'
-      end
-    end,
-  } },
+  python = {
+    {
+      type = 'python',
+      request = 'launch',
+      name = 'Launch file',
+      program = '${file}',
+      pythonPath = function()
+        local cwd = vim.fn.getcwd()
+        if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+          return cwd .. '/venv/bin/python'
+        elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+          return cwd .. '/.venv/bin/python'
+        else
+          return '/usr/bin/python'
+        end
+      end,
+    },
+  },
   sh = {
     {
       type = 'bashdb',
       request = 'launch',
       name = 'Launch file',
       showDebugOutput = true,
-      pathBashdb = vim.fn.stdpath('data') .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb',
-      pathBashdbLib = vim.fn.stdpath('data') .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir',
+      pathBashdb = vim.fn.stdpath('data')
+        .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb',
+      pathBashdbLib = vim.fn.stdpath('data')
+        .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir',
       trace = true,
       file = '${file}',
       program = '${file}',
