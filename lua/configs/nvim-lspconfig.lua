9d36b9d295128de4bd064f35031e173c25dfe783
bekaboo <18127878294@qq.com>
2023-04-05 18:29:35 -0500

    981: Feat: set LSP floating window maximum height & width; cleanup.

--- lua/configs/nvim-lspconfig.lua
+++ lua/configs/nvim-lspconfig.lua
@@ -1,39 +1,61 @@
 local static = require('utils.static')
-local ensure_installed = static.langs:list('lsp_server')
-local icons = static.icons
 
-local function lspconfig_setui()
-  -- Customize LSP floating window border
-  local floating_preview_opts = { border = 'solid' }
-  local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+---Customize LSP floating window border
+local function lspconfig_floating_win()
+  local opts_override = {}
+
+  ---Set LSP floating window options
+  local function set_win_opts()
+    opts_override = {
+      border = 'solid',
+      max_width = math.ceil(vim.go.columns * 0.75),
+      max_height = math.ceil(vim.go.lines * 0.4),
+    }
+  end
+  set_win_opts()
+
+  -- Reset LSP floating window options on VimResized
+  vim.api.nvim_create_augroup('LspFloatingWinOpts', { clear = true })
+  vim.api.nvim_create_autocmd('VimResized', {
+    group = 'LspFloatingWinOpts',
+    callback = set_win_opts,
+  })
+
+  -- Hijack LSP floating window function to use custom options
+  local _open_floating_preview = vim.lsp.util.open_floating_preview
+  ---@diagnostic disable-next-line: duplicate-set-field
   function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-    opts = vim.tbl_deep_extend('force', opts, floating_preview_opts)
-    return orig_util_open_floating_preview(contents, syntax, opts, ...)
+    opts = vim.tbl_deep_extend('force', opts, opts_override)
+    return _open_floating_preview(contents, syntax, opts, ...)
   end
-  local diagnostic_opts = {}
-  -- LSP diagnostic signs
-  diagnostic_opts.signs = {
-    { 'DiagnosticSignError', { text = icons.DiagnosticSignError, texthl = 'DiagnosticSignError', numhl = 'DiagnosticSignError' } },
-    { 'DiagnosticSignWarn', { text = icons.DiagnosticSignWarn, texthl = 'DiagnosticSignWarn', numhl = 'DiagnosticSignWarn' } },
-    { 'DiagnosticSignInfo', { text = icons.DiagnosticSignInfo, texthl = 'DiagnosticSignInfo', numhl = 'DiagnosticSignInfo' } },
-    { 'DiagnosticSignHint', { text = icons.DiagnosticSignHint, texthl = 'DiagnosticSignHint', numhl = 'DiagnosticSignHint' } },
-    { 'DiagnosticSignOk', { text = icons.DiagnosticSignOk, texthl = 'DiagnosticSignOk', numhl = 'DiagnosticSignOk' } },
-  }
-  for _, sign_settings in ipairs(diagnostic_opts.signs) do
-    vim.fn.sign_define(unpack(sign_settings))
+end
+
+---Customize LSP diagnostic UI
+local function lspconfig_diagnostic()
+  local icons = static.icons
+  for _, severity in ipairs({ 'Error', 'Warn', 'Info', 'Hint' }) do
+    local sign_name = 'DiagnosticSign' .. severity
+    vim.fn.sign_define(sign_name, {
+      text = icons[sign_name],
+      texthl = sign_name,
+      numhl = sign_name,
+    })
   end
-  diagnostic_opts.handlers = {
-    -- Enable underline, use default values
-    underline = true,
-    -- Enable virtual text, override spacing to 4
-    virtual_text = {
-      spacing = 4,
-      prefix = ''
-    },
-  }
-  vim.lsp.handlers['textDocument/publishDiagnostics']
-      = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics,
-                     diagnostic_opts.handlers)
+
+  vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+      -- Enable underline, use default values
+      underline = true,
+      -- Enable virtual text, override spacing to 4
+      virtual_text = {
+        spacing = 4,
+        prefix = '',
+      },
+    })
+end
+
+---Customize LspInfo floating window
+local function lspconfig_info_win()
   -- setup LspInfo floating window border
   require('lspconfig.ui.windows').default_options.border = 'solid'
   -- reload LspInfo floating window on VimResized
@@ -50,9 +72,12 @@ local function lspconfig_setui()
   })
 end
 
+---Get LSP server default config from lspconfig
+---@param server_name string LSP server name
+---@return table config
 local function get_server_config(server_name)
-  local status, config
-    = pcall(require, 'configs.lsp-server-configs.' .. server_name)
+  local status, config =
+      pcall(require, 'configs.lsp-server-configs.' .. server_name)
   if not status then
     return require('configs.lsp-server-configs.shared.default')
   else
@@ -60,11 +85,14 @@ local function get_server_config(server_name)
   end
 end
 
+---Setup all LSP servers
 local function lsp_setup()
-  for _, server_name in pairs(ensure_installed) do
+  for _, server_name in pairs(static.langs:list('lsp_server')) do
     require('lspconfig')[server_name].setup(get_server_config(server_name))
   end
 end
 
-lspconfig_setui()
+lspconfig_floating_win()
+lspconfig_diagnostic()
+lspconfig_info_win()
 lsp_setup()
