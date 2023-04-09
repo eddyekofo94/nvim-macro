f3fe3f4ade1375b151aab55ded0b0c287becd571
bekaboo <18127878294@qq.com>
2023-04-09 17:19:59 -0500

    1005: Update: lockfile; follow mason-nvim-dap's breaking change.

--- lua/configs/mason-nvim-dap.lua
+++ lua/configs/mason-nvim-dap.lua
@@ -2,7 +2,7 @@ local mason_nvim_dap = require('mason-nvim-dap')
 mason_nvim_dap.setup({
   ensure_installed = require('utils.static').langs:list('dap'),
   automatic_setup = true,
-})
-mason_nvim_dap.setup_handlers({
-  python = function(_) end, -- suppress auto setup for python
+  handlers = {
+    python = function(_) end, -- suppress auto setup for python
+  }
 })
