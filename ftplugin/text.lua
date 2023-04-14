7add0604069cb3ba935ab0ddead101c8936d8f9d
bekaboo <18127878294@qq.com>
2023-04-14 13:24:26 -0500

    1025: Fix: remove 'j'/'k' keymaps in text ftplugin.

--- ftplugin/text.lua
+++ ftplugin/text.lua
@@ -1,7 +1,5 @@
 vim.wo.wrap = true
 vim.wo.linebreak = true
-vim.keymap.set({ 'n', 'x' }, 'j', 'gj', { noremap = true, buffer = true })
-vim.keymap.set({ 'n', 'x' }, 'k', 'gk', { noremap = true, buffer = true })
 vim.keymap.set({ 'n', 'x' }, '$', 'g$', { noremap = true, buffer = true })
 vim.keymap.set({ 'n', 'x' }, '0', 'g0', { noremap = true, buffer = true })
 vim.keymap.set({ 'n', 'x' }, '^', 'g^', { noremap = true, buffer = true })
