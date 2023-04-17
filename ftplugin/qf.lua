6509e26a29468421fb2310dfc61e4adaeb45ae0d
bekaboo <18127878294@qq.com>
2023-04-17 10:59:38 -0500

    1057: Feat: set keymaps for qflist.

--- ftplugin/qf.lua
+++ ftplugin/qf.lua
@@ -1,3 +1,9 @@
 vim.bo.buflisted = false
 vim.wo.nu        = false
 vim.wo.rnu       = false
+
+vim.keymap.set('n', '<Tab>', '<CR><C-w>p',  { buffer = true })
+vim.keymap.set('n', '<C-j>', 'j<CR><C-w>p', { buffer = true })
+vim.keymap.set('n', '<C-k>', 'k<CR><C-w>p', { buffer = true })
+vim.keymap.set('n', '}',     'j<CR><C-w>p', { buffer = true })
+vim.keymap.set('n', '{',     'k<CR><C-w>p', { buffer = true })
