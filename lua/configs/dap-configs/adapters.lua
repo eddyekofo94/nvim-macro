7f94a46436660c822ec172c2b04a9ef9ae69507a
bekaboo <18127878294@qq.com>
2023-04-17 00:43:23 -0500

    1050: Feat: DAP configs: ask for params and programs on start.

--- lua/configs/dap-configs/adapters.lua
+++ lua/configs/dap-configs/adapters.lua
@@ -1,12 +1,26 @@
-return {
-  python = {
-    type = 'executable',
-    command = 'python',
-    args = { '-m', 'debugpy.adapter' },
-  },
-  bashdb = {
-    type = 'executable',
-    command = vim.fn.stdpath('data') .. '/mason/packages/bash-debug-adapter/bash-debug-adapter',
-    name = 'bashdb',
+local adapters = {}
+
+adapters.python = {
+  type = 'executable',
+  command = 'python',
+  args = { '-m', 'debugpy.adapter' },
+}
+
+adapters.bashdb = {
+  type = 'executable',
+  command = vim.fn.stdpath('data')
+    .. '/mason/packages/bash-debug-adapter/bash-debug-adapter',
+  name = 'bashdb',
+}
+
+adapters.codelldb = {
+  type = 'server',
+  port = '${port}',
+  executable = {
+    command = vim.fn.stdpath('data')
+      .. '/mason/packages/codelldb/extension/adapter/codelldb',
+    args = { '--port', '${port}' },
   },
 }
+
+return adapters
--- lua/configs/dap-configs/configurations.lua
+++ lua/configs/dap-configs/configurations.lua
@@ -1,49 +1,134 @@
-return {
+local dapconfig = {}
+
+---@class dapcache_t
+---@field program string|nil program path
+---@field args table<string, string> args[program] = args for program
+
+---Cache program path, args, etc.
+---@type table<string, dapcache_t>
+local cache = {
   python = {
-    {
-      type = 'python',
-      request = 'launch',
-      name = 'Launch file',
-      program = '${file}',
-      pythonPath = function()
-        local cwd = vim.fn.getcwd()
-        local fpath = vim.fn.expand('%:p:h')
-        local paths = {
-          fpath .. '/venv/bin/python',
-          fpath .. '/.venv/bin/python',
-          cwd .. '/venv/bin/python',
-          cwd .. '/.venv/bin/python',
-          '/usr/bin/python',
-        }
-        for _, path in ipairs(paths) do
-          if vim.fn.executable(path) == 1 then
-            return path
-          end
-        end
-      end,
-    },
+    args = {},
   },
   sh = {
-    {
-      type = 'bashdb',
-      request = 'launch',
-      name = 'Launch file',
-      showDebugOutput = true,
-      pathBashdb = vim.fn.stdpath('data')
-        .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb',
-      pathBashdbLib = vim.fn.stdpath('data')
-        .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir',
-      trace = true,
-      file = '${file}',
-      program = '${file}',
-      cwd = '${workspaceFolder}',
-      pathCat = 'cat',
-      pathBash = '/bin/bash',
-      pathMkfifo = 'mkfifo',
-      pathPkill = 'pkill',
-      args = {},
-      env = {},
-      terminalKind = 'integrated',
-    },
+    args = {},
+  },
+  cpp = {
+    args = {},
   },
 }
+
+dapconfig.python = {
+  {
+    type = 'python',
+    request = 'launch',
+    name = 'Launch file',
+    program = '${file}',
+    args = function()
+      local args = ''
+      local fname = vim.fn.expand('%:t')
+      vim.ui.input({
+        prompt = 'Enter arguments: ',
+        default = cache.python.args[fname],
+        completion = 'file',
+      }, function(input)
+        args = input
+        cache.python.args[fname] = args
+        vim.cmd.stopinsert()
+      end)
+      return vim.split(args, ' ')
+    end,
+    pythonPath = function()
+      local venv = vim.fs.find({ 'venv', '.venv' }, {
+        path = vim.fn.expand('%:p:h'),
+        upward = true,
+      })[1]
+      if venv and vim.fn.executable(venv .. '/bin/python') == 1 then
+        return venv .. '/bin/python'
+      end
+      return vim.fn.exepath('python')
+    end,
+  },
+}
+
+dapconfig.sh = {
+  {
+    type = 'bashdb',
+    request = 'launch',
+    name = 'Launch file',
+    showDebugOutput = true,
+    pathBashdb = vim.fn.stdpath('data')
+      .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb',
+    pathBashdbLib = vim.fn.stdpath('data')
+      .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir',
+    trace = true,
+    file = '${file}',
+    program = '${file}',
+    cwd = '${workspaceFolder}',
+    pathCat = 'cat',
+    pathBash = '/bin/bash',
+    pathMkfifo = 'mkfifo',
+    pathPkill = 'pkill',
+    args = function()
+      local args = ''
+      local fname = vim.fn.expand('%:t')
+      vim.ui.input({
+        prompt = 'Enter arguments: ',
+        default = cache.sh.args[fname],
+        completion = 'file',
+      }, function(input)
+        args = input
+        cache.sh.args[fname] = args
+        vim.cmd.stopinsert()
+      end)
+      return vim.split(args, ' ')
+    end,
+    env = {},
+    terminalKind = 'integrated',
+  },
+}
+
+dapconfig.cpp = {
+  {
+    type = 'codelldb',
+    name = 'Launch file',
+    request = 'launch',
+    program = function()
+      local program
+      vim.ui.input({
+        prompt = 'Enter path to executable: ',
+        default = vim.fs.find({ vim.fn.expand('%:t:r'), 'a.out' }, {
+          path = vim.fn.expand('%:p:h'),
+          upward = true,
+        })[1] or cache.cpp.program,
+        completion = 'file',
+      }, function(input)
+        program = input
+        cache.cpp.program = program
+        vim.cmd.stopinsert()
+      end)
+      return vim.fn.fnamemodify(program, ':p')
+    end,
+    args = function()
+      local args = ''
+      local fpath_base = vim.fn.expand('%:p:r')
+      vim.ui.input({
+        prompt = 'Enter arguments: ',
+        default = cache.cpp.program and cache.cpp.args[cache.cpp.program]
+          or cache.cpp.args[fpath_base],
+        completion = 'file',
+      }, function(input)
+        args = input
+        cache.cpp.args[cache.cpp.program or fpath_base] = args
+        vim.cmd.stopinsert()
+      end)
+      return vim.split(args, ' ')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+  },
+}
+
+dapconfig.c = dapconfig.cpp
+
+return dapconfig
