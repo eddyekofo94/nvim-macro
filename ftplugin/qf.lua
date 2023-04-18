b671fc94a918d93a937064f8d62daf9be2c55159
bekaboo <18127878294@qq.com>
2023-04-18 00:05:50 -0500

    1069: Mod: 'qf' ftplugin: change navigation keymaps.
    Use '<C-n>' and '<C-p>' instead of '}' and '{'.

--- ftplugin/qf.lua
+++ ftplugin/qf.lua
@@ -5,5 +5,5 @@ vim.wo.rnu       = false
 vim.keymap.set('n', '<Tab>', '<CR><C-w>p',  { buffer = true })
 vim.keymap.set('n', '<C-j>', 'j<CR><C-w>p', { buffer = true })
 vim.keymap.set('n', '<C-k>', 'k<CR><C-w>p', { buffer = true })
-vim.keymap.set('n', '}',     'j<CR><C-w>p', { buffer = true })
-vim.keymap.set('n', '{',     'k<CR><C-w>p', { buffer = true })
+vim.keymap.set('n', '<C-n>', 'j<CR><C-w>p', { buffer = true })
+vim.keymap.set('n', '<C-p>', 'k<CR><C-w>p', { buffer = true })
