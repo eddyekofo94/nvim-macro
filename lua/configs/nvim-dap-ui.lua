b7fa1835b1c5421c00b16e4a0057f8f1db4da086
bekaboo <18127878294@qq.com>
2023-04-19 03:25:36 -0500

    1076: Format: nvim-dap-ui config.

--- lua/configs/nvim-dap-ui.lua
+++ lua/configs/nvim-dap-ui.lua
@@ -1,39 +1,54 @@
 local dap, dapui = require('dap'), require('dapui')
-dap.listeners.after.event_initialized['dapui_config'] = function() dapui.open() end
-dap.listeners.before.event_terminated['dapui_config'] = function() dapui.close() end
-dap.listeners.before.event_exited['dapui_config'] = function() dapui.close() end
+-- stylua: ignore start
+dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+dap.listeners.before.event_exited['dapui_config']     = dapui.close
+-- stylua: ignore end
+
 -- Use menu key (Fn + left Ctrl) to open floating window
-vim.keymap.set('n', '<F16>', dapui.float_element, { noremap = true })
+vim.keymap.set('n', '<F16>', dapui.float_element)
 -- Use shift + F12 to evaluate expression
-vim.keymap.set({ 'n', 'x' }, '<F24>', dapui.eval, { noremap = true })
+vim.keymap.set({ 'n', 'x' }, '<F24>', dapui.eval)
+
 dapui.setup({
-  layouts = { {
-      elements = { {
+  layouts = {
+    {
+      elements = {
+        {
           id = 'scopes',
-          size = 0.25
-        }, {
+          size = 0.25,
+        },
+        {
           id = 'breakpoints',
-          size = 0.25
-        }, {
+          size = 0.25,
+        },
+        {
           id = 'stacks',
-          size = 0.25
-        }, {
+          size = 0.25,
+        },
+        {
           id = 'watches',
-          size = 0.25
-        } },
+          size = 0.25,
+        },
+      },
       position = 'left',
-      size = 0.3
-    }, {
-      elements = { {
+      size = 0.3,
+    },
+    {
+      elements = {
+        {
           id = 'repl',
-          size = 0.5
-        }, {
+          size = 0.5,
+        },
+        {
           id = 'console',
-          size = 0.5
-        } },
+          size = 0.5,
+        },
+      },
       position = 'bottom',
-      size = 0.25
-    } },
+      size = 0.25,
+    },
+  },
   icons = {
     expanded = '▼',
     collapsed = '►',
