cc51006e869c8be78a59e57a3c819d5657ed8c33
bekaboo <18127878294@qq.com>
2023-04-20 12:30:06 -0500

    1090: Feat: colorcolumn: use 'winhl' mechanism.

--- plugin/colorcolumn.lua
+++ plugin/colorcolumn.lua
@@ -1,3 +1,4 @@
+-- stylua: ignore start
 local todec = {
   ['0'] = 0,
   ['1'] = 1,
@@ -35,11 +36,7 @@ local tohex = {
   [14] = 'E',
   [15] = 'F',
 }
-
-local store = {
-  previous_cc = '', ---@type string
-  colorcol_bg = '', ---@type string
-}
+-- stylua: ignore end
 
 ---Convert an integer from hexadecimal to decimal
 ---@param hex string
@@ -138,7 +135,7 @@ end
 ---Resolve the colorcolumn value
 ---@param cc string|nil
 ---@return integer|nil cc_number smallest integer >= 0 or nil
-local function resolve_cc(cc)
+local function cc_resolve(cc)
   if not cc or cc == '' then
     return nil
   end
@@ -156,279 +153,87 @@ local function resolve_cc(cc)
   return cc_min
 end
 
----Set a window-local option safely without changing the window view
----@param win integer window handle, 0 for current window
----@param name string option name
----@param value any option value
-local function win_safe_set_option(win, name, value)
-  local winview = vim.fn.winsaveview()
-  vim.wo[win][name] = value
-  vim.fn.winrestview(winview)
-end
-
----Fallback to the first non-empty string
----@vararg string
----@return string|nil
-local function str_fallback(...)
-  local args = { ... }
-  for _, arg in pairs(args) do
-    if type(arg) == 'string' and arg ~= '' then
-      return arg
-    end
-  end
-  return nil
-end
-
----Check if colorcolumn should be displayed in current mode
----@return boolean
-local function in_display_mode()
-  local mode = vim.fn.mode()
-  return vim.startswith(mode, 'i') or vim.startswith(mode, 'R')
-end
-
----Set to be relative to textwidth if textwidth is set
----@param tbl table table passed to autocmd callbacks
-local function cc_set_relative(tbl)
-  if tbl.event == 'BufWinEnter' and vim.b._cc_last_set_by == 'modeline' then
-    return
-  end
-  if vim.bo.textwidth > 0 then
-    vim.w.cc = '+1'
-  else
-    vim.w.cc = str_fallback(vim.b.cc, vim.g.cc)
+---Hide colorcolumn
+---@param winid integer window handler
+local function cc_conceal(winid)
+  winid = winid or 0
+  local new_winhl = (
+    vim.wo[winid].winhl:gsub('ColorColumn:[^,]*', '')
+    .. ',ColorColumn:'
+  ):gsub(',*$', ''):gsub('^,*', ''):gsub(',+', ',')
+  if new_winhl ~= vim.wo[winid].winhl then
+    vim.wo[winid].winhl = new_winhl
   end
 end
 
----Redraw colorcolumn
-local function cc_redraw()
-  local cc = resolve_cc(vim.w.cc)
-  if not cc or not in_display_mode() then
-    win_safe_set_option(0, 'cc', '')
-    return
-  end
-
-  -- Fix 'E976: using Blob as a String' after select a snippet
-  -- entry from LSP server using omnifunc (<C-x><C-o>)
-  local length = vim.fn.strdisplaywidth(vim.fn.getline('.'))
-  local thresh = math.floor(0.75 * cc)
-  if length < thresh then
-    win_safe_set_option(0, 'cc', '')
-    return
-  end
-
-  win_safe_set_option(0, 'cc', vim.w.cc)
-
-  -- Show blended color when len < cc
-  local normal_bg = get_hl('Normal', 'background')
-  if length < cc then
-    vim.api.nvim_set_hl(0, 'ColorColumn', {
-      bg = blend(
-        store.colorcol_bg,
-        normal_bg,
-        (length - thresh) / (cc - thresh)
-      ),
-    })
-  else -- Show error color when length >= cc
-    local warning_color = get_hl('Error', 'foreground', '#FF0000')
-    vim.api.nvim_set_hl(0, 'ColorColumn', {
-      bg = blend(warning_color, normal_bg, 0.4),
-    })
+---Show colorcolumn
+---@param winid integer window handler
+local function cc_show(winid)
+  winid = winid or 0
+  local new_winhl = (
+    vim.wo[winid].winhl:gsub('ColorColumn:[^,]*', '')
+    .. ',ColorColumn:_ColorColumn'
+  ):gsub(',*$', ''):gsub('^,*', ''):gsub(',+', ',')
+  if new_winhl ~= vim.wo[winid].winhl then
+    vim.wo[winid].winhl = new_winhl
   end
 end
 
----Record cc settings of each window and hide them
-local function init()
-  local wins = vim.api.nvim_list_wins()
-  for _, win in ipairs(wins) do
-    vim.w[win].cc = vim.wo[win].cc
+---Conceal colorcolumn in each window
+local function cc_init()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    cc_conceal(win)
   end
-  vim.g.cc = vim.go.cc
-  for _, win in ipairs(wins) do
-    vim.wo[win].cc = ''
-  end
-  vim.go.cc = ''
-  store.colorcol_bg = get_hl('ColorColumn', 'background')
 end
 
--- colorcolumn is a window-local option, with some special rules:
--- 1. When a window is created, it inherits the value of the previous window or
---    the global option
--- 2. When a different buffer is displayed in current window, window-local cc
---    settings changes to the value when the buffer is displayed in the first
---    time, if there's no such value, it uses value of the global option
--- 3. Once the window-local cc is set, it's not changed by the global option
---    or inheritance, it will only change when a different buffer is displayed
---    or the option is set explicitly (via set or setlocal)
-
----Make autocmds to track colorcolumn settings
----@param id number autocmd group id
-local function autocmd_track_cc(id)
-  -- Save previous window cc settings
-  vim.api.nvim_create_autocmd({ 'WinLeave' }, {
-    group = id,
-    callback = function()
-      store.previous_cc = vim.w.cc
-      win_safe_set_option(0, 'cc', '')
-    end,
-  })
-
-  -- Broadcast previous window or global cc settings to new windows
-  vim.api.nvim_create_autocmd({ 'WinNew' }, {
-    group = id,
-    callback = function()
-      vim.w.cc = str_fallback(store.previous_cc, vim.g.cc)
-    end,
-  })
-
-  -- Save cc settings for each window
-  vim.api.nvim_create_autocmd({ 'WinEnter' }, {
-    group = id,
-    callback = function()
-      vim.w.cc = str_fallback(vim.w.cc, vim.wo.cc)
-    end,
-  })
-
-  -- On entering a buffer, check and set vim.b.cc and vim.w.cc in the
-  -- following order:
-  -- 1. If vim.wo.cc is non empty, then it is set from a modeline, use it.
-  --    Notice that this is after the 'FileType' event, which applies ftplugin
-  --    settings
-  -- 2. If vim.b.cc if non empty, it is set previously by broadcasting or an
-  --    ftplugin, use it
-  -- 3. Else use vim.g.cc
-  -- We want to unset vim.wo.cc on leaving a buffer, so that vim.wo.cc reflects
-  -- changes from modelines
-  vim.api.nvim_create_autocmd({ 'BufLeave' }, {
-    group = id,
-    callback = function()
-      win_safe_set_option(0, 'cc', '')
-    end,
-  })
-  vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-    group = id,
-    callback = function()
-      vim.b.cc = str_fallback(vim.wo.cc, vim.b.cc, vim.g.cc)
-      vim.w.cc = str_fallback(vim.wo.cc, vim.b.cc, vim.g.cc)
-      if vim.b.cc == vim.wo.cc then
-        vim.b._cc_last_set_by = 'modeline'
-      end
-      if not in_display_mode() then
-        win_safe_set_option(0, 'cc', '')
-      end
-    end,
-  })
-
-  -- Update cc settings on option change
-  vim.api.nvim_create_autocmd({ 'OptionSet' }, {
-    pattern = 'colorcolumn',
-    group = id,
-    callback = function()
-      if vim.v.option_type == 'global' then
-        vim.g.cc = vim.go.cc
-        vim.w.cc = vim.go.cc
-        vim.b.cc = vim.go.cc
-      elseif vim.v.option_type == 'local' then
-        vim.w.cc = vim.wo.cc
-        vim.b.cc = vim.wo.cc
-      end
-      vim.go.cc = ''
-      win_safe_set_option(0, 'cc', '')
-    end,
-  })
-
-  -- Handle cc settings from ftplugins
-  -- Detect cc changes in a quite hacky way, because OptionSet autocmd is not
-  -- triggered when cc is set in a ftplugin
-  -- Quirk: these two commands are not the same in a ftplugin:
-  --     setlocal cc=80 " vimscript
-  --     vim.wo.cc = 80 -- lua
-  -- The former (vimscript) will set the 'buffer-local' cc, i.e. it will set cc
-  -- for current window BUT will be reset for a different buffer displayed in
-  -- the same window.
-  -- The latter (lua) will set the 'window-local' cc, i.e. it will set cc for
-  -- current window and will NOT be reset for a different buffer displayed in
-  -- the same window.
-  -- Currently there is no way to tell which one is used in a ftplugin, since I
-  -- prefer the vimscript way, I simulate its behavior here when a change is
-  -- detected for cc in current window.
-  vim.api.nvim_create_autocmd({ 'BufReadPre' }, {
-    group = id,
-    callback = function()
-      vim.b._cc = vim.wo.cc
-      vim.g._cc = vim.go.cc
-    end,
-  })
-  vim.api.nvim_create_autocmd({ 'FileType' }, {
-    group = id,
-    callback = function()
-      -- If cc changes between BufReadPre and FileType, it is an ftplugin
-      -- that sets cc, so we accept it as a 'buffer-local' (phony) cc setting
-      -- Notice that we will do nothing if vim.b._cc is nil, which means the
-      -- buffer is not the same buffer that triggers BufReadPre
-      if vim.b._cc and vim.wo.cc ~= vim.b._cc then
-        vim.b.cc = vim.wo.cc
-        if vim.go.cc ~= vim.g._cc then
-          vim.g.cc = vim.go.cc
-        end
-      end
-    end,
-  })
-end
-
----Make autocmds to set colorcolumn relative to textwidth
----@param id number autocmd group id
-local function autocmd_follow_tw(id)
-  -- Set cc to be relative to textwidth if textwidth is set
-  vim.api.nvim_create_autocmd({ 'OptionSet' }, {
-    pattern = 'textwidth',
-    group = id,
-    callback = cc_set_relative,
-  })
-  vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-    group = id,
-    callback = cc_set_relative,
-  })
-end
-
----Make autocmds to update colorcolumn background color
----@param id number autocmd group id
-local function autocmd_update_color(id)
-  -- Save Colorcolum background color
-  vim.api.nvim_create_autocmd({ 'ColorScheme', 'UIEnter' }, {
-    group = id,
-    callback = function()
-      store.colorcol_bg = get_hl('ColorColumn', 'background')
-    end,
-  })
-end
-
----Make autocmds to display colorcolumn
----@param id number autocmd group id
-local function autocmd_display_cc(id)
-  -- Show colored column
-  vim.api.nvim_create_autocmd({
-    'ModeChanged',
-    'TextChangedI',
-    'CursorMovedI',
-    'ColorScheme',
-  }, {
-    group = id,
-    callback = cc_redraw,
-  })
-  vim.api.nvim_create_autocmd({ 'OptionSet' }, {
-    pattern = { 'colorcolumn', 'textwidth' },
-    group = id,
-    callback = cc_redraw,
-  })
-end
-
-local function make_autocmds()
+---Create autocmds for concealing / showing colorcolumn
+local function cc_autocmd()
   local id = vim.api.nvim_create_augroup('AutoColorColumn', {})
-  autocmd_update_color(id)
-  autocmd_track_cc(id)
-  autocmd_follow_tw(id)
-  autocmd_display_cc(id)
+  vim.api.nvim_create_autocmd({ 'InsertLeave', 'WinLeave' }, {
+    group = id,
+    callback = function()
+      if not vim.api.nvim_win_get_config(0).zindex then
+        cc_conceal(0)
+      end
+    end,
+  })
+  vim.api.nvim_create_autocmd({ 'CursorMovedI', 'InsertEnter' }, {
+    group = id,
+    callback = function()
+      local cc = cc_resolve(vim.wo.cc)
+      if not cc or vim.api.nvim_win_get_config(0).zindex then
+        return
+      end
+
+      -- Fix 'E976: using Blob as a String' after select a snippet
+      -- entry from LSP server using omnifunc (<C-x><C-o>)
+      local length = vim.fn.strdisplaywidth(vim.fn.getline('.'))
+      local thresh = math.floor(0.75 * cc)
+      if length < thresh then
+        return
+      end
+
+      -- Show blended color when len < cc
+      local normal_bg = get_hl('Normal', 'background')
+      local colorcolumn_bg = get_hl('ColorColumn', 'background')
+      if length < cc then
+        vim.api.nvim_set_hl(0, '_ColorColumn', {
+          bg = blend(
+            colorcolumn_bg,
+            normal_bg,
+            (length - thresh) / (cc - thresh)
+          ),
+        })
+      else -- Show error color when length >= cc
+        local warning_color = get_hl('Error', 'foreground', '#FF0000')
+        vim.api.nvim_set_hl(0, '_ColorColumn', {
+          bg = blend(warning_color, normal_bg, 0.4),
+        })
+      end
+      cc_show(0)
+    end,
+  })
 end
 
-init()
-make_autocmds()
+cc_init()
+cc_autocmd()
