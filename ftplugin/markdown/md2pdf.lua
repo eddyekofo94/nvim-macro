3b8a55f397003bd285591744215227b857f5e01b
bekaboo <18127878294@qq.com>
2023-04-20 22:34:17 -0500

    1097: Feat: markdown ftplugin md2pdf: completion funcion & comments.
    Change completion function in markdown ftplugin to trigger options
    completion without double dash '--'.
    
    Add annotations and comments.

--- ftplugin/markdown/md2pdf.lua
+++ ftplugin/markdown/md2pdf.lua
@@ -1,3 +1,7 @@
+---Parse command line arguments into args (files) and options
+---@param fargs string[] <f-args>
+---@return string[] args
+---@return table<string, string> opts
 local function parse_args(fargs)
   local args = {}
   local opts = {}
@@ -12,6 +16,8 @@ local function parse_args(fargs)
   return args, opts
 end
 
+---Get list of available viewers
+---@return string[]
 local function get_viewers()
   local viewers = {
     'firefox',
@@ -34,6 +40,8 @@ local function get_viewers()
   return viewers
 end
 
+---Get list of markdown files currently opened in buffers
+---@return string[]
 local function get_md_files()
   local bufnames = {}
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
@@ -45,17 +53,24 @@ local function get_md_files()
   return bufnames
 end
 
-local function complete_md_to_pdf(arg_before, cmdline, cursor_pos)
-  if arg_before == '' then
-    return get_md_files()
-  elseif arg_before == '--' then
-    return {
-      '--viewer',
-      '--pdf-engine',
-    }
-  elseif arg_before == '--viewer=' then
+---Command completion function
+---@param arglead string leading portion of the argument being completed
+---@return string[] completion completion results
+local function complete_md_to_pdf(arglead, _, _)
+  local md_files = get_md_files()
+  local opts = {
+    '--viewer',
+    '--pdf-engine',
+  }
+  if arglead == '' then
+    local completions = md_files
+    for _, opt in ipairs(opts) do
+      table.insert(completions, opt)
+    end
+    return completions
+  elseif arglead == '--viewer=' then
     return get_viewers()
-  elseif arg_before == '--pdf-engine=' then
+  elseif arglead == '--pdf-engine=' then
     return {
       'latexmk',
       'lualatex',
@@ -67,46 +82,69 @@ local function complete_md_to_pdf(arg_before, cmdline, cursor_pos)
   return {}
 end
 
+---Close a handler if it is not nil and not closing
+---@param handler userdata|nil
 local function close_handler(handler)
   if handler and not handler:is_closing() then
     handler:close()
   end
 end
 
-local function spawn_viewer(args, opts)
-  local fname_pdfs = {}
-  for _, fname in ipairs(args) do
-    local fname_pdf = fname:gsub('%.md$', '.pdf')
-    table.insert(fname_pdfs, fname_pdf)
-  end
-
+---Spawn a viewer
+---@param files string[] files to be viewed
+---@param opts table<string, string> options
+local function spawn_viewer(files, opts)
   close_handler(_G.handler_viewer)
-  _G.handler_viewer = vim.loop.spawn(opts.viewer,
-  { args = fname_pdfs },
+  _G.handler_viewer = vim.loop.spawn(
+    opts.viewer,
+    { args = files },
     vim.schedule_wrap(function(code_viewer, _)
       close_handler(_G.handler_viewer)
       if code_viewer ~= 0 then
-        vim.notify(string.format('viewer %s failed with code %d',
-          opts.viewer, code_viewer), vim.log.levels.ERROR)
+        vim.notify(
+          string.format(
+            'viewer %s failed with code %d',
+            opts.viewer,
+            code_viewer
+          ),
+          vim.log.levels.ERROR
+        )
       end
-    end))
+    end)
+  )
 end
 
+---Spawn md2pdf shell script
+---@param args string[] markdown files to be converted
+---@param opts table<string, string> options
 local function spawn_pandoc(args, opts)
   close_handler(_G.handler_md2pdf)
-  _G.handler_md2pdf = vim.loop.spawn('md2pdf', {
-    args = { '--' .. opts['pdf-engine'], unpack(args) },
-  }, vim.schedule_wrap(function(code_md2pdf, _)
-    close_handler(_G.handler_md2pdf)
-    if code_md2pdf ~= 0 then
-      vim.notify(string.format('md2pdf failed with code %d',
-        code_md2pdf), vim.log.levels.ERROR)
-    elseif opts.viewer then
-      spawn_viewer(args, opts)
-    end
-  end))
+  _G.handler_md2pdf = vim.loop.spawn(
+    'md2pdf',
+    {
+      args = { '--' .. opts['pdf-engine'], unpack(args) },
+    },
+    vim.schedule_wrap(function(code_md2pdf, _)
+      close_handler(_G.handler_md2pdf)
+      if code_md2pdf ~= 0 then
+        vim.notify(
+          string.format('md2pdf failed with code %d', code_md2pdf),
+          vim.log.levels.ERROR
+        )
+      elseif opts.viewer then
+        local fname_pdfs = {}
+        for _, fname in ipairs(args) do
+          local fname_pdf = fname:gsub('%.md$', '.pdf')
+          table.insert(fname_pdfs, fname_pdf)
+        end
+        spawn_viewer(fname_pdfs, opts)
+      end
+    end)
+  )
 end
 
+---Convert markdown to pdf
+---@param tbl table information passed to the command
 local function md_to_pdf(tbl)
   local args, opts = parse_args(tbl.fargs)
   args = vim.tbl_deep_extend('force', { vim.fn.expand('%') }, args)
@@ -118,5 +156,5 @@ end
 vim.api.nvim_buf_create_user_command(0, 'MarkdownToPDF', md_to_pdf, {
   nargs = '*',
   complete = complete_md_to_pdf,
-  desc = 'Convert markdown to pdf'
+  desc = 'Convert markdown to pdf',
 })
