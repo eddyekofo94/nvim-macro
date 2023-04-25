ca4015f5de460a026f63b2ff3c413a851e4683ae
bekaboo <18127878294@qq.com>
2023-04-25 17:57:41 -0500

    1103: Cleanup/Format: telescope.nvim config.

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -2,26 +2,25 @@ local telescope = require('telescope')
 local telescope_builtin = require('telescope.builtin')
 local telescope_actions = require('telescope.actions')
 
-local keymap_opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<Leader>F', function() telescope_builtin.builtin() end, keymap_opts)
-vim.keymap.set('n', '<Leader>f', function() telescope_builtin.builtin() end, keymap_opts)
-vim.keymap.set('n', '<Leader>ff', function() telescope_builtin.find_files() end, keymap_opts)
-vim.keymap.set('n', '<Leader>fo', function() telescope_builtin.oldfiles() end, keymap_opts)
-vim.keymap.set('n', '<Leader>f;', function() telescope_builtin.live_grep() end, keymap_opts)
-vim.keymap.set('n', '<Leader>f*', function() telescope_builtin.grep_string() end, keymap_opts)
-vim.keymap.set('n', '<Leader>fh', function() telescope_builtin.help_tags() end, keymap_opts)
-vim.keymap.set('n', '<Leader>fb', function() telescope_builtin.current_buffer_fuzzy_find() end, keymap_opts)
-vim.keymap.set('n', '<Leader>fr', function() telescope_builtin.lsp_references({ jump_type = 'never', include_current_line = true }) end, keymap_opts)
-vim.keymap.set('n', '<Leader>fd', function() telescope_builtin.lsp_definitions({ jump_type = 'never', include_current_line = true }) end, keymap_opts)
-vim.keymap.set('n', '<Leader>fa', function() telescope_builtin.lsp_code_actions() end, keymap_opts)
-vim.keymap.set('n', '<Leader>fe', function() telescope_builtin.diagnostics() end, keymap_opts)
-vim.keymap.set('n', '<Leader>fp', function() telescope_builtin.treesitter() end, keymap_opts)
-vim.keymap.set('n', '<Leader>fs', function() telescope_builtin.lsp_document_symbols() end, keymap_opts)
-vim.keymap.set('n', '<Leader>fS', function() telescope_builtin.lsp_workspace_symbols() end, keymap_opts)
-vim.keymap.set('n', '<Leader>fg', function() telescope_builtin.git_status() end, keymap_opts)
-vim.keymap.set('n', '<Leader>fm', function() telescope_builtin.marks() end, keymap_opts)
-vim.keymap.set('n', '<Leader>fu', function() telescope.extensions.undo.undo() end, keymap_opts)
-vim.keymap.set('n', '<Leader>f<Esc>', '', keymap_opts)
+vim.keymap.set('n', '<Leader>F', function() telescope_builtin.builtin() end)
+vim.keymap.set('n', '<Leader>f', function() telescope_builtin.builtin() end)
+vim.keymap.set('n', '<Leader>ff', function() telescope_builtin.find_files() end)
+vim.keymap.set('n', '<Leader>fo', function() telescope_builtin.oldfiles() end)
+vim.keymap.set('n', '<Leader>f;', function() telescope_builtin.live_grep() end)
+vim.keymap.set('n', '<Leader>f*', function() telescope_builtin.grep_string() end)
+vim.keymap.set('n', '<Leader>fh', function() telescope_builtin.help_tags() end)
+vim.keymap.set('n', '<Leader>fb', function() telescope_builtin.current_buffer_fuzzy_find() end)
+vim.keymap.set('n', '<Leader>fr', function() telescope_builtin.lsp_references() end)
+vim.keymap.set('n', '<Leader>fd', function() telescope_builtin.lsp_definitions() end)
+vim.keymap.set('n', '<Leader>fa', function() telescope_builtin.lsp_code_actions() end)
+vim.keymap.set('n', '<Leader>fe', function() telescope_builtin.diagnostics() end)
+vim.keymap.set('n', '<Leader>fp', function() telescope_builtin.treesitter() end)
+vim.keymap.set('n', '<Leader>fs', function() telescope_builtin.lsp_document_symbols() end)
+vim.keymap.set('n', '<Leader>fS', function() telescope_builtin.lsp_workspace_symbols() end)
+vim.keymap.set('n', '<Leader>fg', function() telescope_builtin.git_status() end)
+vim.keymap.set('n', '<Leader>fm', function() telescope_builtin.marks() end)
+vim.keymap.set('n', '<Leader>fu', function() telescope.extensions.undo.undo() end)
+vim.keymap.set('n', '<Leader>f<Esc>', '')
 
 telescope.setup({
   defaults = {
@@ -51,20 +50,34 @@ telescope.setup({
         ['<M-s>'] = telescope_actions.select_horizontal,
         ['<M-v>'] = telescope_actions.select_vertical,
         ['<M-t>'] = telescope_actions.select_tab,
-        ['<M-Q>'] = telescope_actions.send_to_qflist + telescope_actions.open_qflist,
+        ['<M-Q>'] = telescope_actions.send_to_qflist
+          + telescope_actions.open_qflist,
       },
 
       n = {
         ['q'] = telescope_actions.close,
         ['<esc>'] = telescope_actions.close,
+        ['<C-n>'] = telescope_actions.move_selection_next,
+        ['<C-p>'] = telescope_actions.move_selection_previous,
         ['<M-c>'] = telescope_actions.close,
         ['<M-s>'] = telescope_actions.select_horizontal,
         ['<M-v>'] = telescope_actions.select_vertical,
         ['<M-t>'] = telescope_actions.select_tab,
-        ['<M-Q>'] = telescope_actions.send_to_qflist + telescope_actions.open_qflist,
-        ['<C-n>'] = telescope_actions.move_selection_next,
-        ['<C-p>'] = telescope_actions.move_selection_previous,
+        ['<M-Q>'] = telescope_actions.send_to_qflist
+          + telescope_actions.open_qflist,
       },
+    },
+  },
+  pickers = {
+    colorscheme = {
+      enable_preview = true,
+    },
+    lsp_references = {
+      include_current_line = true,
+      jump_type = 'never',
+    },
+    lsp_definitions = {
+      jump_type = 'never',
     },
   },
   extensions = {
@@ -76,8 +89,8 @@ telescope.setup({
         },
         n = {
           ['<CR>'] = require('telescope-undo.actions').restore,
-          ['ya'] = require("telescope-undo.actions").yank_additions,
-          ['yd'] = require("telescope-undo.actions").yank_deletions,
+          ['ya'] = require('telescope-undo.actions').yank_additions,
+          ['yd'] = require('telescope-undo.actions').yank_deletions,
         },
       },
     },
