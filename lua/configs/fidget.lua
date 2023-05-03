2db4781a2cd30bfbc6a9bbc164745409c843467a
bekaboo <18127878294@qq.com>
2023-05-03 02:06:31 -0500

    1140: Mod: fidget config: show fidget in insert mode.

--- lua/configs/fidget.lua
+++ lua/configs/fidget.lua
@@ -2,7 +2,6 @@ require('fidget').setup({
   text = { spinner = 'dots' },
   fmt = {
     fidget = function(fidget_name, spinner)
-      if string.match(vim.api.nvim_get_mode().mode, 'i.?') then return nil end
       return string.format('%s %s', spinner, fidget_name)
     end,
     task = function(_) return nil end,
