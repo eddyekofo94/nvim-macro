2bb1ddf68fc236ccc848c21d7eee005054978496
bekaboo <18127878294@qq.com>
2023-05-06 20:20:06 -0500

    1151: Mod: colorscheme: change lualine theme.

--- lua/colors/cockatoo/lua/lualine/themes/cockatoo.lua
+++ lua/colors/cockatoo/lua/lualine/themes/cockatoo.lua
@@ -9,28 +9,33 @@ lualine_theme.dark = {
     c = { fg = plt.smoke, bg = plt.ocean },
   },
   normal = {
-    a = { fg = plt.smoke, bg = plt.space, gui = 'bold' },
-    b = { fg = plt.smoke, bg = plt.ocean },
-    c = { fg = plt.smoke, bg = plt.deepsea },
+    a = { fg = plt.space, bg = plt.turquoise, gui = 'bold' },
+    b = { fg = plt.smoke, bg = plt.deepsea },
+    c = { fg = plt.smoke, bg = plt.ocean },
   },
   visual = {
-    a = { fg = plt.space, bg = plt.orange, gui = 'bold' },
+    a = { fg = plt.space, bg = plt.flashlight, gui = 'bold' },
     b = { fg = plt.smoke, bg = plt.thunder },
+    c = { fg = plt.smoke, bg = plt.deepsea },
   },
   replace = {
     a = { fg = plt.space, bg = plt.yellow, gui = 'bold' },
     b = { fg = plt.smoke, bg = plt.thunder },
+    c = { fg = plt.smoke, bg = plt.deepsea },
   },
   insert = {
-    a = { fg = plt.space, bg = plt.flashlight, gui = 'bold' },
+    a = { fg = plt.space, bg = plt.orange, gui = 'bold' },
     b = { fg = plt.smoke, bg = plt.thunder },
+    c = { fg = plt.smoke, bg = plt.deepsea },
   },
   terminal = {
-    a = { fg = plt.space, bg = plt.turquoise, gui = 'bold' }
+    a = { fg = plt.space, bg = plt.ochre, gui = 'bold' },
+    c = { fg = plt.smoke, bg = plt.deepsea },
   },
   command = {
-    a = { fg = plt.space, bg = plt.flashlight, gui = 'bold' },
+    a = { fg = plt.space, bg = plt.orange, gui = 'bold' },
     b = { fg = plt.smoke, bg = plt.thunder },
+    c = { fg = plt.smoke, bg = plt.deepsea },
   },
 }
 
@@ -46,23 +51,19 @@ lualine_theme.light = {
     c = { fg = plt.smoke, bg = plt.ocean },
   },
   visual = {
-    a = { fg = plt.space, bg = plt.yellow, gui = 'bold' },
-    b = { fg = plt.smoke, bg = plt.thunder },
+    a = { fg = plt.space, bg = plt.flashlight, gui = 'bold' },
   },
   replace = {
-    a = { fg = plt.space, bg = plt.orange, gui = 'bold' },
-    b = { fg = plt.smoke, bg = plt.thunder },
+    a = { fg = plt.space, bg = plt.yellow, gui = 'bold' },
   },
   insert = {
-    a = { fg = plt.space, bg = plt.purple, gui = 'bold' },
-    b = { fg = plt.smoke, bg = plt.thunder },
+    a = { fg = plt.space, bg = plt.orange, gui = 'bold' },
   },
   terminal = {
-    a = { fg = plt.space, bg = plt.turquoise, gui = 'bold' }
+    a = { fg = plt.space, bg = plt.orange, gui = 'bold' },
   },
   command = {
-    a = { fg = plt.space, bg = plt.flashlight, gui = 'bold' },
-    b = { fg = plt.smoke, bg = plt.thunder },
+    a = { fg = plt.space, bg = plt.orange, gui = 'bold' },
   },
 }
 
