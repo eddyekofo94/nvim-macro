40511618b75a2cde2c57d247a713b626ca4eec01
bekaboo <18127878294@qq.com>
2023-05-10 01:06:43 -0500

    1153: Cleanup/Mod: plugins.lua.

--- lua/init/plugins.lua
+++ lua/init/plugins.lua
@@ -1,22 +1,70 @@
-local function manage_plugins(config)
-  config = vim.tbl_deep_extend('force', {
-    root = vim.fn.stdpath('data') .. '/lazy',
+---Read file contents
+---@param path string
+---@return string?
+local function read_file(path)
+  local file = io.open(path, 'r')
+  if not file then
+    return nil
+  end
+  local content = file:read('*a')
+  file:close()
+  return content
+end
+
+---Install package manager if not already installed
+---@return boolean success
+local function bootstrap()
+  vim.g.package_path = vim.fn.stdpath('data') .. '/site/pack/packages/opt'
+  vim.g.package_lock = vim.fn.stdpath('config') .. '/package-lock.json'
+  local lazy_path = vim.g.package_path .. '/lazy.nvim'
+  vim.opt.rtp:prepend(lazy_path)
+  vim.opt.pp:prepend(lazy_path)
+  if not vim.loop.fs_stat(lazy_path) then
+    local lock = read_file(vim.g.package_lock)
+    local lock_data = lock and vim.json.decode(lock) or nil
+    local commit = lock_data
+        and lock_data['lazy.nvim']
+        and lock_data['lazy.nvim'].commit
+      or nil
+    local url = 'https://github.com/folke/lazy.nvim.git'
+    vim.notify('Installing lazy.nvim...', vim.log.levels.INFO)
+    vim.fn.mkdir(vim.g.package_path, 'p')
+    local cloned = os.execute(
+      table.concat(
+        { 'git', 'clone', '--filter=blob:none', url, lazy_path },
+        ' '
+      )
+    )
+    if commit then
+      os.execute(table.concat({
+        'git',
+        '--git-dir=' .. lazy_path .. '/.git',
+        '--work-tree=' .. lazy_path,
+        'checkout',
+        commit,
+      }, ' '))
+    end
+    if cloned ~= 0 then
+      vim.notify('Failed to clone lazy.nvim', vim.log.levels.ERROR)
+      return false
+    end
+    vim.notify('lazy.nvim cloned to ' .. lazy_path, vim.log.levels.INFO)
+  end
+  return true
+end
+
+---Enable modules
+---@param module_names string[]
+local function enable_modules(module_names)
+  local config = {
+    root = vim.g.package_path,
     ui = {
       border = 'shadow',
-      size = {
-        width = 0.7,
-        height = 0.74,
-      },
+      size = { width = 0.7, height = 0.74 },
     },
-    checker = {
-      enabled = false,
-    },
-    change_detection = {
-      notify = false,
-    },
-    install = {
-      colorscheme = { 'cockatoo' },
-    },
+    checker = { enabled = false },
+    change_detection = { notify = false },
+    install = { colorscheme = { 'cockatoo' } },
     performance = {
       rtp = {
         disabled_plugins = {
@@ -31,38 +79,34 @@ local function manage_plugins(config)
         },
       },
     },
-  }, config)
-
-  -- Install lazy if not already installed
-  local url = 'https://github.com/folke/lazy.nvim.git'
-  local lazy_path = config.root .. '/lazy.nvim'
-  vim.opt.rtp:prepend(lazy_path)
-  vim.opt.pp:prepend(lazy_path)
-  if not vim.loop.fs_stat(lazy_path) then
-    vim.notify('Installing lazy.nvim...', vim.log.levels.INFO)
-    vim.fn.system({ 'git', 'clone', '--filter=blob:none', url, lazy_path })
-    vim.notify('lazy.nvim cloned to ' .. lazy_path, vim.log.levels.INFO)
+  }
+  local modules = {}
+  for _, module_name in ipairs(module_names) do
+    vim.list_extend(modules, require('modules.' .. module_name))
   end
-
-  local lazy_view_config = require('lazy.view.config')
-  lazy_view_config.keys.details = '<Tab>'
-
-  require('lazy').setup(config)
+  require('lazy').setup(modules, config)
 end
 
+if not bootstrap() then
+  return
+end
 if vim.g.vscode then
-  manage_plugins({
-    spec = {
-      { import = 'modules.base' },
-      { import = 'modules.treesitter' },
-      { import = 'modules.editor' },
-    },
+  enable_modules({
+    'base',
+    'treesitter',
+    'editor',
   })
 else
-  manage_plugins({
-    spec = {
-      { import = 'modules' },
-    },
+  enable_modules({
+    'base',
+    'completion',
+    'debug',
+    'editor',
+    'lsp',
+    'markup',
+    'tools',
+    'treesitter',
+    'ui',
   })
 end
 
