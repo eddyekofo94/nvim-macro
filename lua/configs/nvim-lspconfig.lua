18fd9e7897df8f2ed1b5445f818f89c4269b5fa9
bekaboo <18127878294@qq.com>
2023-05-12 01:12:42 -0500

    1178: Mod: lspconfig: use icons from statis.lua.

--- lua/configs/nvim-lspconfig.lua
+++ lua/configs/nvim-lspconfig.lua
@@ -49,7 +49,7 @@ local function lspconfig_diagnostic()
       -- Enable virtual text, override spacing to 4
       virtual_text = {
         spacing = 4,
-        prefix = '',
+        prefix = static.icons.ArrowLeft,
       },
     })
 
