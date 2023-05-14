3961886081621d5c725b6a18dc033b18af5bbfbd
bekaboo <18127878294@qq.com>
2023-05-14 18:35:00 -0500

    1226: Feat: nvim-treesitter-context config: add keymap to jump to the context.

--- lua/configs/nvim-treesitter-context.lua
+++ lua/configs/nvim-treesitter-context.lua
@@ -1,4 +1,7 @@
-require('treesitter-context').setup({
+local ts_context = require('treesitter-context')
+
+vim.keymap.set('n', '[C', ts_context.go_to_context)
+ts_context.setup({
   max_lines = 4,
   min_window_height = 8,
   mode = 'topline',
