e27b7ba013bbc71b81f6bc9e14f1b53b554a6a6f
bekaboo <18127878294@qq.com>
2023-05-15 17:52:08 -0500

    1235: Feat: winbar: more treesitter types, icons and hlgroups.

--- lua/plugin/winbar/sources/treesitter.lua
+++ lua/plugin/winbar/sources/treesitter.lua
@@ -6,14 +6,21 @@ local bar = require('plugin.winbar.bar')
 local types = {
   'array',
   'boolean',
+  'break_statement',
   'call',
+  'case_statement',
   'class',
   'constant',
   'constructor',
+  'continue_statement',
+  'delete',
+  'do_statement',
   'enum',
   'enum_member',
   'event',
+  'for_statement',
   'function',
+  'if_statement',
   'interface',
   'keyword',
   'list',
@@ -32,11 +39,13 @@ local types = {
   'specifier',
   'string',
   'struct',
+  'switch_statement',
   'type',
   'type_parameter',
   'unit',
   'value',
   'variable',
+  'while_statement',
   'declaration',
   'field',
   'identifier',
@@ -50,60 +59,54 @@ local types = {
 ---@param cursor integer[] cursor position
 ---@return winbar_symbol_t[] symbols winbar symbols
 local function get_symbols(buf, cursor)
-  local parsers_ok, parsers = pcall(require, 'nvim-treesitter.parsers')
-  if not parsers_ok or not parsers.has_parser() then
+  if not vim.treesitter.highlighter.active[buf] then
     return {}
   end
 
   local symbols = {}
   local prev_type_rank = math.huge
+  local prev_row = math.huge
   local current_node = vim.treesitter.get_node({
     bufnr = buf,
     pos = { cursor[1] - 1, cursor[2] },
   })
   while current_node do
     local ts_type = current_node:type()
-    for type_rank, type in ipairs(types) do
-      if ts_type:find(type, 1, true) then
-        local lsp_type = funcs.string.snake_to_camel(type)
-        local name = vim.trim(
-          vim.treesitter
-            .get_node_text(current_node, buf)
-            :match(string.rep('[#~%w%._%->!]*', 4, '%s*'))
-            :gsub('\n.*', '')
-        )
-        if
-          vim.tbl_isempty(symbols)
-          or symbols[1].name ~= name
-          or vim.tbl_contains({
-            'if',
-            'for',
-            'while',
-            'do',
-            'try',
-            'catch',
-            'finally',
-            'switch',
-            'case',
-            'default',
-          }, name)
-        then
-          table.insert(
-            symbols,
-            1,
-            bar.winbar_symbol_t:new({
-              icon = icons[lsp_type],
-              name = name,
-              icon_hl = 'WinBarIconKind' .. lsp_type,
-            })
-          )
-          prev_type_rank = type_rank
-          break
-        elseif symbols[1].name == name and type_rank < prev_type_rank then
-          symbols[1].icon = icons[lsp_type]
-          symbols[1].icon_hl = 'WinBarIconKind' .. lsp_type
-          prev_type_rank = type_rank
-          break
+    local name = vim.trim(
+      vim.treesitter
+        .get_node_text(current_node, buf)
+        :match(string.rep('[#~%w%._%->!]*', 4, '%s*'))
+        :gsub('\n.*', '')
+    )
+    local start_row = (current_node:range())
+    if name ~= '' then
+      for type_rank, type in ipairs(types) do
+        if ts_type:find(type, 1, true) then
+          local lsp_type = funcs.string.snake_to_camel(type)
+          if
+            vim.tbl_isempty(symbols)
+            or symbols[1].name ~= name
+            or start_row < prev_row
+          then
+            table.insert(
+              symbols,
+              1,
+              bar.winbar_symbol_t:new({
+                icon = icons[lsp_type],
+                name = name,
+                icon_hl = 'WinBarIconKind' .. lsp_type,
+              })
+            )
+            prev_type_rank = type_rank
+            prev_row = start_row
+            break
+          elseif type_rank < prev_type_rank then
+            symbols[1].icon = icons[lsp_type]
+            symbols[1].icon_hl = 'WinBarIconKind' .. lsp_type
+            prev_type_rank = type_rank
+            prev_row = start_row
+            break
+          end
         end
       end
     end
