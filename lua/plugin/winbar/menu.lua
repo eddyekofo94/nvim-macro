3804faa9a6465c3655c60626f41f11a9c3077c93
bekaboo <18127878294@qq.com>
2023-05-23 09:15:49 -0500

    1288: Feat: winbar: menu keymaps: allow specifying modes in keymap configs.

--- lua/plugin/winbar/menu.lua
+++ lua/plugin/winbar/menu.lua
@@ -382,8 +382,15 @@ function winbar_menu_t:make_buf()
   vim.bo[self.buf].ft = 'winbar_menu'
 
   -- Set buffer-local keymaps
-  for key, callback in pairs(configs.opts.menu.keymaps) do
-    vim.keymap.set('n', key, callback, { buffer = self.buf })
+  -- Default modes: normal and visual
+  for key, mapping in pairs(configs.opts.menu.keymaps) do
+    if type(mapping) == 'function' or type(mapping) == 'string' then
+      vim.keymap.set({ 'x', 'n' }, key, mapping, { buffer = self.buf })
+    elseif type(mapping) == 'table' then
+      for mode, rhs in pairs(mapping) do
+        vim.keymap.set(mode, key, rhs, { buffer = self.buf })
+      end
+    end
   end
 
   -- Set buffer-local autocmds
