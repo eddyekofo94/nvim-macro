d1855967796350293b852e3597a2df5209d1a698
bekaboo <18127878294@qq.com>
2023-06-03 21:33:21 -0500

    1350: Refactor: winbar: change hl-WinBarMenuCurrentSymbol.

--- lua/plugin/winbar/hlgroups.lua
+++ lua/plugin/winbar/hlgroups.lua
@@ -64,7 +64,7 @@ local function init()
     WinBarIconUISeparatorMenu       = { link = 'WinBarIconUISeparator' },
     WinBarMenuCurrentContext        = { link = 'PmenuSel' },
     WinBarMenuCurrentEntry          = { link = 'Visual' },
-    WinBarMenuCurrentSymbol         = { link = 'PmenuSel' },
+    WinBarMenuCurrentSymbol         = { bold = true },
   }
   -- stylua: ignore end
   for hl_name, hl_settings in pairs(hlgroups) do
