1e6de8395efa6248f94a6e2812c793534fb2f2da
bekaboo <18127878294@qq.com>
2023-06-07 02:22:03 -0500

    1373: Feat: winbar: look ahead to find the component to preview.

--- lua/plugin/winbar/menu.lua
+++ lua/plugin/winbar/menu.lua
@@ -110,13 +110,14 @@ end
 
 ---Get the component at the given position in the winbar menu
 ---@param col integer 1-indexed, byte-indexed
+---@param look_ahead boolean? whether to look ahead for the next component if the given position does not contain a component
 ---@return winbar_symbol_t?
 ---@return {start: integer, end: integer}? range of the component in the menu, byte-indexed, 0-indexed, start-inclusive, end-exclusive
-function winbar_menu_entry_t:get_component_at(col)
+function winbar_menu_entry_t:get_component_at(col, look_ahead)
   local col_offset = self.padding.left
   for _, component in ipairs(self.components) do
     local component_len = component:bytewidth()
-    if col > col_offset and col <= col_offset + component_len then
+    if (look_ahead or col > col_offset) and col <= col_offset + component_len then
       return component,
         {
           start = col_offset,
@@ -238,9 +239,10 @@ end
 
 ---Get the component at the given position in the winbar menu
 ---@param pos integer[] {row: integer, col: integer}, 1-indexed, byte-indexed
+---@param look_ahead boolean? whether to look ahead for the component at the given position
 ---@return winbar_symbol_t?
 ---@return {start: integer, end: integer}? range of the component in the menu, byte-indexed, 0-indexed, start-inclusive, end-exclusive
-function winbar_menu_t:get_component_at(pos)
+function winbar_menu_t:get_component_at(pos, look_ahead)
   if not self.entries or vim.tbl_isempty(self.entries) then
     return nil, nil
   end
@@ -250,7 +252,7 @@ function winbar_menu_t:get_component_at(pos)
   if not entry or not entry.components then
     return nil, nil
   end
-  return entry:get_component_at(col)
+  return entry:get_component_at(col, look_ahead)
 end
 
 ---"Click" the component at the given position in the winbar menu
@@ -464,8 +466,8 @@ function winbar_menu_t:make_buf()
         configs.opts.menu.preview.enable
         and not vim.deep_equal(cursor, self.prev_cursor)
       then
-        local component = self:get_component_at({ cursor[1], cursor[2] + 1 })
-        if component and component.preview then
+        local component = self:get_component_at({ cursor[1], cursor[2] + 1 }, true)
+        if component then
           component:preview()
         end
       end
