24da7a30b66fa6a40ab5d3b2bb9db7bd1d971e92
bekaboo <18127878294@qq.com>
2023-06-11 16:16:45 -0500

    1400: Fix: tmux config: allow count prefix in navigation.

--- lua/configs/tmux.lua
+++ lua/configs/tmux.lua
@@ -12,7 +12,18 @@ tmux.setup({
   },
 })
 
-vim.keymap.set('n', '<M-h>', tmux.move_left, { noremap = true })
-vim.keymap.set('n', '<M-j>', tmux.move_bottom, { noremap = true })
-vim.keymap.set('n', '<M-k>', tmux.move_top, { noremap = true })
-vim.keymap.set('n', '<M-l>', tmux.move_right, { noremap = true })
+---Wrap tmux navigation functions to allow for count prefix
+---@param fn function
+---@return function
+local function movement_wrap(fn)
+  return function()
+    for _ = 1, vim.v.count1 do
+      fn()
+    end
+  end
+end
+
+vim.keymap.set('n', '<M-h>', movement_wrap(tmux.move_left))
+vim.keymap.set('n', '<M-j>', movement_wrap(tmux.move_bottom))
+vim.keymap.set('n', '<M-k>', movement_wrap(tmux.move_top))
+vim.keymap.set('n', '<M-l>', movement_wrap(tmux.move_right))
