d227cc456ebc71bd3e631560af628331149c4c18
bekaboo <18127878294@qq.com>
2023-06-11 02:16:52 -0500

    1396: Refactor: winbar: remove pcall() in sources init file.

--- lua/plugin/winbar/sources/init.lua
+++ lua/plugin/winbar/sources/init.lua
@@ -4,11 +4,8 @@
 ---@type table<string, winbar_source_t>
 return setmetatable({}, {
   __index = function(self, key)
-    local ok, source = pcall(require, 'plugin.winbar.sources.' .. key)
-    if ok then
-      self[key] = source
-      return source
-    end
-    return nil
+    local source = require('plugin.winbar.sources.' .. key)
+    self[key] = source
+    return source
   end,
 })
