4d77614b688ab060ac5c75024398b50c58a9cbb7
bekaboo <18127878294@qq.com>
2023-06-16 02:47:27 -0500

    1442: Feat: ftplugin man: add keymap nowait attributes; init cursor pos.

--- after/ftplugin/man.lua
+++ after/ftplugin/man.lua
@@ -3,6 +3,8 @@ vim.opt_local.relativenumber = false
 vim.opt_local.signcolumn = 'no'
 vim.opt_local.scrolloff = 999
 
-vim.keymap.set({ 'n', 'x' }, 'q', 'ZQ', { buffer = true })
-vim.keymap.set({ 'n', 'x' }, 'd', '<C-d>', { buffer = true })
-vim.keymap.set({ 'n', 'x' }, 'u', '<C-u>', { buffer = true })
+vim.keymap.set({ 'n', 'x' }, 'q', 'ZQ', { buffer = true, nowait = true })
+vim.keymap.set({ 'n', 'x' }, 'd', '<C-d>', { buffer = true, nowait = true })
+vim.keymap.set({ 'n', 'x' }, 'u', '<C-u>', { buffer = true, nowait = true })
+
+vim.cmd('normal! M')
