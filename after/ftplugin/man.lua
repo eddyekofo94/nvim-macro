7922e3c49ce9bbf00909c08e0f7ded80b8a728de
bekaboo <18127878294@qq.com>
2023-06-16 00:52:57 -0500

    1437: Fix/Refactor: rm manpager plugin in favor of man ftplugin; fix keymaps in man buffers.

---
+++ after/ftplugin/man.lua
@@ -0,0 +1,8 @@
+vim.opt_local.number = false
+vim.opt_local.relativenumber = false
+vim.opt_local.signcolumn = 'no'
+vim.opt_local.scrolloff = 999
+
+vim.keymap.set({ 'n', 'x' }, 'q', 'ZQ', { buffer = true })
+vim.keymap.set({ 'n', 'x' }, 'd', '<C-d>', { buffer = true })
+vim.keymap.set({ 'n', 'x' }, 'u', '<C-u>', { buffer = true })
