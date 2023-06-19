61174b0d468fd444699dbf7f24e67c77dc0990c5
bekaboo <18127878294@qq.com>
2023-06-18 22:18:20 -0500

    1477: Feat: snippets: add more snip nodes.

--- lua/snippets/utils/snips.lua
+++ lua/snippets/utils/snips.lua
@@ -9,10 +9,23 @@ M.sa = ls.extend_decorator.apply(s, {
   snippetType = 'autosnippet',
 })
 
+-- Auto multi-snippets
+M.msa = ls.extend_decorator.apply(ms, {
+  common = { snippetType = 'autosnippet' },
+})
+
 -- Math auto snippets, automatically expands in math zone
 M.sam = ls.extend_decorator.apply(M.sa, {
   condition = conds.in_mathzone,
   show_condition = conds.in_mathzone,
+})
+
+-- Math auto multi-snippets, automatically expands in math zone
+M.msam = ls.extend_decorator.apply(M.msa, {
+  common = {
+    condition = conds.in_mathzone,
+    show_condition = conds.in_mathzone,
+  },
 })
 
 -- Math auto snippets with regex trigger
@@ -20,14 +33,29 @@ M.samr = ls.extend_decorator.apply(M.sam, {
   regTrig = true,
 })
 
+-- Math auto multi-snippets with regex trigger
+M.msamr = ls.extend_decorator.apply(M.msam, {
+  common = { regTrig = true },
+})
+
 -- Math operator auto snippets, wordTrig = false
 M.samo = ls.extend_decorator.apply(M.sam, {
   wordTrig = false,
 })
 
+-- Math operator auto multi-snippets, wordTrig = false
+M.msamo = ls.extend_decorator.apply(M.msam, {
+  common = { wordTrig = false },
+})
+
 -- Math operator auto snippets with regex trigger
 M.samor = ls.extend_decorator.apply(M.samo, {
   regTrig = true,
+})
+
+-- Math operator auto multi-snippets with regex trigger
+M.msamor = ls.extend_decorator.apply(M.msamo, {
+  common = { regTrig = true },
 })
 
 -- Snippets that does not expand in mathzone
@@ -36,10 +64,26 @@ M.sM = ls.extend_decorator.apply(s, {
   show_condition = -conds.in_mathzone,
 })
 
+-- Multi-snippets that does not expand in mathzone
+M.msM = ls.extend_decorator.apply(ms, {
+  common = {
+    condition = -conds.in_mathzone,
+    show_condition = -conds.in_mathzone,
+  },
+})
+
 -- Normal zone snippets
 M.sn = ls.extend_decorator.apply(s, {
   condition = conds.in_normalzone,
   show_condition = conds.in_normalzone,
+})
+
+-- Normal zone multi-snippets
+M.msn = ls.extend_decorator.apply(ms, {
+  common = {
+    condition = conds.in_normalzone,
+    show_condition = conds.in_normalzone,
+  },
 })
 
 -- Snippets that does not expand in string or comments ('code' snippets)
