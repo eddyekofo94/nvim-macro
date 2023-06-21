487ca36f0fdfd28defbfd477dd9f1adaf96313b7
bekaboo <18127878294@qq.com>
2023-06-21 05:22:12 -0500

    1524: Feat: ftplugin: python: set cursorcolumn only if cursorline is set and cursorlineopt is 'line' or 'both'.

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,1 +1,5 @@
-vim.opt_local.cursorcolumn = true
+local cursorline = vim.opt_local.cursorline:get()
+local cursorlineopt = vim.opt_local.cursorlineopt:get()
+if cursorline and (cursorlineopt == 'both' or cursorlineopt == 'line') then
+  vim.opt_local.cursorcolumn = true
+end
