a7aa27bc082a281525f1feaa38133ce3320372ee
bekaboo <18127878294@qq.com>
2023-06-22 00:35:47 -0500

    1529: Feat: ftplugin: qf: centure the cursor line in the source window.

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -5,9 +5,9 @@ vim.opt_local.rnu = false
 -- stylua: ignore end
 
 -- stylua: ignore start
-vim.keymap.set('n', '<Tab>', '<CR><C-w>p',  { buffer = true })
-vim.keymap.set('n', '<C-j>', 'j<CR><C-w>p', { buffer = true })
-vim.keymap.set('n', '<C-k>', 'k<CR><C-w>p', { buffer = true })
-vim.keymap.set('n', '<C-n>', 'j<CR><C-w>p', { buffer = true })
-vim.keymap.set('n', '<C-p>', 'k<CR><C-w>p', { buffer = true })
+vim.keymap.set('n', '<Tab>', '<CR>zz<C-w>p',  { buffer = true })
+vim.keymap.set('n', '<C-j>', 'j<CR>zz<C-w>p', { buffer = true })
+vim.keymap.set('n', '<C-k>', 'k<CR>zz<C-w>p', { buffer = true })
+vim.keymap.set('n', '<C-n>', 'j<CR>zz<C-w>p', { buffer = true })
+vim.keymap.set('n', '<C-p>', 'k<CR>zz<C-w>p', { buffer = true })
 -- stylua: ignore end
