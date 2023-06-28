53cd9074d8db325a2df97442d0f0e0dfb09d303c
bekaboo <18127878294@qq.com>
2023-06-28 01:31:43 -0500

    1585: Feat: allow multiple lazygit terminals.

--- lua/configs/toggleterm.lua
+++ lua/configs/toggleterm.lua
@@ -6,7 +6,7 @@ toggleterm.setup({
   shade_terminals = false,
   start_in_insert = false,
   hide_numbers = true,
-  autochdir = true,
+  autochdir = false,
   persist_size = false,
   direction = 'horizontal',
   float_opts = {
@@ -41,26 +41,66 @@ toggleterm.setup({
   end,
 })
 
-local lazygit = terminal.Terminal:new({
-  count = 999,
-  cmd = 'lazygit',
-  direction = 'float',
-  hidden = true,
-  on_open = function()
-    vim.cmd('silent! normal! 0')
-    vim.cmd('silent! startinsert!')
+local num_lazygits = 0
+local last_visited_git_repo = nil
+
+---@type table<string, Terminal>
+local lazygits = setmetatable({}, {
+  __index = function(self, path)
+    if type(path) ~= 'string' or not vim.uv.fs_stat(path) then
+      return nil
+    end
+    local lazygit = terminal.Terminal:new({
+      count = 1000 + num_lazygits,
+      cmd = 'lazygit',
+      dir = path,
+      direction = 'float',
+      hidden = true,
+      on_open = function()
+        vim.cmd('silent! normal! 0')
+        vim.cmd('silent! startinsert!')
+      end,
+    })
+    self[path] = lazygit
+    num_lazygits = num_lazygits + 1
+    return lazygit
   end,
 })
 
 ---Toggle lazygit
-local function lazygit_toggle()
-  lazygit:toggle()
+---@param dir string?
+local function lazygit_toggle(dir)
+  dir = vim.fs.normalize(dir or vim.fn.getcwd())
+  local git_repo = vim
+    .system({ 'git', '-C', dir, 'rev-parse', '--show-toplevel' })
+    :wait().stdout
+    :gsub('\n.*', '')
+  if lazygits[git_repo] then
+    lazygits[git_repo]:toggle()
+    if lazygits[git_repo]:is_open() then
+      last_visited_git_repo = git_repo
+    end
+  elseif last_visited_git_repo and lazygits[last_visited_git_repo] then
+    lazygits[last_visited_git_repo]:toggle()
+  else
+    vim.notify('[lazygit] not inside git repo', vim.log.levels.INFO)
+  end
 end
 
 vim.keymap.set({ 't', 'n' }, '<M-i>', lazygit_toggle)
 vim.keymap.set({ 't', 'n' }, '<C-\\>g', lazygit_toggle)
 vim.keymap.set({ 't', 'n' }, '<C-\\><C-g>', lazygit_toggle)
-vim.api.nvim_create_user_command('Lazygit', lazygit_toggle, {})
+vim.api.nvim_create_user_command('Lazygit', function(info)
+  lazygit_toggle(info.fargs[1])
+end, {
+  nargs = '?',
+  complete = function(arglead)
+    local existing_paths = vim.tbl_keys(lazygits)
+    return vim.tbl_isempty(existing_paths)
+        and vim.fn.getcompletion(arglead, 'dir')
+      or existing_paths
+  end,
+})
 
 -- stylua: ignore start
 vim.keymap.set({ 't', 'n' }, '<C-\\><C-\\>', function() toggleterm.toggle_command(nil,                    vim.v.count) end)
