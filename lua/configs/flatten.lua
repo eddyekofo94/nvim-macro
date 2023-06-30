7103ecff7f975f2322021fcd63bb4508082801a3
bekaboo <18127878294@qq.com>
2023-06-29 20:42:52 -0500

    1592: Fix: flatten config: see willothy/flatten.nvim#51.

--- lua/configs/flatten.lua
+++ lua/configs/flatten.lua
@@ -1,28 +1,56 @@
-local ok, toggleterm = pcall(require, 'toggleterm')
-if not ok then
-  print('flatten: toggleterm not found')
-  toggleterm = {
-    toggle = function(_) end,
-  }
-end
-
 require('flatten').setup({
   window = {
     open = 'alternate',
   },
   callbacks = {
     should_block = function(argv)
-      return vim.tbl_contains(argv, '-b')
+      for _, arg in ipairs(argv) do
+        if arg:find('.git/rebase-merge', 1, true) then
+          return true
+        end
+      end
+      return false
     end,
-    post_open = function(_, winnr, _, is_blocking)
-      if is_blocking then
-        toggleterm.toggle(0)
+    post_open = function(bufnr, winnr, ft, is_blocking)
+      if is_blocking or ft == 'gitcommit' or ft == 'gitrebase' then
+        -- Hide the terminal while it's blocking
+        for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+          if
+            vim.api
+              .nvim_buf_get_name(vim.api.nvim_win_get_buf(win))
+              :find('#toggleterm#')
+          then
+            require('toggleterm').toggle(0)
+            vim.t.toggleterm_hidden = true
+          end
+        end
       else
+        -- If it's a normal file, just switch to its window
         vim.api.nvim_set_current_win(winnr)
+      end
+      -- If the file is a git commit or git rebase, create one-shot autocmd to
+      -- delete its buffer on write
+      if ft == 'gitcommit' or ft == 'gitrebase' then
+        vim.api.nvim_create_autocmd('BufWritePost', {
+          buffer = bufnr,
+          once = true,
+          callback = function()
+            vim.defer_fn(function()
+              vim.api.nvim_buf_delete(bufnr, {})
+            end, 50)
+            return true
+          end,
+        })
       end
     end,
     block_end = function()
-      toggleterm.toggle(0)
+      -- After blocking ends (for a git commit, etc), reopen the terminal
+      vim.defer_fn(function()
+        if vim.t.toggleterm_hidden then
+          require('toggleterm').toggle(0)
+          vim.t.toggleterm_hidden = nil
+        end
+      end, 50)
     end,
   },
 })
