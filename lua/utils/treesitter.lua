47468b8337626eabd212c8845d27295ab4ddefd4
bekaboo <18127878294@qq.com>
2023-07-04 00:43:26 -0500

    1623: Feat: add param 'mode' to utils.treesitter.in_ts_node().

--- lua/utils/treesitter.lua
+++ lua/utils/treesitter.lua
@@ -8,20 +8,25 @@ function M.ts_active(buf)
   return vim.treesitter.highlighter.active[buf] ~= nil
 end
 
----Returns whether current cursor is in a comment
+---Returns whether cursor is in a specific type of treesitter node
 ---@param type string type of node
 ---@param pos integer[]? 1,0-indexed position, default: current cursor position
 ---@param buf integer? default: current buffer
+---@param mode string? default: current mode
 ---@return boolean
-function M.in_tsnode(type, pos, buf)
+function M.in_tsnode(type, pos, buf, mode)
   pos = pos or vim.api.nvim_win_get_cursor(0)
   buf = buf or vim.api.nvim_get_current_buf()
+  mode = mode or vim.api.nvim_get_mode().mode
   if not M.ts_active(buf) then
     return false
   end
   local node = vim.treesitter.get_node({
     bufnr = buf,
-    pos = { pos[1] - 1, pos[2] - 1 },
+    pos = {
+      pos[1] - 1,
+      pos[2] - (pos[2] >= 1 and mode:match('^i') and 1 or 0),
+    },
   })
   if not node then
     return false
