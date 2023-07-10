afe533d8a3a333732d5d346ec9c964efbd6b4dde
bekaboo <18127878294@qq.com>
2023-07-09 23:34:34 -0500

    1673: Fix: utils.keymap: should abbrev commands in ':' cmdline only.

--- lua/utils/keymap.lua
+++ lua/utils/keymap.lua
@@ -10,10 +10,13 @@ function M.command_abbrev(trig, command, opts)
   vim.keymap.set('ca', trig, function()
     local cmdline_before = vim.fn.getcmdline():sub(1, vim.fn.getcmdpos() - 1)
     local trig_escaped = vim.pesc(trig)
-    return not (cmdline_before:find('^%s*' .. trig_escaped .. '$'))
-        and not (cmdline_before:find('|%s*' .. trig_escaped .. '$'))
-        and trig
-      or command
+    -- stylua: ignore start
+    return vim.fn.getcmdtype() == ':'
+        and (cmdline_before:find('^%s*' .. trig_escaped .. '$')
+          or cmdline_before:find('|%s*' .. trig_escaped .. '$'))
+        and command
+      or trig
+    -- stylua: ignore end
   end, vim.tbl_deep_extend('keep', { expr = true }, opts or {}))
 end
 
