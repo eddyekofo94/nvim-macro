b28e279f1be36fa164df3b4325f108b7b8c82e31
bekaboo <18127878294@qq.com>
2023-07-16 21:32:13 -0500

    1683: Tweak: copilot config: use <C-f> to accept inline suggestion.

--- lua/configs/copilot.lua
+++ lua/configs/copilot.lua
@@ -1,18 +1,31 @@
+if vim.g.loaded_coplilot then
+  return
+end
+vim.g.loaded_coplilot = true
+
+local copilot = require('copilot')
+local suggestion = require('copilot.suggestion')
+local utils = require('utils')
+
+copilot.setup({
+  suggestion = {
+    auto_trigger = true,
+    keymap = {
+      accept = false,
+    },
+  },
+  filetypes = {
+    markdown = true,
+    help = true,
+  },
+})
+
 vim.defer_fn(function()
-  if vim.g.loaded_coplilot then
-    return
-  end
-  vim.g.loaded_coplilot = true
-  require('copilot').setup({
-    suggestion = {
-      auto_trigger = true,
-      keymap = {
-        accept = '<C-j>',
-      },
-    },
-    filetypes = {
-      markdown = true,
-      help = true,
-    },
-  })
-end, 100)
+  utils.keymap.amend('i', '<C-f>', function(fallback)
+    if suggestion.is_visible() then
+      suggestion.accept()
+    else
+      fallback()
+    end
+  end)
+end, 10)
