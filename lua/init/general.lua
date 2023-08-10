700bcbe14ab403615b173e1a4e99b43c64b4d6e1
bekaboo <18127878294@qq.com>
2023-08-10 16:49:59 +0800

    1718: Refactor: general: guicursor setting.

--- lua/init/general.lua
+++ lua/init/general.lua
@@ -34,15 +34,9 @@ opt.conceallevel   = 2
 -- For unknown reasons, wezterm won't reload config for current session
 -- when we spawn 'setbg' or 'setcolors' scripts from nvim using libuv
 -- if GUI cursor highlights are set for modes n,v,r,cr
-if vim.env.WEZTERM then
-  opt.gcr = 'n-v:block'
-    .. ',i-c-ci-ve:blinkoff500-blinkon500-block-TermCursor'
-    .. ',r-cr:hor20,o:hor50'
-else
-  opt.gcr = 'n-v:block-Cursor/lCursor'
-    .. ',i-c-ci-ve:blinkoff500-blinkon500-block-TermCursor'
-    .. ',r-cr:hor20,o:hor50-Cursor/lCursor'
-end
+opt.gcr = vim.env.WEZTERM
+    and 'n-v:block,i-c-ci-ve:blinkoff500-blinkon500-block-TermCursor,r-cr:hor20,o:hor50'
+  or 'n-v:block-Cursor/lCursor,i-c-ci-ve:blinkoff500-blinkon500-block-TermCursor,r-cr:hor20,o:hor50-Cursor/lCursor'
 
 -- Use patience algorithm for diffing
 opt.diffopt:append('algorithm:patience')
