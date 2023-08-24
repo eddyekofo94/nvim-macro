9e7bcb6e0a393ea6d723dfcb873e8c9025129064
bekaboo <18127878294@qq.com>
2023-08-24 22:11:27 +0800

    1802: Fix: autocmds: WinCloseJmp wronly sets previous window

--- lua/init/autocmds.lua
+++ lua/init/autocmds.lua
@@ -48,36 +48,12 @@ local autocmds = {
   },
 
   {
-    { 'WinEnter' },
+    { 'WinClosed' },
     {
       pattern = '*',
       group = 'WinCloseJmp',
       desc = 'Jump to last accessed window on closing the current one.',
-      callback = function()
-        if '' ~= vim.api.nvim_win_get_config(0).relative then
-          return
-        end
-        -- Record the window we jump from (previous) and to (current)
-        if nil == vim.t.winid_rec then
-          vim.t.winid_rec = {
-            prev = vim.fn.win_getid(),
-            current = vim.fn.win_getid(),
-          }
-        else
-          vim.t.winid_rec = {
-            prev = vim.t.winid_rec.current,
-            current = vim.fn.win_getid(),
-          }
-        end
-        -- Loop through all windows to check if the
-        -- previous one has been closed
-        for winnr = 1, vim.fn.winnr('$') do
-          if vim.fn.win_getid(winnr) == vim.t.winid_rec.prev then
-            return -- Return if previous window is not closed
-          end
-        end
-        vim.cmd('wincmd p')
-      end,
+      command = "if expand('<amatch>') == win_getid() | wincmd p | endif",
     },
   },
 
