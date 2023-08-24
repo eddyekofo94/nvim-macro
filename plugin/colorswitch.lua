710c2123e323f49bd6efb6d35cf0f2cc570935cc
bekaboo <18127878294@qq.com>
2023-08-24 14:37:42 +0800

    1798: Refactor: split color/bg switching autocmds into separate plugin

---
+++ plugin/colorswitch.lua
@@ -0,0 +1,102 @@
+-- Colorschemes other than the default colorscheme looks bad when the terminal
+-- does not support truecolor
+if vim.env.COLORTERM ~= 'truecolor' then
+  return
+end
+
+-- 1. Restore dark/light background and colorscheme from ShaDa so that nvim
+--    "remembers" the background and colorscheme when it is restarted.
+-- 2. Change background on receiving signal SIGUSER1 to make nvim colorscheme
+--    consistent with the system background.
+--    - The signal is sent by setbg/setcolors scripts when the background or
+--      colorscheme is changed by the user.
+-- 3. Spawn setbg/setcolors on colorscheme change to make other nvim instances
+--    and system color consistent with the current nvim instance.
+
+local groupid = vim.api.nvim_create_augroup('SwitchBackground', {})
+
+vim.api.nvim_create_autocmd({ 'BufReadPre', 'UIEnter' }, {
+  once = true,
+  group = groupid,
+  desc = 'Restore dark/light background and colorscheme from ShaDa.',
+  callback = function()
+    if vim.g.theme_restored then
+      return
+    end
+    vim.g.theme_restored = true
+    if vim.g.BACKGROUND and vim.g.BACKGROUND ~= vim.go.background then
+      vim.go.background = vim.g.BACKGROUND
+    end
+    if not vim.g.colors_name or vim.g.COLORSNAME ~= vim.g.colors_name then
+      vim.cmd('silent! colorscheme ' .. (vim.g.COLORSNAME or 'nano'))
+    end
+    return true
+  end,
+})
+
+vim.api.nvim_create_autocmd('Signal', {
+  nested = true,
+  pattern = 'SIGUSR1',
+  group = groupid,
+  desc = 'Change background on receiving signal SIGUSER1.',
+  callback = function()
+    local hrtime = vim.uv.hrtime()
+    -- Check the last time when a signal is received/sent to avoid
+    -- the infinite loop of
+    -- -> receiving signal
+    -- -> setting bg
+    -- -> sending signals to other nvim instances
+    -- -> receiving signals from other nvim instances
+    -- -> setting bg
+    -- -> ...
+    if vim.g.sig_hrtime and hrtime - vim.g.sig_hrtime < 500000000 then
+      return
+    end
+    vim.g.sig_hrtime = hrtime
+    vim.cmd.rshada()
+    -- Must save the background and colorscheme name read from ShaDa
+    -- because setting background or colorscheme will overwrite them
+    local background = vim.g.BACKGROUND or 'dark'
+    local colors_name = vim.g.COLORSNAME or 'nano'
+    if vim.go.background ~= background then
+      vim.go.background = background
+    end
+    if vim.g.colors_name ~= colors_name then
+      vim.cmd('silent! colorscheme ' .. colors_name)
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd('Colorscheme', {
+  group = 'SwitchBackground',
+  desc = 'Spawn setbg/setcolors on colorscheme change.',
+  callback = function()
+    vim.g.BACKGROUND = vim.go.background
+    vim.g.COLORSNAME = vim.g.colors_name
+    vim.cmd.wshada()
+    local hrtime = vim.uv.hrtime()
+    if vim.g.sig_hrtime and hrtime - vim.g.sig_hrtime < 500000000 then
+      return
+    end
+    vim.g.sig_hrtime = hrtime
+    local pid = vim.fn.getpid()
+    if vim.fn.executable('setbg') == 1 then
+      vim.uv.spawn('setbg', {
+        args = {
+          vim.go.background,
+          '--exclude-nvim-processes=' .. pid,
+        },
+        stdio = { nil, nil, nil },
+      })
+    end
+    if vim.fn.executable('setcolors') == 1 then
+      vim.uv.spawn('setcolors', {
+        args = {
+          vim.g.colors_name,
+          '--exclude-nvim-processes=' .. pid,
+        },
+        stdio = { nil, nil, nil },
+      })
+    end
+  end,
+})
