d60e87333e8684d8a41d707be6f56a7804414262
bekaboo <18127878294@qq.com>
2023-08-28 15:19:02 +0800

    1827: Fix: plugin colorswitch: should be enabled when running in GUI

--- plugin/colorswitch.lua
+++ plugin/colorswitch.lua
@@ -1,6 +1,6 @@
 -- Colorschemes other than the default colorscheme looks bad when the terminal
 -- does not support truecolor
-if vim.env.COLORTERM ~= 'truecolor' then
+if vim.env.COLORTERM ~= 'truecolor' and vim.fn.has('gui_running') == 0 then
   return
 end
 
