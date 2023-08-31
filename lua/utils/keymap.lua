b0bb6c68f7d50bc5005957268b3078fa1e621b90
bekaboo <18127878294@qq.com>
2023-09-01 00:56:52 +0800

    1850: Fix: utils keymap: should use getcmdcompltype() to check if at command position

--- lua/utils/keymap.lua
+++ lua/utils/keymap.lua
@@ -1,22 +1,13 @@
 local M = {}
 
----Set abbreviation that only expand when the trigger is at the start of the
----command line or after a pipe "|"
----(i.e. only when the trigger is at the position of a command)
+---Set abbreviation that only expand when the trigger is at the position of
+---a command
 ---@param trig string
 ---@param command string
 ---@param opts table?
 function M.command_abbrev(trig, command, opts)
   vim.keymap.set('ca', trig, function()
-    local cmdline_before = vim.fn.getcmdline():sub(1, vim.fn.getcmdpos() - 1)
-    local trig_escaped = vim.pesc(trig)
-    -- stylua: ignore start
-    return vim.fn.getcmdtype() == ':'
-        and (cmdline_before:find('^%s*' .. trig_escaped .. '$')
-          or cmdline_before:find('|%s*' .. trig_escaped .. '$'))
-        and command
-      or trig
-    -- stylua: ignore end
+    return vim.fn.getcmdcompltype() == 'command' and command or trig
   end, vim.tbl_deep_extend('keep', { expr = true }, opts or {}))
 end
 
