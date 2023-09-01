0be9a81b4c1c3462abada259fd25614c93ce0d52
bekaboo <18127878294@qq.com>
2023-09-01 14:28:16 +0800

    1857: Fix/Refactor: term utils: shall_esc() error on invalid channel id

--- lua/utils/term.lua
+++ lua/utils/term.lua
@@ -16,20 +16,14 @@ local commands_shall_esc = {
 
 ---Decide if pressing <Esc> in nvim builtin terminal should exit terminal mode
 ---caveat: does not work with `sudo [opts] <command>`
----@param pid integer? pid of a terminal job, default to jobpid(&channel)
----@param commands table<string, true> table of commands that if running, pressing <Esc> should exit terminal mode
----@return boolean? true if should exit terminal mode, nil if cannot decide
-function M.shall_esc(pid, commands)
-  pid = pid or vim.bo.channel == 0 and 0 or vim.fn.jobpid(vim.bo.channel)
-  if pid == 0 then -- Invalid pid
-    return
+---@return boolean true if should exit terminal mode
+function M.shall_esc()
+  local chan_valid, pid = pcall(vim.fn.jobpid, vim.bo.channel)
+  if not chan_valid then
+    return true
   end
-  commands = vim.tbl_deep_extend('force', commands_shall_esc, commands or {})
   local c = vim.trim(vim.fn.system('ps h -o comm -g ' .. pid .. ' | tail -n1'))
-  if vim.v.shell_error > 0 then
-    return nil
-  end
-  return commands[c] ~= nil
+  return vim.v.shell_error > 0 or commands_shall_esc[c] ~= nil
 end
 
 return M
