ab8c437f0307c1e8b36ff5461ab28cac1fae2b78
bekaboo <18127878294@qq.com>
2023-10-19 15:24:46 +0800

    1931: Remove: colorscheme: catppuccin

--- lua/configs/flatten.lua
+++ lua/configs/flatten.lua
@@ -11,23 +11,8 @@ require('flatten').setup({
       end
       return false
     end,
-    post_open = function(bufnr, winnr, ft, is_blocking)
-      if is_blocking or ft == 'gitcommit' or ft == 'gitrebase' then
-        -- Hide the terminal while it's blocking
-        for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
-          if
-            vim.api
-              .nvim_buf_get_name(vim.api.nvim_win_get_buf(win))
-              :find('#toggleterm#')
-          then
-            require('toggleterm').toggle(0)
-            vim.t.toggleterm_hidden = true
-          end
-        end
-      else
-        -- If it's a normal file, just switch to its window
-        vim.api.nvim_set_current_win(winnr)
-      end
+    post_open = function(bufnr, winnr, ft, _)
+      vim.api.nvim_set_current_win(winnr)
       -- If the file is a git commit or git rebase, create one-shot autocmd to
       -- delete its buffer on write
       if ft == 'gitcommit' or ft == 'gitrebase' then
@@ -42,15 +27,6 @@ require('flatten').setup({
           end,
         })
       end
-    end,
-    block_end = function()
-      -- After blocking ends (for a git commit, etc), reopen the terminal
-      vim.defer_fn(function()
-        if vim.t.toggleterm_hidden then
-          require('toggleterm').toggle(0)
-          vim.t.toggleterm_hidden = nil
-        end
-      end, 50)
     end,
   },
   one_per = {
