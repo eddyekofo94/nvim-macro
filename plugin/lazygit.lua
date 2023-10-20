0c98832a55a44da37ffd9411c4d0796a976330bc
bekaboo <18127878294@qq.com>
2023-10-20 17:23:53 +0800

    1903: Feat/Refactor: add 'Lazygit' command; change 'lzg' abbrevs

--- plugin/lazygit.lua
+++ plugin/lazygit.lua
@@ -1,3 +1,43 @@
-vim.keymap.set('n', '<M-i>', function()
-  require('plugin.lazygit').toggle({ '<M-i>' })
-end)
+local utils = require('utils')
+local toggle_key = '<M-i>'
+
+---@alias jobid integer
+---@type table<string, fterm_t>
+local lazygits = {}
+
+---Toggle lazygit at project root or current working directory
+---@param root string? path to the git repo, default to current project root path
+---@return nil
+local function lazygit_toggle(root)
+  if not root then
+    ---@diagnostic disable-next-line: undefined-field
+    local cwd = vim.uv.cwd()
+    root = utils.fs.proj_dir(cwd, { '.git/' }) or cwd
+  end
+  if root then
+    if lazygits[root] then
+      lazygits[root]:toggle()
+    else
+      lazygits[root] = utils.classes.fterm_t:new('lazygit', {
+        winopts = { border = 'solid' },
+        jobopts = { cwd = root },
+        termopts = { toggle_keys = { toggle_key } },
+      })
+    end
+  else
+    vim.notify('[lazygit] vim.uv.cwd() failed!', vim.log.levels.WARN)
+  end
+end
+
+vim.keymap.set('n', toggle_key, lazygit_toggle)
+
+utils.keymap.command_abbrev('lzg', 'Lazygit')
+utils.keymap.command_abbrev('lzgit', 'Lazygit')
+utils.keymap.command_abbrev('lazygit', 'Lazygit')
+vim.api.nvim_create_user_command('Lazygit', function(info)
+  lazygit_toggle(info.fargs[1])
+end, {
+  nargs = '?',
+  complete = 'dir',
+  desc = 'Toggle lazygit window, optionally accept an argument as the git repo path.',
+})
