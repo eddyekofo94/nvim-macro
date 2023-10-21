5217fd7018a1d9b1efba51f8035e437a3217b1cf
bekaboo <18127878294@qq.com>
2023-10-21 16:31:04 +0800

    1910: Feat: add new devicons

--- lua/configs/nvim-web-devicons.lua
+++ lua/configs/nvim-web-devicons.lua
@@ -14,6 +14,24 @@ require('nvim-web-devicons').setup({
       icon = vim.trim(icons.Desktop),
       name = 'DesktopEntry',
     },
+    lock = {
+      color = '#bbbbbb',
+      cterm_color = '250',
+      icon = vim.trim(icons.Lock),
+      name = 'Lock',
+    },
+    git = {
+      color = '#e84d31',
+      cterm_color = '196',
+      icon = vim.trim(icons.Git),
+      name = 'GitLogo',
+    },
+    commit_editmsg = {
+      color = '#e84d31',
+      cterm_color = '239',
+      icon = vim.trim(icons.Git),
+      name = 'GitCommit',
+    },
   },
   override_by_extension = {
     cu = {
@@ -39,7 +57,31 @@ require('nvim-web-devicons').setup({
       cterm_color = '61',
       icon = vim.trim(icons.Elisp),
       name = 'Elisp',
-    }
+    },
+    patch = {
+      color = '#e84d31',
+      cterm_color = '166',
+      icon = vim.trim(icons.Git),
+      name = 'Patch',
+    },
+    md = {
+      color = '#6fb5ca',
+      cterm_color = '74',
+      icon = vim.trim(icons.Markdown),
+      name = 'Md',
+    },
+    mdx = {
+      color = '#6fb5ca',
+      cterm_color = '74',
+      icon = vim.trim(icons.Markdown),
+      name = 'Mdx',
+    },
+    markdown = {
+      color = '#6fb5ca',
+      cterm_color = '74',
+      icon = vim.trim(icons.Markdown),
+      name = 'Markdown',
+    },
   },
   override_by_filename = {
     run_datasets = {
@@ -47,6 +89,48 @@ require('nvim-web-devicons').setup({
       cterm_color = '238',
       icon = vim.trim(icons.Sh),
       name = 'ShellRunDatasets',
+    },
+    ['package.json'] = {
+      color = '#bbbbbb',
+      cterm_color = '250',
+      icon = vim.trim(icons.Lock),
+      name = 'PackageJson',
+    },
+    ['package-lock.json'] = {
+      color = '#bbbbbb',
+      cterm_color = '250',
+      icon = vim.trim(icons.Lock),
+      name = 'PackageLockJson',
+    },
+    ['.luacheckrc'] = {
+      color = '#6d8086',
+      cterm_color = '66',
+      icon = vim.trim(icons.Config),
+      name = 'LuaCheckRc',
+    },
+    ['.gitattributes'] = {
+      color = '#e84d31',
+      cterm_color = '239',
+      icon = vim.trim(icons.Git),
+      name = 'GitAttributes',
+    },
+    ['.gitconfig'] = {
+      color = '#e84d31',
+      cterm_color = '239',
+      icon = vim.trim(icons.Git),
+      name = 'GitConfig',
+    },
+    ['.gitignore'] = {
+      color = '#e84d31',
+      cterm_color = '239',
+      icon = vim.trim(icons.Git),
+      name = 'GitIgnore',
+    },
+    ['.gitmodules'] = {
+      color = '#e84d31',
+      cterm_color = '239',
+      icon = vim.trim(icons.Git),
+      name = 'GitModules',
     },
   },
 })
