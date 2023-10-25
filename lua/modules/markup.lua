034b5ef3bc9565df28a24fd0226cc9b4d7b5a0dc
bekaboo <18127878294@qq.com>
2023-10-25 10:36:28 +0800

    1932: Fix: markdown-preview installation
    See https://github.com/iamcco/markdown-preview.nvim/issues/616

--- lua/modules/markup.lua
+++ lua/modules/markup.lua
@@ -9,7 +9,7 @@ return {
 
   {
     'iamcco/markdown-preview.nvim',
-    build = 'cd app && npm install',
+    build = 'cd app && npm install && cd - && git restore .',
     ft = 'markdown',
     init = function()
       vim.g.mkdp_filetypes = { 'markdown' }
