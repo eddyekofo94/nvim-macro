42b1458dec24ee40af3e898d3367608997226ddf
bekaboo <18127878294@qq.com>
2023-11-07 20:53:10 +0800

    2028: Refactor: general: only disable plugins shipped with nvim

--- lua/init/general.lua
+++ lua/init/general.lua
@@ -85,17 +85,14 @@ opt.spelloptions  = 'camel'
 opt.spellsuggest  = 'best,9'
 
 -- disable plugins shipped with neovim
-g.loaded_gzip            = 1
-g.loaded_tar             = 1
-g.loaded_tarPlugin       = 1
-g.loaded_zip             = 1
-g.loaded_zipPlugin       = 1
-g.loaded_getscript       = 1
-g.loaded_getscriptPlugin = 1
-g.loaded_vimball         = 1
-g.loaded_vimballPlugin   = 1
-g.loaded_matchit         = 1
-g.loaded_2html_plugin    = 1
-g.loaded_logiPat         = 1
-g.loaded_rrhelper        = 1
+g.loaded_2html_plugin      = 1
+g.loaded_gzip              = 1
+g.loaded_matchit           = 1
+g.loaded_tar               = 1
+g.loaded_tarPlugin         = 1
+g.loaded_tutor_mode_plugin = 1
+g.loaded_vimball           = 1
+g.loaded_vimballPlugin     = 1
+g.loaded_zip               = 1
+g.loaded_zipPlugin         = 1
 -- stylua: ignore end
