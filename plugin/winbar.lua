1e611344a814c499909df6da6c20e659d528e8d5
bekaboo <18127878294@qq.com>
2023-11-15 00:41:20 +0800

    2063: Tweak: plugin winbar: show symbols only

--- plugin/winbar.lua
+++ plugin/winbar.lua
@@ -5,9 +5,31 @@ vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufWritePost' }, {
     local static = require('utils.static')
     local winbar = require('plugin.winbar')
     local api = require('plugin.winbar.api')
+    local utils = require('plugin.winbar.utils')
+    local sources = require('plugin.winbar.sources')
     winbar.setup({
       general = {
         update_interval = 32,
+        enable = function(buf, win)
+          return vim.fn.win_gettype(win) == ''
+            and vim.wo[win].winbar == ''
+            and vim.bo[buf].bt == ''
+            and (
+              vim.bo[buf].ft == 'markdown'
+              or utils.treesitter.is_active(buf)
+            )
+        end,
+      },
+      bar = {
+        sources = function(buf)
+          return vim.bo[buf].ft == 'markdown' and { sources.markdown }
+            or {
+              utils.source.fallback({
+                sources.lsp,
+                sources.treesitter,
+              }),
+            }
+        end,
       },
       sources = {
         path = {
