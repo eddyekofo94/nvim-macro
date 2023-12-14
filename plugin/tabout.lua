046942e14352eae5e84f7c5cfb6327b76e5e30ed
bekaboo <18127878294@qq.com>
2023-12-14 20:23:36 +0800

    2247: Style: plugin tabout

--- plugin/tabout.lua
+++ plugin/tabout.lua
@@ -1,4 +1,7 @@
--- stylua: ignore start
-vim.keymap.set('i', '<Tab>', function() require('plugin.tabout').do_key('<Tab>') end)
-vim.keymap.set('i', '<S-Tab>', function() require('plugin.tabout').do_key('<S-Tab>') end)
--- stylua: ignore end
+vim.keymap.set('i', '<Tab>', function()
+  require('plugin.tabout').do_key('<Tab>')
+end)
+
+vim.keymap.set('i', '<S-Tab>', function()
+  require('plugin.tabout').do_key('<S-Tab>')
+end)
