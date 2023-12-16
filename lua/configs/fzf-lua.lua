2035c046754a2a32eeb3e68fa2568aa88b0c1161
bekaboo <18127878294@qq.com>
2023-12-16 15:05:00 +0800

    2270: Feat: fzf-lua config: use `<C-r>` to paste from registers

--- lua/configs/fzf-lua.lua
+++ lua/configs/fzf-lua.lua
@@ -35,6 +35,14 @@ fzf.setup({
         relativenumber = false,
       },
     },
+    on_create = function()
+      vim.keymap.set(
+        't',
+        '<C-r>',
+        [['<C-\><C-N>"' . nr2char(getchar()) . 'pi']],
+        { expr = true, buffer = true }
+      )
+    end,
   },
   hls = {
     normal = 'TelescopePromptNormal',
