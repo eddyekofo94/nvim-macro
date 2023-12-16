6827cd4ff1f0d5ba568b0fea3c77664d31f9d05b
bekaboo <18127878294@qq.com>
2023-12-16 14:47:05 +0800

    2268: Fix: syntax markdown: add buf validity check

--- syntax/markdown.lua
+++ syntax/markdown.lua
@@ -2,11 +2,14 @@ if vim.b.current_syntax ~= nil then
   vim.b.current_syntax = nil
 end
 
-local buf = vim.api.nvim_get_current_buf()
 -- Use vim.schedule to avoid freezing neovim
 -- when reloading markdown files
+local buf = vim.api.nvim_get_current_buf()
 vim.schedule(function()
   vim.api.nvim_buf_call(buf, function()
+    if not vim.api.nvim_buf_is_valid(buf) or vim.b[buf].large_file then
+      return
+    end
     -- Apply treesitter highlighting for better
     -- embedded code block syntax highlighting
     pcall(vim.treesitter.start, buf, 'markdown')
