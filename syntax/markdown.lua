b3d31cb1699f06382660ef5d271447f20c256132
bekaboo <18127878294@qq.com>
2023-12-19 04:05:36 +0800

    2297: Docs: syntax markdown: modify comments

--- syntax/markdown.lua
+++ syntax/markdown.lua
@@ -2,19 +2,16 @@ if vim.b.current_syntax ~= nil then
   vim.b.current_syntax = nil
 end
 
--- Use vim.schedule to avoid freezing neovim
--- when reloading markdown files
+-- Use vim.schedule to avoid freezing neovim when reloading markdown files
 local buf = vim.api.nvim_get_current_buf()
 vim.schedule(function()
   vim.api.nvim_buf_call(buf, function()
     if not vim.api.nvim_buf_is_valid(buf) or vim.b[buf].large_file then
       return
     end
-    -- Apply treesitter highlighting for better
-    -- embedded code block syntax highlighting
+    -- Get good embedded code block syntax highlighting from treesitter
+    -- and good math conceal from vimtex at the same time
     pcall(vim.treesitter.start, buf, 'markdown')
-    -- Apply regex syntax rules to get math conceal
-    -- provided by vimtex and url conceal
     vim.cmd.runtime('syntax/mkd.vim')
     vim.b[buf].current_syntax = 'mkd'
   end)
