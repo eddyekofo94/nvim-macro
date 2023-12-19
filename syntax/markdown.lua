50ea104659adbfe512c51fc47314900ed3c04199
bekaboo <18127878294@qq.com>
2023-12-20 02:29:45 +0800

    2308: Refactor: syntax: markdown: remove redundant `nvim_buf_call()`

--- syntax/markdown.lua
+++ syntax/markdown.lua
@@ -5,14 +5,12 @@ end
 -- Use vim.schedule to avoid freezing neovim when reloading markdown files
 local buf = vim.api.nvim_get_current_buf()
 vim.schedule(function()
-  vim.api.nvim_buf_call(buf, function()
-    if not vim.api.nvim_buf_is_valid(buf) or vim.b[buf].large_file then
-      return
-    end
-    -- Get good embedded code block syntax highlighting from treesitter
-    -- and good math conceal from vimtex at the same time
-    pcall(vim.treesitter.start, buf, 'markdown')
-    vim.cmd.runtime('syntax/mkd.vim')
-    vim.b[buf].current_syntax = 'mkd'
-  end)
+  if not vim.api.nvim_buf_is_valid(buf) or vim.b[buf].large_file then
+    return
+  end
+  -- Get good embedded code block syntax highlighting from treesitter
+  -- and good math conceal from vimtex at the same time
+  pcall(vim.treesitter.start, buf, 'markdown')
+  vim.cmd.runtime('syntax/mkd.vim')
+  vim.b[buf].current_syntax = 'mkd'
 end)
