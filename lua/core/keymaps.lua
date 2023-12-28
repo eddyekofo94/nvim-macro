8a778daca8e45cf9ac14a1f2548ea2ed27367909
bekaboo <18127878294@qq.com>
2023-12-28 16:18:20 +0800

    2365: Refactor: core.misc & core.keymaps: move some functions to misc

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -158,30 +158,7 @@ vim.keymap.set({ 'o', 'x' }, 'a`', '2i`', { noremap = false })
 
 -- Close all floating windows
 vim.keymap.set('n', 'q', function()
-  local count = 0
-  local current_win = vim.api.nvim_get_current_win()
-  -- close current win only if it's a floating window
-  if vim.api.nvim_win_get_config(current_win).relative ~= '' then
-    vim.api.nvim_win_close(current_win, true)
-    return
-  end
-  for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
-    if vim.api.nvim_win_is_valid(win) then
-      local config = vim.api.nvim_win_get_config(win)
-      -- close floating windows that can be focused
-      if config.relative ~= '' and config.focusable then
-        vim.api.nvim_win_close(win, false) -- do not force
-        count = count + 1
-      end
-    end
-  end
-  if count == 0 then -- Fallback
-    vim.api.nvim_feedkeys(
-      vim.api.nvim_replace_termcodes('q', true, true, true),
-      'n',
-      false
-    )
-  end
+  require('utils.misc').q()
 end)
 
 -- Text object: current buffer
@@ -192,110 +169,18 @@ vim.keymap.set('o', 'af', '<Cmd>silent! normal m`Vaf<CR><Cmd>silent! normal! ``<
 vim.keymap.set('o', 'if', '<Cmd>silent! normal m`Vif<CR><Cmd>silent! normal! ``<CR>', { silent = true, noremap = false })
 -- stylua: ignore end
 
--- Text object: folds
----Returns the key sequence to select around/inside a fold,
----supposed to be called in visual mode
----@param motion 'i'|'a'
----@return string
-function _G.textobj_fold(motion)
-  local lnum = vim.fn.line('.') --[[@as integer]]
-  local sel_start = vim.fn.line('v')
-  local lev = vim.fn.foldlevel(lnum)
-  local levp = vim.fn.foldlevel(lnum - 1)
-  -- Multi-line selection with cursor on top of selection
-  if sel_start > lnum then
-    return (lev == 0 and 'zk' or lev > levp and levp > 0 and 'k' or '')
-      .. vim.v.count1
-      .. (motion == 'i' and ']zkV[zj' or ']zV[z')
-  end
-  return (lev == 0 and 'zj' or lev > levp and 'j' or '')
-    .. vim.v.count1
-    .. (motion == 'i' and '[zjV]zk' or '[zV]z')
-end
 -- stylua: ignore start
-vim.keymap.set('x', 'iz', '":<C-u>silent! keepjumps normal! " . v:lua.textobj_fold("i") . "<CR>"', { silent = true, expr = true, noremap = false })
-vim.keymap.set('x', 'az', '":<C-u>silent! keepjumps normal! " . v:lua.textobj_fold("a") . "<CR>"', { silent = true, expr = true, noremap = false })
+vim.keymap.set('x', 'iz', [[':<C-u>silent! keepjumps normal! ' . v:lua.require'utils.misc'.textobj_fold('i') . '<CR>']], { silent = true, expr = true, noremap = false })
+vim.keymap.set('x', 'az', [[':<C-u>silent! keepjumps normal! ' . v:lua.require'utils.misc'.textobj_fold('a') . '<CR>']], { silent = true, expr = true, noremap = false })
 vim.keymap.set('o', 'iz', '<Cmd>silent! normal Viz<CR>', { silent = true, noremap = false })
 vim.keymap.set('o', 'az', '<Cmd>silent! normal Vaz<CR>', { silent = true, noremap = false })
 -- stylua: ignore end
 
 -- Use 'g{' and 'g}' to move to the first/last line of a paragraph
-vim.keymap.set({ 'n', 'x' }, 'g{', function()
-  local chunk_size = 10
-  local linenr = vim.fn.line('.')
-  local count = vim.v.count1
-
-  -- If current line is the first line of paragraph, move one line
-  -- upwards first to goto the first line of previous paragraph
-  if linenr >= 2 then
-    local lines = vim.api.nvim_buf_get_lines(0, linenr - 2, linenr, false)
-    if lines[1]:match('^$') and lines[2]:match('%S') then
-      linenr = linenr - 1
-    end
-  end
-
-  while linenr >= 1 do
-    local chunk = vim.api.nvim_buf_get_lines(
-      0,
-      math.max(0, linenr - chunk_size - 1),
-      linenr - 1,
-      false
-    )
-    for i, line in ipairs(vim.iter(chunk):rev():totable()) do
-      local current_linenr = linenr - i
-      if line:match('^$') then
-        count = count - 1
-        if count <= 0 then
-          vim.cmd.normal({ "m'", bang = true })
-          vim.cmd(tostring(current_linenr + 1))
-          return
-        end
-      elseif current_linenr <= 1 then
-        vim.cmd.normal({ "m'", bang = true })
-        vim.cmd('1')
-        return
-      end
-    end
-    linenr = linenr - chunk_size
-  end
-end, { noremap = false })
-
-vim.keymap.set({ 'n', 'x' }, 'g}', function()
-  local chunk_size = 10
-  local linenr = vim.fn.line('.')
-  local buf_line_count = vim.api.nvim_buf_line_count(0)
-  local count = vim.v.count1
-
-  -- If current line is the last line of paragraph, move one line
-  -- downwards first to goto the last line of next paragraph
-  if buf_line_count - linenr >= 1 then
-    local lines = vim.api.nvim_buf_get_lines(0, linenr - 1, linenr + 1, false)
-    if lines[1]:match('%S') and lines[2]:match('^$') then
-      linenr = linenr + 1
-    end
-  end
-
-  while linenr <= buf_line_count do
-    local chunk =
-      vim.api.nvim_buf_get_lines(0, linenr, linenr + chunk_size, false)
-    for i, line in ipairs(chunk) do
-      local current_linenr = linenr + i
-      if line:match('^$') then
-        count = count - 1
-        if count <= 0 then
-          vim.cmd.normal({ "m'", bang = true })
-          vim.cmd(tostring(current_linenr - 1))
-          return
-        end
-      elseif current_linenr >= buf_line_count then
-        vim.cmd.normal({ "m'", bang = true })
-        vim.cmd(tostring(buf_line_count))
-        return
-      end
-    end
-    linenr = linenr + chunk_size
-  end
-end, { noremap = false })
+-- stylua: ignore start
+vim.keymap.set({ 'n', 'x' }, 'g{', function() require('utils.misc').goto_paragraph_firstline() end, { noremap = false })
+vim.keymap.set({ 'n', 'x' }, 'g}', function() require('utils.misc').goto_paragraph_lastline() end, { noremap = false })
+-- stylua: ignore end
 
 vim.keymap.set('o', 'g{', '<Cmd>silent! normal Vg{<CR>', { noremap = false })
 vim.keymap.set('o', 'g}', '<Cmd>silent! normal Vg}<CR>', { noremap = false })
--- lua/utils/misc.lua
+++ lua/utils/misc.lua
@@ -108,4 +108,138 @@ function M.qftf(info)
   end, qflist)
 end
 
+-- Text object: folds
+---Returns the key sequence to select around/inside a fold,
+---supposed to be called in visual mode
+---@param motion 'i'|'a'
+---@return string
+function M.textobj_fold(motion)
+  local lnum = vim.fn.line('.') --[[@as integer]]
+  local sel_start = vim.fn.line('v')
+  local lev = vim.fn.foldlevel(lnum)
+  local levp = vim.fn.foldlevel(lnum - 1)
+  -- Multi-line selection with cursor on top of selection
+  if sel_start > lnum then
+    return (lev == 0 and 'zk' or lev > levp and levp > 0 and 'k' or '')
+      .. vim.v.count1
+      .. (motion == 'i' and ']zkV[zj' or ']zV[z')
+  end
+  return (lev == 0 and 'zj' or lev > levp and 'j' or '')
+    .. vim.v.count1
+    .. (motion == 'i' and '[zjV]zk' or '[zV]z')
+end
+
+---Go to the first line of current paragraph
+---@return nil
+function M.goto_paragraph_firstline()
+  local chunk_size = 10
+  local linenr = vim.fn.line('.')
+  local count = vim.v.count1
+
+  -- If current line is the first line of paragraph, move one line
+  -- upwards first to goto the first line of previous paragraph
+  if linenr >= 2 then
+    local lines = vim.api.nvim_buf_get_lines(0, linenr - 2, linenr, false)
+    if lines[1]:match('^$') and lines[2]:match('%S') then
+      linenr = linenr - 1
+    end
+  end
+
+  while linenr >= 1 do
+    local chunk = vim.api.nvim_buf_get_lines(
+      0,
+      math.max(0, linenr - chunk_size - 1),
+      linenr - 1,
+      false
+    )
+    for i, line in ipairs(vim.iter(chunk):rev():totable()) do
+      local current_linenr = linenr - i
+      if line:match('^$') then
+        count = count - 1
+        if count <= 0 then
+          vim.cmd.normal({ "m'", bang = true })
+          vim.cmd(tostring(current_linenr + 1))
+          return
+        end
+      elseif current_linenr <= 1 then
+        vim.cmd.normal({ "m'", bang = true })
+        vim.cmd('1')
+        return
+      end
+    end
+    linenr = linenr - chunk_size
+  end
+end
+
+---Go to the last line of current paragraph
+---@return nil
+function M.goto_paragraph_lastline()
+  local chunk_size = 10
+  local linenr = vim.fn.line('.')
+  local buf_line_count = vim.api.nvim_buf_line_count(0)
+  local count = vim.v.count1
+
+  -- If current line is the last line of paragraph, move one line
+  -- downwards first to goto the last line of next paragraph
+  if buf_line_count - linenr >= 1 then
+    local lines = vim.api.nvim_buf_get_lines(0, linenr - 1, linenr + 1, false)
+    if lines[1]:match('%S') and lines[2]:match('^$') then
+      linenr = linenr + 1
+    end
+  end
+
+  while linenr <= buf_line_count do
+    local chunk =
+      vim.api.nvim_buf_get_lines(0, linenr, linenr + chunk_size, false)
+    for i, line in ipairs(chunk) do
+      local current_linenr = linenr + i
+      if line:match('^$') then
+        count = count - 1
+        if count <= 0 then
+          vim.cmd.normal({ "m'", bang = true })
+          vim.cmd(tostring(current_linenr - 1))
+          return
+        end
+      elseif current_linenr >= buf_line_count then
+        vim.cmd.normal({ "m'", bang = true })
+        vim.cmd(tostring(buf_line_count))
+        return
+      end
+    end
+    linenr = linenr + chunk_size
+  end
+end
+
+---Close floating windows with 'q'
+--- 1. If current window is a floating window, close it and return
+--- 2. Else, close all floating windows that can be focused
+--- 3. Fallback to normal mode 'q' if no floating window can be focused
+---@return nil
+function M.q()
+  local count = 0
+  local current_win = vim.api.nvim_get_current_win()
+  -- Close current win only if it's a floating window
+  if vim.api.nvim_win_get_config(current_win).relative ~= '' then
+    vim.api.nvim_win_close(current_win, true)
+    return
+  end
+  for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+    if vim.api.nvim_win_is_valid(win) then
+      local config = vim.api.nvim_win_get_config(win)
+      -- Close floating windows that can be focused
+      if config.relative ~= '' and config.focusable then
+        vim.api.nvim_win_close(win, false) -- do not force
+        count = count + 1
+      end
+    end
+  end
+  if count == 0 then -- Fallback
+    vim.api.nvim_feedkeys(
+      vim.api.nvim_replace_termcodes('q', true, true, true),
+      'n',
+      false
+    )
+  end
+end
+
 return M
