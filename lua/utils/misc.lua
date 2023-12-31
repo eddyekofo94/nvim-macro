e70f836bba593123b3cb5717c9b66c054706a8db
bekaboo <18127878294@qq.com>
2024-01-01 04:59:45 +0800

    2397: Fix/Perf: utils.misc: func `qftf()`: too many results to unpack

--- lua/utils/misc.lua
+++ lua/utils/misc.lua
@@ -12,16 +12,44 @@ function M.qftf(info)
     return {}
   end
 
+  local fname_str_cache = {}
+  local lnum_str_cache = {}
+  local col_str_cache = {}
+  local type_str_cache = {}
+  local nr_str_cache = {}
+
+  local fname_width_cache = {}
+  local lnum_width_cache = {}
+  local col_width_cache = {}
+  local type_width_cache = {}
+  local nr_width_cache = {}
+
+  ---Traverse the qflist and get the maximum display width of the
+  ---transformed string; cache the transformed string and its width
+  ---in table `str_cache` and `width_cache` respectively
   ---@param trans fun(item: table): string|number
-  ---@param max_width integer?
+  ---@param max_width_allowed integer?
+  ---@param str_cache table
+  ---@param width_cache table
   ---@return integer
-  local function _get_width(trans, max_width)
-    return math.min(
-      max_width or math.huge,
-      math.max(unpack(vim.tbl_map(function(item)
-        return vim.fn.strdisplaywidth(tostring(trans(item)))
-      end, qflist)))
-    )
+  local function _traverse(
+    trans,
+    max_width_allowed,
+    str_cache,
+    width_cache
+  )
+    max_width_allowed = max_width_allowed or math.huge
+    local max_width_seen = 0
+    for i, item in ipairs(qflist) do
+      local str = tostring(trans(item))
+      local width = vim.fn.strdisplaywidth(str)
+      str_cache[i] = str
+      width_cache[i] = width
+      if width > max_width_seen then
+        max_width_seen = width
+      end
+    end
+    return math.min(max_width_allowed, max_width_seen)
   end
 
   ---@param item table
@@ -70,42 +98,65 @@ function M.qftf(info)
     return item.nr <= 0 and '' or ' ' .. item.nr
   end
 
+  -- stylua: ignore start
   local max_width = math.ceil(vim.go.columns / 2)
-  local fname_width = _get_width(_fname_trans, max_width)
-  local lnum_width = _get_width(_lnum_trans, max_width)
-  local col_width = _get_width(_col_trans, max_width)
-  local type_width = _get_width(_type_trans, max_width)
-  local nr_width = _get_width(_nr_trans, max_width)
+  local fname_width = _traverse(_fname_trans, max_width, fname_str_cache, fname_width_cache)
+  local lnum_width = _traverse(_lnum_trans, max_width, lnum_str_cache, lnum_width_cache)
+  local col_width = _traverse(_col_trans, max_width, col_str_cache, col_width_cache)
+  local type_width = _traverse(_type_trans, max_width, type_str_cache, type_width_cache)
+  local nr_width = _traverse(_nr_trans, max_width, nr_str_cache, nr_width_cache)
+  -- stylua: ignore end
 
+  local lines = {} ---@type string[]
   local format_str = vim.g.modern_ui and '%s %s:%s%s%s %s'
     or '%s│%s:%s%s%s│ %s'
-  return vim.tbl_map(function(item)
+
+  local function _fill_item(idx, item)
     if item.valid == 0 then
-      return ''
+      table.insert(lines, '')
+      return
     end
 
-    local fname = tostring(_fname_trans(item))
+    local fname = fname_str_cache[idx]
+    local fname_cur_width = fname_width_cache[idx]
+
     if item.lnum == 0 and item.col == 0 then
-      return fname
+      table.insert(lines, fname)
+      return
     end
 
-    local lnum = tostring(_lnum_trans(item))
-    local col = tostring(_col_trans(item))
-    local type = tostring(_type_trans(item))
-    local nr = tostring(_nr_trans(item))
-    return string.format(
-      format_str,
-      -- Do not use `string.format()` here because it only allows
-      -- at most 99 characters for alignment and alignment is
-      -- based on byte length instead of display length
-      fname .. string.rep(' ', fname_width - vim.fn.strdisplaywidth(fname)),
-      string.rep(' ', lnum_width - vim.fn.strdisplaywidth(lnum)) .. lnum,
-      col .. string.rep(' ', col_width - vim.fn.strdisplaywidth(col)),
-      type .. string.rep(' ', type_width - vim.fn.strdisplaywidth(type)),
-      nr .. string.rep(' ', nr_width - vim.fn.strdisplaywidth(nr)),
-      item.text
+    local lnum = lnum_str_cache[idx]
+    local col = col_str_cache[idx]
+    local type = type_str_cache[idx]
+    local nr = nr_str_cache[idx]
+
+    local lnum_cur_width = lnum_width_cache[idx]
+    local col_cur_width = col_width_cache[idx]
+    local type_cur_width = type_width_cache[idx]
+    local nr_cur_width = nr_width_cache[idx]
+
+    table.insert(
+      lines,
+      string.format(
+        format_str,
+        -- Do not use `string.format()` here because it only allows
+        -- at most 99 characters for alignment and alignment is
+        -- based on byte length instead of display length
+        fname .. string.rep(' ', fname_width - fname_cur_width),
+        string.rep(' ', lnum_width - lnum_cur_width) .. lnum,
+        col .. string.rep(' ', col_width - col_cur_width),
+        type .. string.rep(' ', type_width - type_cur_width),
+        nr .. string.rep(' ', nr_width - nr_cur_width),
+        item.text
+      )
     )
-  end, qflist)
+  end
+
+  for i, item in ipairs(qflist) do
+    _fill_item(i, item)
+  end
+
+  return lines
 end
 
 -- Text object: folds
