fb06087b1df5ebd25ef3486600fef71abea2c739
Eddy Ekofo <eddy.ekofo@amadeus.com>
2024-01-03 10:47:31 +0000

    Disable some maps

--- lua/configs/vim-easy-align.lua
+++ lua/configs/vim-easy-align.lua
@@ -1,15 +1,15 @@
 vim.g.easy_align_delimiters = {
-  ['\\'] = {
+  ["\\"] = {
     pattern = [[\\\+]],
   },
-  ['/'] = {
+  ["/"] = {
     pattern = [[//\+\|/\*\|\*/]],
-    delimiter_align = 'c',
-    ignore_groups = '!Comment',
+    delimiter_align = "c",
+    ignore_groups = "!Comment",
   },
 }
 
 -- stylua: ignore start
-vim.keymap.set({ 'n', 'x' }, 'gl', '<Plug>(EasyAlign)', { noremap = false })
-vim.keymap.set({ 'n', 'x' }, 'gL', '<Plug>(LiveEasyAlign)', { noremap = false })
+-- vim.keymap.set({ 'n', 'x' }, 'gl', '<Plug>(EasyAlign)', { noremap = false })
+-- vim.keymap.set({ 'n', 'x' }, 'gL', '<Plug>(LiveEasyAlign)', { noremap = false })
 -- stylua: ignore end
