e5ae9796f0a85f5b8f60eafb631c4ad6c8f070a6
bekaboo <18127878294@qq.com>
2024-01-10 01:25:39 +0800

    feat(configs): ultimate-autopair: disable autopairing in cmdline regex

--- lua/configs/ultimate-autopair.lua
+++ lua/configs/ultimate-autopair.lua
@@ -1,4 +1,39 @@
+---Record previous cmdline completion types,
+---cmdcompltype[1] is the current completion type,
+---cmdcompltype[2] is the previous completion type
+---@type string[]
+local compltype = {}
+
+vim.api.nvim_create_autocmd('CmdlineChanged', {
+  desc = 'Record cmd compltype to determine whether to autopair.',
+  group = vim.api.nvim_create_augroup('AutopairRecordCmdCompltype', {}),
+  callback = function()
+    local type = vim.fn.getcmdcompltype()
+    if compltype[1] == type then
+      return
+    end
+    compltype[2] = compltype[1]
+    compltype[1] = type
+  end,
+})
+
 require('ultimate-autopair').setup({
+  extensions = {
+    alpha = { after = true },
+    cond = {
+      cond = function(f)
+        return not f.in_macro()
+          -- Disable autopairs when inserting a regex,
+          -- e.g. `:s/{pattern}/{string}/[flags]` or
+          -- `:g/{pattern}/[cmd]`, etc.
+          and (
+            not f.in_cmdline()
+            or compltype[1] ~= ''
+            or compltype[2] ~= 'command'
+          )
+      end,
+    },
+  },
   { '\\(', '\\)' },
   { '\\[', '\\]' },
   { '\\{', '\\}' },
