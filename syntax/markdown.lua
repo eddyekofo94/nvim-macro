6bd74787aca14715fbd792d8e669aff6830347af
bekaboo <18127878294@qq.com>
2024-01-09 16:03:17 +0800

    fix(syntax): markdown: invalid buffer

--- syntax/markdown.lua
+++ syntax/markdown.lua
@@ -5,13 +5,13 @@ end
 -- Use vim.schedule to avoid freezing neovim when reloading markdown files
 local buf = vim.api.nvim_get_current_buf()
 vim.schedule(function()
+  if not vim.api.nvim_buf_is_valid(buf) then
+    return
+  end
   -- Need to use `nvim_buf_call()` else opening LSP hover window will
   -- mess up the syntax highlighting of current buffer, seems like
   -- a bug of neovim
   vim.api.nvim_buf_call(buf, function()
-    if not vim.api.nvim_buf_is_valid(buf) then
-      return
-    end
     -- Get good embedded code block syntax highlighting from treesitter
     -- and good math conceal from vimtex at the same time
     if not vim.b.midfile then
