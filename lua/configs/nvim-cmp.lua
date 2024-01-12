3b23808a3f9740ecc024c4ec979e495eb4648dbe
bekaboo <18127878294@qq.com>
2024-01-13 01:54:13 +0800

    fix(configs): nvim-cmp: revert 441b1a46 for func/expr compl distinction
    Bug:    Because the opening parenthesis after func is removed in cmdline
            completion menu, it is hard to tell which entries are functions
            and which are expressions for some commands, e.g. `:call`
    Fix:    Revert 441b1a46

--- lua/configs/nvim-cmp.lua
+++ lua/configs/nvim-cmp.lua
@@ -232,14 +232,6 @@ cmp.setup({
         cmp_item.abbr = path_escaped
         cmp_item.dup = 0
         entry.completion_item.label = path_escaped
-      elseif compltype == 'function' or compltype == 'expression' then
-        -- Remove trailing open parenthesis in func or expr completions,
-        -- we now have cmdline autopairs for that
-        local func_name_no_open_paren = cmp_item.word:gsub('%($', '', 1)
-        cmp_item.word = func_name_no_open_paren
-        cmp_item.abbr = func_name_no_open_paren
-        cmp_item.dup = 0
-        entry.completion_item.label = func_name_no_open_paren
       end
 
       -- Use special icons for file / directory completions
