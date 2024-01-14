da81be051148f24ee80f311502d4e0162cf8388b
bekaboo <18127878294@qq.com>
2024-01-14 12:53:40 +0800

    fix(utils): win: `effective_lines()` wrong when `&laststatus` is 1

--- lua/utils/win.lua
+++ lua/utils/win.lua
@@ -25,9 +25,14 @@ function M.effective_lines()
     - (
       ls == 0 and 0
       or (ls == 2 or ls == 3) and 1
-      or #vim.tbl_filter(function(win)
-        return vim.fn.win_gettype(win) ~= 'popup'
-      end, vim.api.nvim_tabpage_list_wins(0))
+      or (
+        #vim.tbl_filter(function(win)
+              return vim.fn.win_gettype(win) ~= 'popup'
+            end, vim.api.nvim_tabpage_list_wins(0))
+            > 1
+          and 1
+        or 0
+      )
     )
 end
 
