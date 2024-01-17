3302cdcc6d51f13c5b7777d95fe645442d1380e1
bekaboo <18127878294@qq.com>
2024-01-18 01:31:46 +0800

    tweak(configs): gitsigns: make gitsigns keymaps global

--- lua/configs/gitsigns.lua
+++ lua/configs/gitsigns.lua
@@ -1,6 +1,7 @@
 local icons = require('utils').static.icons
+local gs = require('gitsigns')
 
-require('gitsigns').setup({
+gs.setup({
   preview_config = {
     border = 'solid',
     style = 'minimal',
@@ -19,70 +20,62 @@ require('gitsigns').setup({
     virt_text_pos = 'eol',
     delay = 100,
   },
-  on_attach = function(bufnr)
-    local gs = package.loaded.gitsigns
-
-    local function map(mode, l, r, opts)
-      opts = opts or {}
-      opts.buffer = bufnr
-      vim.keymap.set(mode, l, r, opts)
-    end
-
-    -- Navigation
-    map({ 'n', 'x' }, ']c', function()
-      if vim.wo.diff then
-        vim.api.nvim_feedkeys(vim.v.count1 .. ']c', 'n', true)
-        return
-      end
-      for _ = 1, vim.v.count1 do
-        gs.next_hunk()
-      end
-    end)
-
-    map({ 'n', 'x' }, '[c', function()
-      if vim.wo.diff then
-        vim.api.nvim_feedkeys(vim.v.count1 .. '[c', 'n', true)
-        return
-      end
-      for _ = 1, vim.v.count1 do
-        gs.prev_hunk()
-      end
-    end)
-
-    -- Actions
-    map('n', '<leader>gs', gs.stage_hunk)
-    map('n', '<leader>gr', gs.reset_hunk)
-    map('n', '<leader>gS', gs.stage_buffer)
-    map('n', '<leader>gu', gs.undo_stage_hunk)
-    map('n', '<leader>gR', gs.reset_buffer)
-    map('n', '<leader>gp', gs.preview_hunk)
-    map('n', '<leader>gb', gs.blame_line)
-
-    map('x', '<leader>gs', function()
-      gs.stage_hunk({
-        vim.fn.line('.'),
-        vim.fn.line('v'),
-      })
-    end)
-    map('x', '<leader>gr', function()
-      gs.reset_hunk({
-        vim.fn.line('.'),
-        vim.fn.line('v'),
-      })
-    end)
-
-    -- Text object
-    map(
-      { 'o', 'x' },
-      'ic',
-      ':<C-U>Gitsigns select_hunk<CR>',
-      { silent = true }
-    )
-    map(
-      { 'o', 'x' },
-      'ac',
-      ':<C-U>Gitsigns select_hunk<CR>',
-      { silent = true }
-    )
-  end,
 })
+
+-- Setup keymaps
+-- Navigation
+vim.keymap.set({ 'n', 'x' }, ']c', function()
+  if vim.wo.diff then
+    vim.api.nvim_feedkeys(vim.v.count1 .. ']c', 'n', true)
+    return
+  end
+  for _ = 1, vim.v.count1 do
+    gs.next_hunk()
+  end
+end)
+
+vim.keymap.set({ 'n', 'x' }, '[c', function()
+  if vim.wo.diff then
+    vim.api.nvim_feedkeys(vim.v.count1 .. '[c', 'n', true)
+    return
+  end
+  for _ = 1, vim.v.count1 do
+    gs.prev_hunk()
+  end
+end)
+
+-- Actions
+vim.keymap.set('n', '<leader>gs', gs.stage_hunk)
+vim.keymap.set('n', '<leader>gr', gs.reset_hunk)
+vim.keymap.set('n', '<leader>gS', gs.stage_buffer)
+vim.keymap.set('n', '<leader>gu', gs.undo_stage_hunk)
+vim.keymap.set('n', '<leader>gR', gs.reset_buffer)
+vim.keymap.set('n', '<leader>gp', gs.preview_hunk)
+vim.keymap.set('n', '<leader>gb', gs.blame_line)
+
+vim.keymap.set('x', '<leader>gs', function()
+  gs.stage_hunk({
+    vim.fn.line('.'),
+    vim.fn.line('v'),
+  })
+end)
+vim.keymap.set('x', '<leader>gr', function()
+  gs.reset_hunk({
+    vim.fn.line('.'),
+    vim.fn.line('v'),
+  })
+end)
+
+-- Text object
+vim.keymap.set(
+  { 'o', 'x' },
+  'ic',
+  ':<C-U>Gitsigns select_hunk<CR>',
+  { silent = true }
+)
+vim.keymap.set(
+  { 'o', 'x' },
+  'ac',
+  ':<C-U>Gitsigns select_hunk<CR>',
+  { silent = true }
+)
