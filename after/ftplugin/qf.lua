856f1c42f91ef08a82cf1287dcb11bb8fdcfb376
bekaboo <18127878294@qq.com>
2024-01-22 03:54:00 +0800

    tweak(ftplugin): qf: remove `<C-n/p/j/k>` keymaps

--- after/ftplugin/qf.lua
+++ after/ftplugin/qf.lua
@@ -10,14 +10,6 @@ vim.opt_local.rnu = false
 vim.opt_local.signcolumn = 'no'
 vim.opt_local.statuscolumn = ''
 
--- stylua: ignore start
-vim.keymap.set('n', '=',     '<CR>zz<C-w>p',  { buffer = true })
-vim.keymap.set('n', '<C-j>', 'j<CR>zz<C-w>p', { buffer = true })
-vim.keymap.set('n', '<C-k>', 'k<CR>zz<C-w>p', { buffer = true })
-vim.keymap.set('n', '<C-n>', 'j<CR>zz<C-w>p', { buffer = true })
-vim.keymap.set('n', '<C-p>', 'k<CR>zz<C-w>p', { buffer = true })
--- stylua: ignore end
-
 -- Provides `:Cfilter` and `:Lfilter` commands
 vim.cmd.packadd({
   args = { 'cfilter' },
