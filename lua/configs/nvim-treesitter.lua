40e313a19c81d4c10224a448d28606ffd5d29188
bekaboo <18127878294@qq.com>
2024-02-06 00:44:52 +0800

    fix(configs): nvim-treesitter: fold not initialized for the first buffer

--- lua/configs/nvim-treesitter.lua
+++ lua/configs/nvim-treesitter.lua
@@ -1,24 +1,31 @@
 local ts_configs = require('nvim-treesitter.configs')
 
+---@param buf integer
+---@return nil
+local function enable_ts_folding(buf)
+  -- Treesitter folding is extremely slow in large markdown files,
+  -- making typing and undo lag as hell
+  if not vim.api.nvim_buf_is_valid(buf) or vim.bo[buf].ft == 'markdown' then
+    return
+  end
+  vim.api.nvim_buf_call(buf, function()
+    local o = vim.opt_local
+    local fdm = o.fdm:get() ---@diagnostic disable-line: undefined-field
+    local fde = o.fde:get() ---@diagnostic disable-line: undefined-field
+    local fdt = o.fdt:get() ---@diagnostic disable-line: undefined-field
+    o.fdm = fdm == 'manual' and 'expr' or fdm
+    o.fde = fde == '0' and 'nvim_treesitter#foldexpr()' or fde
+    o.fdt = fdt == 'foldtext()' and 'v:lua.vim.treesitter.foldtext()' or fdt
+  end)
+end
+
+enable_ts_folding(0)
+
 -- Set treesitter folds
 vim.api.nvim_create_autocmd('FileType', {
   group = vim.api.nvim_create_augroup('TSFolds', {}),
   callback = function(info)
-    -- Treesitter folding is extremely slow in large markdown files,
-    -- making typing and undo lag as hell
-    if info.match == 'markdown' then
-      return
-    end
-    vim.api.nvim_buf_call(info.buf, function()
-      local o = vim.opt_local
-      local fdm = o.fdm:get() ---@diagnostic disable-line: undefined-field
-      local fde = o.fde:get() ---@diagnostic disable-line: undefined-field
-      local fdt = o.fdt:get() ---@diagnostic disable-line: undefined-field
-      o.fdm = o.fdm:get() == 'manual' and 'expr' or fdm
-      o.fde = o.fde:get() == '0' and 'nvim_treesitter#foldexpr()' or fde
-      o.fdt = o.fdt:get() == 'foldtext()' and 'v:lua.vim.treesitter.foldtext()'
-        or fdt
-    end)
+    enable_ts_folding(info.buf)
   end,
 })
 
