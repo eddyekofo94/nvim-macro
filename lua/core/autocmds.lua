46d4563775c5aebb6b66f209921e494ceacb42c2
bekaboo <18127878294@qq.com>
2024-03-27 00:07:03 +0800

    fix(autocmd): syntax disabled in oil buffer after opening big files

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -30,7 +30,15 @@ augroup('BigFileSettings', {
         vim.opt_local.signcolumn = 'no'
         vim.opt_local.foldcolumn = '0'
         vim.opt_local.winbar = ''
-        vim.cmd.syntax('off')
+        vim.opt_local.syntax = ''
+        autocmd('BufReadPost', {
+          once = true,
+          buffer = info.buf,
+          callback = function()
+            vim.opt_local.syntax = ''
+            return true
+          end,
+        })
       end
     end,
   },
