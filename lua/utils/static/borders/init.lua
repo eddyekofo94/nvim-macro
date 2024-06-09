743718b6fd268bf8222a04ff898fd40973f08daa
bekaboo <18127878294@qq.com>
2024-06-10 01:41:11 +0800

    fix(utils): static.borders: wrong `require` path

--- lua/utils/static/borders/init.lua
+++ lua/utils/static/borders/init.lua
@@ -1,2 +1,2 @@
-return vim.g.no_nf and require('utils.static._borders_no_nf')
-  or require('utils.static._borders')
+return vim.g.no_nf and require('utils.static.borders._borders_no_nf')
+  or require('utils.static.borders._borders')
