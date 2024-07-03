66e5f8ecd752b176f8914ee1e7c824f39a2e7cd0
bekaboo <18127878294@qq.com>
2024-07-03 23:49:40 +0800

    fix(utils): term: not considering all foreground commands checking TUI

--- lua/utils/term.lua
+++ lua/utils/term.lua
@@ -12,23 +12,30 @@ M.TUI_REGEX = vim.regex(
 ---@param buf integer? buffer handler
 ---@return boolean?
 function M.running_tui(buf)
-  local cmd = M.fg_cmd(buf)
-  return cmd and M.TUI_REGEX:match_str(cmd) and true
+  local cmds = M.fg_cmds(buf)
+  for _, cmd in ipairs(cmds) do
+    if M.TUI_REGEX:match_str(cmd) then
+      return true
+    end
+  end
+  return false
 end
 
 ---Get the command running in the foreground in the terminal buffer 'buf'
 ---@param buf integer? terminal buffer handler, default to 0 (current)
----@return string?: command string
-function M.fg_cmd(buf)
+---@return string[]: command running in the foreground
+function M.fg_cmds(buf)
   buf = buf or 0
   if not vim.api.nvim_buf_is_valid(buf) or vim.bo[buf].bt ~= 'terminal' then
-    return
+    return {}
   end
   local channel = vim.bo[buf].channel
   local chan_valid, pid = pcall(vim.fn.jobpid, channel)
   if not chan_valid then
-    return
+    return {}
   end
+
+  local cmds = {}
   for _, stat_cmd_str in
     ipairs(vim.split(vim.fn.system('ps h -o stat,args -g ' .. pid), '\n', {
       trimempty = true,
@@ -38,9 +45,11 @@ function M.fg_cmd(buf)
       trimempty = true,
     }))
     if stat:find('^%w+%+') then
-      return cmd
+      table.insert(cmds, cmd)
     end
   end
+
+  return cmds
 end
 
 return M
