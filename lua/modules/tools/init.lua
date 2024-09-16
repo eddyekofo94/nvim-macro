71f136c00e3ab06c07456fb1c56b3f4ac5f7116a
Eddy Ekofo <eddy.ekofo@amadeus.com>
2024-09-16 16:18:27 +0000

    Added Tabby for cleaner tabs

--- lua/modules/tools/init.lua
+++ lua/modules/tools/init.lua
@@ -231,4 +231,12 @@ return {
       require "configs.oil"
     end,
   },
+  {
+    "nanozuki/tabby.nvim",
+    event = "VimEnter", -- if you want lazy load, see below
+    dependencies = "nvim-tree/nvim-web-devicons",
+    config = function()
+      require("tabby").setup {}
+    end,
+  },
 }
